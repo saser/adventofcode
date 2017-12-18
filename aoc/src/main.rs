6cb5677d351c66b422b2884d8edbef0e9fc5cbc2
Christian Persson <saser@live.se>
2017-12-19 00:45:37 +0100

    Clean up argument parsing a bit

--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -1,54 +1,56 @@
 extern crate base;
+#[macro_use]
 extern crate clap;
 
 use base::Part;
 use clap::{App, Arg, ArgMatches};
+use std::process;
+
+static APP_NAME: &'static str = "aoc";
+static APP_VERSION: &'static str = "0.1.0";
+static APP_AUTHOR: &'static str = "Christian Persson <saser@live.se>";
+static APP_ABOUT: &'static str = "Runs solutions for the Advent of Code 2017 programming problems";
+
+macro_rules! eprintln {
+    ($($arg:tt)*) => (
+        use std::io::Write;
+        let _ = writeln!(&mut ::std::io::stderr(), $($arg)* );
+    )
+}
 
 fn main() {
-    let app = App::new("adventofcode2017")
-        .version("0.1.0")
-        .author("Christian Persson <saser@live.se>")
-        .about("Runs solutions for the Advent of Code 2017 programming problems")
+    let app = create_app();
+    let matches = app.get_matches();
+
+    let (day, part) = parse_arguments(&matches).unwrap_or_else(|e| {
+        eprintln!("Unable to parse arguments: {}", e);
+        process::exit(1);
+    });
+    println!("day: {}", day);
+    println!("part: {}", part);
+}
+
+fn create_app() -> App<'static, 'static> {
+    App::new(APP_NAME)
+        .version(APP_VERSION)
+        .author(APP_AUTHOR)
+        .about(APP_ABOUT)
         .arg(Arg::with_name("day")
             .help("Specifies which day (1-25) to run")
             .takes_value(true)
             .required(true))
         .arg(Arg::with_name("part")
-            .help("Specifies which part of the problem to run (both if unspecified)")
+            .help("Specifies which part of the problem to run")
             .takes_value(true)
-            .required(false)
-            .possible_values(&["1", "2"]));
-    let matches = app.get_matches();
-
-    let day = parse_day(&matches).unwrap_or_else(|msg| {
-        println!("{}", msg);
-        std::process::exit(1);
-    });
-    let parts = parse_part(&matches);
-
-    println!("Day: {}", day);
-    println!("Part(s): {:?}", parts);
+            .required(true)
+            .possible_values(&["1", "2"]))
 }
 
-fn parse_day(matches: &ArgMatches) -> Result<u8, String> {
-    let day = matches.value_of("day").unwrap();
-    let day = day.parse::<u8>().unwrap();
-    match day {
-        1...25 => Ok(day),
-        _ => Err(format!("invalid day: {}", day)),
+fn parse_arguments(matches: &ArgMatches) -> Result<(u8, Part), String> {
+    let day = value_t!(matches.value_of("day"), u8).unwrap();
+    if day < 1 || day > 25 {
+        return Err("day must be 1-25 (inclusive)".to_string());
     }
-}
-
-fn parse_part(matches: &ArgMatches) -> Vec<Part> {
-    let part = matches.value_of("part");
-    match part {
-        Some(p) => {
-            vec![match p {
-                     "1" => Part::One,
-                     "2" => Part::Two,
-                     _ => unreachable!(),
-                 }]
-        }
-        None => vec![Part::One, Part::Two],
-    }
+    let part = value_t!(matches.value_of("part"), Part).unwrap();
+    Ok((day, part))
 }
--- base/src/lib.rs
+++ base/src/lib.rs
@@ -1,5 +1,30 @@
+use std::fmt;
+use std::str::FromStr;
+
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum Part {
     One,
     Two,
+}
+
+impl FromStr for Part {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "1" => Ok(Part::One),
+            "2" => Ok(Part::Two),
+            _ => Err(format!("part must be 1 or 2; was: {}", s)),
+        }
+    }
+}
+
+impl fmt::Display for Part {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let s = match *self {
+            Part::One => "1",
+            Part::Two => "2",
+        };
+        write!(f, "{}", s)
+    }
 }
