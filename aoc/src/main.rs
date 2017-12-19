e16ed7289e3f95905da63b3c09cf74daf9ab2957
Christian Persson <saser@live.se>
2017-12-19 01:03:01 +0100

    Add reading of input file

--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -4,6 +4,8 @@ extern crate clap;
 
 use base::Part;
 use clap::{App, Arg, ArgMatches};
+use std::fs::File;
+use std::io::{self, Read};
 use std::process;
 
 static APP_NAME: &'static str = "aoc";
@@ -26,9 +28,13 @@ fn main() {
         eprintln!("Unable to parse arguments: {}", e);
         process::exit(1);
     });
+    let input = read_input(&input_path).unwrap_or_else(|e| {
+        eprintln!("Unable to read input file: {}", e);
+        process::exit(1);
+    });
     println!("day: {}", day);
     println!("part: {}", part);
-    println!("input file path: {}", input_path);
+    println!("input: {}", input);
 }
 
 fn create_app() -> App<'static, 'static> {
@@ -59,4 +65,11 @@ fn parse_arguments(matches: &ArgMatches) -> Result<(u8, Part, String), String> {
     let part = value_t!(matches.value_of("part"), Part).unwrap();
     let input_path = matches.value_of("input_file").unwrap().to_string();
     Ok((day, part, input_path))
+}
+
+fn read_input(path: &str) -> io::Result<String> {
+    let mut input = String::new();
+    let mut file = File::open(path)?;
+    file.read_to_string(&mut input)?;
+    Ok(input)
 }
