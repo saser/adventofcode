7c65e08567ded87f6f8d29910bcbd0c5ae62eb14
Christian Persson <saser@live.se>
2017-12-22 21:00:33 +0100

    day02: implement solution for part 2

--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -32,8 +32,7 @@ fn parse_line(line: &str) -> Vec<u32> {
 }
 
 fn min_max(nums: &[u32]) -> u32 {
-    // Can also be solved using the `Iterator::max` and `Iterator::min` methods,
-    // but that's no fun.
+    // Can also be solved using the `Iterator::max` and `Iterator::min` methods, but that's no fun.
 
     let mut min = nums[0];
     let mut max = nums[0];
@@ -48,7 +47,26 @@ fn min_max(nums: &[u32]) -> u32 {
 }
 
 fn divisors(nums: &[u32]) -> u32 {
-    unimplemented!()
+    // This is a very bad, brute-force approach to the problem. But I can't think of any other way
+    // to do it that is more optimal.
+
+    for (i, &x) in nums.iter().enumerate() {
+        for (j, &y) in nums.iter().enumerate() {
+            if i == j {
+                continue;
+            };
+
+            if x % y == 0 {
+                return x / y;
+            } else if y % x == 0 {
+                return y / x;
+            }
+        }
+    }
+
+    // This is unreachable since the input is guaranteed to have exactly one pair of numbers that
+    // divide each other, so the function is guaranteed to return in the above loops.
+    unreachable!()
 }
 
 #[cfg(test)]
