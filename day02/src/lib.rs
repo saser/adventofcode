3fc9ce9d62b611c8d35cc1aebfae1fc685689754
Christian Persson <saser@live.se>
2017-12-22 20:32:08 +0100

    day02: add the test examples for part 2

--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -58,4 +58,44 @@ mod tests {
         }
     }
 
+    mod part2 {
+        use super::*;
+
+        #[test]
+        fn example_1() {
+            let solver = get_solver();
+            let input = "5 9 2 8";
+            let expected = "4";
+            assert_eq!(expected, solver.solve(Part::Two, input).unwrap());
+        }
+
+        #[test]
+        fn example_2() {
+            let solver = get_solver();
+            let input = "9 4 7 3";
+            let expected = "3";
+            assert_eq!(expected, solver.solve(Part::Two, input).unwrap());
+        }
+
+        #[test]
+        fn example_3() {
+            let solver = get_solver();
+            let input = "3 8 6 5";
+            let expected = "2";
+            assert_eq!(expected, solver.solve(Part::Two, input).unwrap());
+        }
+
+        #[test]
+        fn example_all() {
+            let solver = get_solver();
+            let input = "\
+5 9 2 8
+9 4 7 3
+3 8 6 5\
+            ";
+            let expected = "4";
+            assert_eq!(expected, solver.solve(Part::Two, input).unwrap());
+        }
+    }
+
 }
