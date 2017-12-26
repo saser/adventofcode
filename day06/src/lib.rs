63af459f3c5af631aa85caf44caf04860fdf9f8b
Christian Persson <saser@live.se>
2017-12-26 15:39:02 +0100

    day06: add example test for part 2

--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -83,4 +83,16 @@ mod tests {
             assert_eq!(expected, solver.solve(Part::One, input).unwrap());
         }
     }
+
+    mod part2 {
+        use super::*;
+
+        #[test]
+        fn example() {
+            let solver = get_solver();
+            let input = "0 2 7 0";
+            let expected = "4";
+            assert_eq!(expected, solver.solve(Part::Two, input).unwrap());
+        }
+    }
 }
