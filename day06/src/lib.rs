b3f6925f4add9e5521eae73ffc1981448ecd0874
Christian Persson <saser@live.se>
2017-12-26 15:16:03 +0100

    day06: implement solution for day 1

--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -1,6 +1,7 @@
 extern crate base;
 
 use base::{Part, Solver};
+use std::collections::HashSet;
 
 pub fn get_solver() -> Box<Solver> {
     Box::new(Day06)
@@ -11,7 +12,10 @@ struct Day06;
 impl Solver for Day06 {
     fn solve(&self, part: Part, input: &str) -> Result<String, String> {
         let banks = parse_input(input);
-        Err("day 6 not yet implemented".to_string())
+        match part {
+            Part::One => Ok(count_redistributions(&banks).to_string()),
+            _ => Err("part 2 not yet implemented".to_string()),
+        }
     }
 }
 
@@ -21,6 +25,47 @@ fn parse_input(input: &str) -> Vec<u64> {
         .map(str::parse)
         .map(Result::unwrap)
         .collect()
+}
+
+fn count_redistributions(banks: &[u64]) -> u64 {
+    let mut distributions: HashSet<Vec<u64>> = HashSet::new();
+    let mut distribution = Vec::from(banks);
+    distributions.insert(distribution.clone());
+
+    let mut counter = 0;
+    while counter < distributions.len() {
+        distribution = redistribute(&distribution);
+        distributions.insert(distribution.clone());
+        counter += 1;
+    }
+    counter as u64
+}
+
+fn redistribute(banks: &[u64]) -> Vec<u64> {
+    let mut vec = Vec::from(banks);
+
+    let max_bank_index = find_max_index(&vec);
+    let mut blocks_to_redistribute = vec[max_bank_index];
+    vec[max_bank_index] = 0;
+
+    let len = vec.len();
+    let mut bank_to_increase_index = (max_bank_index + 1) % len;
+    while blocks_to_redistribute > 0 {
+        vec[bank_to_increase_index] += 1;
+        bank_to_increase_index = (bank_to_increase_index + 1) % len;
+        blocks_to_redistribute -= 1;
+    }
+    vec
+}
+
+fn find_max_index<T: Ord>(banks: &[T]) -> usize {
+    banks.iter()
+        .enumerate()
+        .fold(0, |max_index, (index, bank)| if bank > &banks[max_index] {
+            index
+        } else {
+            max_index
+        })
 }
 
 #[cfg(test)]
