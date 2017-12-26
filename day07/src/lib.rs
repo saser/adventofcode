45c3fec3dfd4d4d6ce59667c4f4de3f95db1ecce
Christian Persson <saser@live.se>
2017-12-26 17:10:30 +0100

    day07: create a basic struct representing a program

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -1,6 +1,7 @@
 extern crate base;
 
 use base::{Part, Solver};
+use std::str::FromStr;
 
 pub fn get_solver() -> Box<Solver> {
     Box::new(Day07)
@@ -11,6 +12,20 @@ struct Day07;
 impl Solver for Day07 {
     fn solve(&self, part: Part, input: &str) -> Result<String, String> {
         Err("day XX not yet implemented".to_string())
+    }
+}
+
+struct Program {
+    name: String,
+    weight: u64,
+    holding_up: Vec<String>,
+    held_up_by: Option<String>,
+}
+
+impl FromStr for Program {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Program, String> {
+        unimplemented!()
     }
 }
 
