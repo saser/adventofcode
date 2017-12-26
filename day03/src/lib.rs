92c7be43fbe6d0b90f9db1f6a860e6a633500794
Christian Persson <saser@live.se>
2017-12-26 01:12:39 +0100

    day03: re-implement part 1, implement part 2
    Part 2 apparently runs in < 1 ms, but part 1 takes ~60 ms. Interesting.

--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -2,6 +2,7 @@ extern crate base;
 
 use base::{Part, Solver};
 use base::grid::*;
+use std::collections::HashMap;
 
 pub fn get_solver() -> Box<Solver> {
     Box::new(Day03)
@@ -15,7 +16,7 @@ impl Solver for Day03 {
         let number = parse_input(input.trim());
         match part {
             Part::One => Ok(distance_to_center(number).to_string()),
-            Part::Two => Err("part 2 not done yet".to_string()),
+            Part::Two => Ok(first_after_number_by_summing(number).to_string()),
         }
     }
 }
@@ -24,39 +25,110 @@ fn parse_input(input: &str) -> u64 {
     str::parse(input).unwrap()
 }
 
-fn distance_to_center(target_number: u64) -> u64 {
-    let mut traveler = Traveler::default();
-    // The traveler starts out facing north. Turn clockwise once, to make it face east.
-    traveler.turn(Turn::Clockwise);
-    // The spiral starts at 1, not 0.
-    let mut number = 1;
-    // The innermost layer, containing only 1, is layer 1.
-    let mut current_layer = 1;
-    let mut steps_to_corners = Vec::with_capacity(4);
-    let mut to_corner = 1;
-    let mut to_next_layer = 1;
+struct SpiralTraveler {
+    traveler: Traveler,
+    current_layer: u64,
+    steps_to_corners: Vec<u64>,
+    to_corner: u64,
+    to_next_layer: u64,
+}
 
-    while number < target_number {
-        traveler.step();
-        number += 1;
-
-        to_next_layer -= 1;
-        if to_next_layer == 0 {
-            current_layer += 1;
-            let layer_side = 2 * current_layer - 1;
-            let steps = layer_side - 1;
-            steps_to_corners.append(&mut vec![steps + 1, steps, steps, steps - 1]);
-            to_next_layer = 4 * steps;
-        }
-
-        to_corner -= 1;
-        if to_corner == 0 {
-            traveler.turn(Turn::CounterClockwise);
-            to_corner = steps_to_corners.pop().unwrap();
+impl SpiralTraveler {
+    fn new() -> SpiralTraveler {
+        let mut traveler = Traveler::default();
+        traveler.direction = Direction::East;
+        SpiralTraveler {
+            traveler: traveler,
+            current_layer: 1,
+            steps_to_corners: Vec::with_capacity(4),
+            to_corner: 1,
+            to_next_layer: 1,
         }
     }
+}
 
-    traveler.pos().manhattan_distance()
+impl Iterator for SpiralTraveler {
+    type Item = Point;
+
+    fn next(&mut self) -> Option<Point> {
+        self.traveler.step();
+
+        self.to_next_layer -= 1;
+        if self.to_next_layer == 0 {
+            self.current_layer += 1;
+            let layer_side = 2 * self.current_layer - 1;
+            let steps = layer_side - 1;
+            self.steps_to_corners.append(&mut vec![steps + 1, steps, steps, steps - 1]);
+            self.to_next_layer = 4 * steps;
+        }
+
+        self.to_corner -= 1;
+        if self.to_corner == 0 {
+            self.traveler.turn(Turn::CounterClockwise);
+            self.to_corner = self.steps_to_corners.pop().unwrap();
+        }
+        Some(self.traveler.pos)
+    }
+}
+
+struct Spiral {
+    spiral_traveler: SpiralTraveler,
+    grid: HashMap<Point, u64>,
+    pos: Point,
+    value: u64,
+    next_value_fun: Box<Fn(&Spiral) -> u64>,
+}
+
+impl Spiral {
+    fn new(next_value_fun: Box<Fn(&Spiral) -> u64>) -> Spiral {
+        let initial_pos = Point { x: 0, y: 0 };
+        let mut initial_grid = HashMap::new();
+        initial_grid.insert(initial_pos, 1);
+        Spiral {
+            spiral_traveler: SpiralTraveler::new(),
+            grid: initial_grid,
+            pos: initial_pos,
+            value: 1,
+            next_value_fun: next_value_fun,
+        }
+    }
+}
+
+impl Iterator for Spiral {
+    type Item = (Point, u64);
+
+    fn next(&mut self) -> Option<(Point, u64)> {
+        self.pos = self.spiral_traveler.next().unwrap();
+        self.value = (self.next_value_fun)(self);
+        self.grid.insert(self.pos, self.value);
+        Some((self.pos, self.value))
+    }
+}
+
+fn first_after_number_by_summing(target_number: u64) -> u64 {
+    let mut spiral = Spiral::new(Box::new(|spiral| {
+        let current_pos = spiral.pos;
+        let deltas = [Point { x: 1, y: 0 },
+                      Point { x: 1, y: 1 },
+                      Point { x: 0, y: 1 },
+                      Point { x: -1, y: 1 },
+                      Point { x: -1, y: 0 },
+                      Point { x: -1, y: -1 },
+                      Point { x: 0, y: -1 },
+                      Point { x: 1, y: -1 }];
+        deltas.iter()
+            .map(|&delta| current_pos + delta)
+            .map(|pos| spiral.grid.get(&pos))
+            .map(|op_val| op_val.unwrap_or(&0))
+            .sum()
+    }));
+    spiral.find(|&(_, value)| value > target_number).unwrap().1
+}
+
+fn distance_to_center(target_number: u64) -> u64 {
+    let mut spiral = Spiral::new(Box::new(|spiral| spiral.value + 1));
+    let (pos, _) = spiral.find(|&(_, value)| value == target_number).unwrap();
+    pos.manhattan_distance()
 }
 
 fn required_layers(target_number: u64) -> u64 {
