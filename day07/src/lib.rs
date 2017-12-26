e87ae5851df63051dfef4e55507b8140d34f8176
Christian Persson <saser@live.se>
2017-12-26 17:37:30 +0100

    day07: add parsing (with simple tests) for programs

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -1,6 +1,10 @@
 extern crate base;
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
 
 use base::{Part, Solver};
+use regex::Regex;
 use std::str::FromStr;
 
 pub fn get_solver() -> Box<Solver> {
@@ -25,13 +29,57 @@ struct Program {
 impl FromStr for Program {
     type Err = String;
     fn from_str(s: &str) -> Result<Program, String> {
-        unimplemented!()
+        lazy_static! {
+            static ref NAME_AND_WEIGHT: Regex = Regex::new(r"(?P<name>\w+) \((?P<weight>\d+)\)").unwrap();
+        }
+        let parts: Vec<&str> = s.split(" -> ").collect();
+        let (name_and_weight, programs) = (parts[0],
+                                           if parts.len() == 2 {
+                                               Some(parts[1])
+                                           } else {
+                                               None
+                                           });
+        let name_and_weight_caps = NAME_AND_WEIGHT.captures(name_and_weight).unwrap();
+        let name = name_and_weight_caps["name"].to_string();
+        let weight: u64 = name_and_weight_caps["weight"].parse().unwrap();
+        let holding_up: Vec<String> = match programs {
+            None => Vec::new(),
+            Some(program_str) => program_str.split(", ").map(String::from).collect(),
+        };
+
+        Ok(Program {
+            name: name,
+            weight: weight,
+            holding_up: holding_up,
+            held_up_by: None,
+        })
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    mod parse_tests {
+        use super::*;
+
+        #[test]
+        fn program_not_holding_up() {
+            let input = "pbga (66)";
+            let program = Program::from_str(input).unwrap();
+            assert_eq!("pbga", &program.name);
+            assert_eq!(66, program.weight);
+        }
+
+        #[test]
+        fn program_holding_up() {
+            let input = "fwft (72) -> ktlj, cntj, xhth";
+            let program = Program::from_str(input).unwrap();
+            assert_eq!("fwft", &program.name);
+            assert_eq!(72, program.weight);
+            assert_eq!(&["ktlj", "cntj", "xhth"], &program.holding_up[..]);
+        }
+    }
 
     mod part1 {
         use super::*;
