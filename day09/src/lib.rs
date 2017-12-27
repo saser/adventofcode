5c195bf3c55ca933623a058ea0f553b40c4f0147
Christian Persson <saser@live.se>
2017-12-27 14:16:55 +0100

    day09: add example tests for part 1

--- day09/src/lib.rs
+++ day09/src/lib.rs
@@ -22,10 +22,66 @@ mod tests {
         use super::*;
 
         #[test]
-        fn example() {
+        fn example_1() {
             let solver = get_solver();
-            let input = "put some input here";
-            let expected = "expected output";
+            let input = "{}";
+            let expected = "1";
+            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
+        }
+
+        #[test]
+        fn example_2() {
+            let solver = get_solver();
+            let input = "{{{}}}";
+            let expected = "6";
+            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
+        }
+
+        #[test]
+        fn example_3() {
+            let solver = get_solver();
+            let input = "{{},{}}";
+            let expected = "5";
+            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
+        }
+
+        #[test]
+        fn example_4() {
+            let solver = get_solver();
+            let input = "{{{},{},{{}}}}";
+            let expected = "16";
+            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
+        }
+
+        #[test]
+        fn example_5() {
+            let solver = get_solver();
+            let input = "{<a>,<a>,<a>,<a>}";
+            let expected = "1";
+            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
+        }
+
+        #[test]
+        fn example_6() {
+            let solver = get_solver();
+            let input = "{{<ab>},{<ab>},{<ab>},{<ab>}}";
+            let expected = "9";
+            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
+        }
+
+        #[test]
+        fn example_7() {
+            let solver = get_solver();
+            let input = "{{<!!>},{<!!>},{<!!>},{<!!>}}";
+            let expected = "9";
+            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
+        }
+
+        #[test]
+        fn example_8() {
+            let solver = get_solver();
+            let input = "{{<a!>},{<a!>},{<a!>},{<ab>}}";
+            let expected = "3";
             assert_eq!(expected, solver.solve(Part::One, input).unwrap());
         }
     }
