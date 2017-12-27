1c391f98b40fb314198c6240e9a4c5527ec2f973
Christian Persson <saser@live.se>
2017-12-28 00:47:59 +0100

    day12: implement solution for part 1

--- day12/src/lib.rs
+++ day12/src/lib.rs
@@ -1,7 +1,7 @@
 extern crate base;
 
 use base::{Part, Solver};
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::str::FromStr;
 
 pub fn get_solver() -> Box<Solver> {
@@ -12,7 +12,9 @@ struct Day12;
 
 impl Solver for Day12 {
     fn solve(&self, part: Part, input: &str) -> Result<String, String> {
-        Err("day 12 not yet implemented".to_string())
+        let connections = parse_input(input);
+        let group = find_group(&connections, 0);
+        Ok(group.len().to_string())
     }
 }
 
@@ -31,6 +33,27 @@ fn parse_line(line: &str) -> (u64, Vec<u64>) {
         .map(Result::unwrap)
         .collect();
     (program, connected)
+}
+
+fn find_group(connections: &HashMap<u64, Vec<u64>>, included_program: u64) -> Vec<u64> {
+    let mut queue: VecDeque<u64> = VecDeque::new();
+    let mut group: Vec<u64> = Vec::new();
+    let mut visited = vec![false; connections.len()];
+    queue.push_back(included_program);
+
+    while let Some(program) = queue.pop_front() {
+        if visited[program as usize] {
+            continue;
+        }
+
+        group.push(program);
+        visited[program as usize] = true;
+        let connected_programs = connections.get(&program).unwrap();
+        for &connected_program in connected_programs {
+            queue.push_back(connected_program);
+        }
+    }
+    group
 }
 
 #[cfg(test)]
