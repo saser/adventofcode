a3c43e265665d1f58fcda0cabfaeeb729ecd4d3e
Christian Persson <saser@live.se>
2017-12-27 20:19:46 +0100

    day10: implement solution for part 1

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -1,6 +1,7 @@
 extern crate base;
 
 use base::{Part, Solver};
+use std::str::FromStr;
 
 pub fn get_solver() -> Box<Solver> {
     Box::new(Day10)
@@ -10,7 +11,60 @@ struct Day10;
 
 impl Solver for Day10 {
     fn solve(&self, part: Part, input: &str) -> Result<String, String> {
-        Err("day 10 not yet implemented".to_string())
+        let mut vector = initialize_vector(256);
+        let lengths = parse_input(input);
+        knot_hash(&mut vector, &lengths);
+        let product = vector[0] * vector[1];
+        Ok(product.to_string())
+    }
+}
+
+fn parse_input(input: &str) -> Vec<u8> {
+    input.split(',')
+        .map(u8::from_str)
+        .map(Result::unwrap)
+        .collect()
+}
+
+fn initialize_vector(size: u64) -> Vec<u64> {
+    (0..size).collect()
+}
+
+fn indices_wrapping(slice_length: usize, start: usize, length: usize) -> Vec<usize> {
+    (start..start + length).map(|i| i % slice_length).collect()
+}
+
+fn reverse_by_indices<T: Copy>(slice: &mut [T], indices: &[usize]) {
+    if indices.len() < 1 {
+        return;
+    }
+
+    let mut i = 0;
+    let mut j = indices.len() - 1;
+    while i < j {
+        let early = slice[indices[i]];
+        let late = slice[indices[j]];
+        slice[indices[i]] = late;
+        slice[indices[j]] = early;
+
+        i += 1;
+        j -= 1;
+    }
+}
+
+fn perform_knot<T: Copy>(slice: &mut [T], start: usize, length: usize) {
+    let indices = indices_wrapping(slice.len(), start, length);
+    reverse_by_indices(slice, &indices);
+}
+
+fn knot_hash<T: Copy>(slice: &mut [T], lengths: &[u8]) {
+    let mut current = 0;
+    let mut skip_size = 0;
+    let len = slice.len();
+    for &length in lengths {
+        perform_knot(slice, current, length as usize);
+        current = (current + length as usize + skip_size) % len;
+        skip_size += 1;
     }
 }
 
