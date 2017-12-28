c56d3bbe6214f64df06e669cccc7d8ef9e7a9d58
Christian Persson <saser@live.se>
2017-12-28 22:23:07 +0100

    day15: add parsing functions

--- day15/src/lib.rs
+++ day15/src/lib.rs
@@ -1,6 +1,7 @@
 extern crate base;
 
 use base::{Part, Solver};
+use std::str::FromStr;
 
 pub fn get_solver() -> Box<Solver> {
     Box::new(Day15)
@@ -12,6 +13,16 @@ impl Solver for Day15 {
     fn solve(&self, part: Part, input: &str) -> Result<String, String> {
         Err("day 15 not yet implemented".to_string())
     }
+}
+
+fn parse_input(input: &str) -> (u64, u64) {
+    let lines: Vec<&str> = input.lines().collect();
+    (parse_line(&lines[0]), parse_line(&lines[1]))
+}
+
+fn parse_line(line: &str) -> u64 {
+    let parts: Vec<&str> = line.split(' ').collect();
+    u64::from_str(parts[parts.len() - 1]).unwrap()
 }
 
 #[cfg(test)]
