de1c3a45fcf6222c84a828f589ccaed728949518
Christian Persson <saser@live.se>
2017-12-28 11:57:16 +0100

    day13 + aoc: add skeleton

---
+++ day13/src/lib.rs
@@ -0,0 +1,44 @@
+extern crate base;
+
+use base::{Part, Solver};
+
+pub fn get_solver() -> Box<Solver> {
+    Box::new(Day13)
+}
+
+struct Day13;
+
+impl Solver for Day13 {
+    fn solve(&self, part: Part, input: &str) -> Result<String, String> {
+        Err("day 13 not yet implemented".to_string())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod part1 {
+        use super::*;
+
+        #[test]
+        fn example() {
+            let solver = get_solver();
+            let input = "put some input here";
+            let expected = "expected output";
+            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
+        }
+    }
+
+    mod part2 {
+        use super::*;
+
+        #[test]
+        fn example() {
+            let solver = get_solver();
+            let input = "put some input here";
+            let expected = "expected output";
+            assert_eq!(expected, solver.solve(Part::Two, input).unwrap());
+        }
+    }
+}
