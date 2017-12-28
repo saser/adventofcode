4a7f49172b5be2216e1cebfa9e85e629de997642
Christian Persson <saser@live.se>
2017-12-28 16:44:02 +0100

    day14: implement solution for part 1

--- day14/src/lib.rs
+++ day14/src/lib.rs
@@ -11,8 +11,43 @@ struct Day14;
 
 impl Solver for Day14 {
     fn solve(&self, part: Part, input: &str) -> Result<String, String> {
-        Err("day 14 not yet implemented".to_string())
+        let strings = strings_to_hash(input);
+        match part {
+            Part::One => Ok(total_bits(&strings).to_string()),
+            Part::Two => Err("part 2 not implemented yet".to_string()),
+        }
     }
+}
+
+fn strings_to_hash(input: &str) -> Vec<String> {
+    (0..128)
+        .map(|i| format!("{}-{}", input, i))
+        .collect()
+}
+
+fn hex_digit_to_binary(digit: char) -> String {
+    format!("{:04b}", digit.to_digit(16).unwrap())
+}
+
+fn hash_to_binary(hash: &str) -> String {
+    hash.chars()
+        .map(hex_digit_to_binary)
+        .collect::<Vec<String>>()
+        .join("")
+}
+
+fn bits_in_hash(hash: &str) -> usize {
+    hash_to_binary(hash)
+        .chars()
+        .filter(|&c| c == '1')
+        .count()
+}
+
+fn total_bits(strings: &[String]) -> usize {
+    strings.iter()
+        .map(|string| day10::full_hash_str(&string))
+        .map(|hash| bits_in_hash(&hash))
+        .sum()
 }
 
 #[cfg(test)]
