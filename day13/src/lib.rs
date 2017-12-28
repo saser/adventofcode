0550d89514dfd89de90ab1cba0bd8109645bb209
Christian Persson <saser@live.se>
2017-12-28 12:00:13 +0100

    day13: add parsing code

--- day13/src/lib.rs
+++ day13/src/lib.rs
@@ -1,6 +1,8 @@
 extern crate base;
 
 use base::{Part, Solver};
+use std::collections::HashMap;
+use std::str::FromStr;
 
 pub fn get_solver() -> Box<Solver> {
     Box::new(Day13)
@@ -13,6 +15,20 @@ impl Solver for Day13 {
         Err("day 13 not yet implemented".to_string())
     }
 }
+
+fn parse_input(input: &str) -> HashMap<u64, u64> {
+    input.lines()
+        .map(parse_line)
+        .collect()
+}
+
+fn parse_line(line: &str) -> (u64, u64) {
+    let parts: Vec<&str> = line.split(": ").collect();
+    let layer = u64::from_str(parts[0]).unwrap();
+    let depth = u64::from_str(parts[1]).unwrap();
+    (layer, depth)
+}
+
 
 #[cfg(test)]
 mod tests {
