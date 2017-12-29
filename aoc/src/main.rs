70d65989078fa146fc5ff3e42c3df7c86937cb24
Christian Persson <saser@live.se>
2017-12-29 17:56:51 +0100

    day19 + aoc: add skeleton

--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -19,6 +19,7 @@ extern crate day15;
 extern crate day16;
 extern crate day17;
 extern crate day18;
+extern crate day19;
 
 use base::{Part, Solver};
 use clap::{App, Arg, ArgMatches};
@@ -136,6 +137,7 @@ fn get_solver(day: u8) -> Result<Box<Solver>, String> {
         16 => Ok(day16::get_solver()),
         17 => Ok(day17::get_solver()),
         18 => Ok(day18::get_solver()),
+        19 => Ok(day19::get_solver()),
         _ => Err(format!("no solver for day {}", day)),
     }
 }
