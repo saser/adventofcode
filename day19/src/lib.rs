cd074416c31e37440acd109ce03bfa22342992b1
Christian Persson <saser@live.se>
2018-01-09 23:33:25 +0100

    day19: add solution for part 1

--- day19/src/lib.rs
+++ day19/src/lib.rs
@@ -1,6 +1,7 @@
 extern crate base;
 
 use base::{Part, Solver};
+use base::grid::*;
 
 pub fn get_solver() -> Box<Solver> {
     Box::new(Day19)
@@ -10,7 +11,12 @@ struct Day19;
 
 impl Solver for Day19 {
     fn solve(&self, part: Part, input: &str) -> Result<String, String> {
-        Err("day 19 not yet implemented".to_string())
+        let grid = parse_input(input);
+        let traveler = TileTraveler::from(&grid);
+        match part {
+            Part::One => Ok(collect_letters(&traveler)),
+            Part::Two => Err("part 2 not yet implemented".to_string()),
+        }
     }
 }
 
@@ -22,7 +28,97 @@ fn parse_line(line: &str) -> Vec<Tile> {
     line.chars().map(|c| Tile::from(c)).collect()
 }
 
-#[derive(Clone, Copy, Eq, Hash, PartialEq)]
+fn find_starting_point(grid: &Vec<Vec<Tile>>) -> Point {
+    let (column, _) = grid[0]
+        .iter()
+        .enumerate()
+        .find(|&(_, &pipe)| pipe == Tile::Vertical)
+        .unwrap();
+    Point {
+        x: column as i64,
+        y: 0,
+    }
+}
+
+fn collect_letters(traveler: &TileTraveler) -> String {
+    traveler
+        .tiles()
+        .filter_map(|tile| {
+            if let Tile::Letter(c) = tile {
+                Some(c)
+            } else {
+                None
+            }
+        })
+        .map(|c| c.to_string())
+        .collect::<Vec<String>>()
+        .join("")
+}
+
+#[derive(Clone, Eq, PartialEq)]
+struct TileTraveler {
+    grid: Vec<Vec<Tile>>,
+    traveler: Traveler,
+}
+
+impl TileTraveler {
+    fn from(grid: &Vec<Vec<Tile>>) -> TileTraveler {
+        let grid = grid.clone();
+        let traveler = Traveler {
+            pos: find_starting_point(&grid),
+            direction: Direction::North,
+        };
+        TileTraveler { grid, traveler }
+    }
+
+    fn tiles(&self) -> Tiles {
+        Tiles {
+            tile_traveler: self.clone(),
+        }
+    }
+}
+
+struct Tiles {
+    tile_traveler: TileTraveler,
+}
+
+impl Iterator for Tiles {
+    type Item = Tile;
+
+    fn next(&mut self) -> Option<Tile> {
+        let Point { x, y } = self.tile_traveler.traveler.pos;
+        let current_tile = self.tile_traveler.grid[y as usize][x as usize];
+
+        let next_dir = if let Tile::Corner = current_tile {
+            [Turn::Clockwise, Turn::CounterClockwise]
+                .iter()
+                .map(|&turn| self.tile_traveler.traveler.direction.turn(turn))
+                .find(|dir| {
+                    let Point { x, y } = self.tile_traveler.traveler.pos + dir.as_point();
+                    self.tile_traveler.grid[y as usize][x as usize] != Tile::Empty
+                })
+        } else {
+            None
+        }.unwrap_or(self.tile_traveler.traveler.direction);
+        self.tile_traveler.traveler.direction = next_dir;
+
+        let Point {
+            x: next_x,
+            y: next_y,
+        } = self.tile_traveler.traveler.peek_step();
+
+        let next_tile = self.tile_traveler.grid[next_y as usize][next_x as usize];
+        match next_tile {
+            Tile::Empty => None,
+            _ => {
+                self.tile_traveler.traveler.step();
+                Some(next_tile)
+            }
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 enum Tile {
     Empty,
     Horizontal,
@@ -53,14 +149,7 @@ mod tests {
         #[test]
         fn example() {
             let solver = get_solver();
-            let input = "\
-     |          
-     |  +--+    
-     A  |  C    
- F---|----E|--+ 
-     |  |  |  D 
-     +B-+  +--+ \
-            ";
+            let input = "     |          \n     |  +--+    \n     A  |  C    \n F---|----E|--+ \n     |  |  |  D \n     +B-+  +--+ ";
             let expected = "ABCDEF";
             assert_eq!(expected, solver.solve(Part::One, input).unwrap());
         }
