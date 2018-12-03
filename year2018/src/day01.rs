73d1aa371d986e9172647d7b0b09d4a8e36b5125
Christian Persson <saser@live.se>
2018-12-03 14:15:32 +0100

    2018/01: add example tests for part 1

--- year2018/src/day01.rs
+++ year2018/src/day01.rs
@@ -20,10 +20,51 @@ mod tests {
         use super::*;
 
         #[test]
-        fn example() {
+        fn example_1() {
             let solver = get_solver();
-            let input = "put some input here";
-            let expected = "expected output";
+            let input = "\
++1
+-2
++3
++1\
+            ";
+            let expected = "4";
+            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
+        }
+
+        #[test]
+        fn example_2() {
+            let solver = get_solver();
+            let input = "\
++1
++1
++1\
+            ";
+            let expected = "3";
+            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
+        }
+
+        #[test]
+        fn example_3() {
+            let solver = get_solver();
+            let input = "\
++1
++1
+-2\
+            ";
+            let expected = "0";
+            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
+        }
+
+        #[test]
+        fn example_4() {
+            let solver = get_solver();
+            let input = "\
+-1
+-2
+-3\
+            ";
+            let expected = "-6";
             assert_eq!(expected, solver.solve(Part::One, input).unwrap());
         }
     }
