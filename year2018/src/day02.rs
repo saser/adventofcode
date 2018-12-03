0537cec5715813eda3c7f358c6f369aa5ada5b27
Christian Persson <saser@live.se>
2018-12-03 15:20:48 +0100

    2018/02: add example test for part 1

--- year2018/src/day02.rs
+++ year2018/src/day02.rs
@@ -22,8 +22,16 @@ mod tests {
         #[test]
         fn example() {
             let solver = get_solver();
-            let input = "put some input here";
-            let expected = "expected output";
+            let input = "\
+abcdef
+bababc
+abbcde
+abcccd
+aabcdd
+abcdee
+ababab\
+            ";
+            let expected = "12";
             assert_eq!(expected, solver.solve(Part::One, input).unwrap());
         }
     }
