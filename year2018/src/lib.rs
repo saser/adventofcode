1b264aeb8dcf0778cb9da0d5c1432ca9e165a7d3
Christian Persson <saser@live.se>
2018-12-03 15:10:14 +0100

    2018/02: add skeleton and input for day 02

--- year2018/src/lib.rs
+++ year2018/src/lib.rs
@@ -3,6 +3,7 @@ extern crate base;
 use base::{Solver, YearDispatcher};
 
 pub mod day01;
+pub mod day02;
 
 pub struct Year2018;
 
@@ -14,6 +15,7 @@ impl YearDispatcher for Year2018 {
     fn get_solver(&self, day: u8) -> Result<Box<dyn Solver>, String> {
         match day {
             01 => Ok(day01::get_solver()),
+            02 => Ok(day02::get_solver()),
             _ => Err(format!("no solver for day {}", day)),
         }
     }
