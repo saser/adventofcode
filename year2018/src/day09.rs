f81a887cc6a421471f0737981df1326dfb6149f6
Christian Persson <saser@live.se>
2018-12-09 23:49:41 +0100

    2018/09: add example tests

--- year2018/src/day09.rs
+++ year2018/src/day09.rs
@@ -31,10 +31,50 @@ mod tests {
         }
 
         #[test]
-        fn example() {
+        fn example_1() {
             let solver = get_solver();
-            let input = "put some input here";
-            let expected = "expected output";
+            let input = "9 players; last marble is worth 25 points";
+            let expected = "32";
+            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
+        }
+
+        #[test]
+        fn example_2() {
+            let solver = get_solver();
+            let input = "10 players; last marble is worth 1618 points";
+            let expected = "8317";
+            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
+        }
+
+        #[test]
+        fn example_3() {
+            let solver = get_solver();
+            let input = "13 players; last marble is worth 7999 points";
+            let expected = "146373";
+            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
+        }
+
+        #[test]
+        fn example_4() {
+            let solver = get_solver();
+            let input = "17 players; last marble is worth 1104 points";
+            let expected = "2764";
+            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
+        }
+
+        #[test]
+        fn example_5() {
+            let solver = get_solver();
+            let input = "21 players; last marble is worth 6111 points";
+            let expected = "54718";
+            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
+        }
+
+        #[test]
+        fn example_6() {
+            let solver = get_solver();
+            let input = "30 players; last marble is worth 5807 points";
+            let expected = "37305";
             assert_eq!(expected, solver.solve(Part::One, input).unwrap());
         }
     }
