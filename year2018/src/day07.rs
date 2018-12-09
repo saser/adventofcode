b270ed133b9ba6607a94434f5d5d26a70bb92ee4
Christian Persson <saser@live.se>
2018-12-09 22:27:26 +0100

    2018/07: implement solution for part 2

--- year2018/src/day07.rs
+++ year2018/src/day07.rs
@@ -37,9 +37,10 @@ struct Day07;
 impl Solver for Day07 {
     fn solve(&self, part: Part, input: &str) -> Result<String, String> {
         let (dependencies, dependants) = parse_input(input);
+        let workers = 5;
         match part {
-            Part::One => Ok(run_until_completion(&dependencies, &dependants)),
-            Part::Two => Err("day 07 part 2 not yet implemented".to_string()),
+            Part::One => Ok(determine_order(&dependencies, &dependants)),
+            Part::Two => Ok(seconds_with_workers(workers, &dependencies, &dependants).to_string()),
         }
     }
 }
@@ -76,8 +77,8 @@ fn parse_instruction(instruction: &str) -> (char, char) {
     (dependency, dependant)
 }
 
-fn run_until_completion(dependencies: &Dependencies, dependants: &Dependants) -> String {
-    let mut s = String::new();
+fn determine_order(dependencies: &Dependencies, dependants: &Dependants) -> String {
+    let mut order = String::new();
     let mut done = HashSet::new();
     let mut available = (&dependants.keys().cloned().collect::<HashSet<char>>()
         - &dependencies.keys().cloned().collect::<HashSet<char>>())
@@ -92,7 +93,7 @@ fn run_until_completion(dependencies: &Dependencies, dependants: &Dependants) ->
         if done.contains(&next_step.0) {
             continue;
         }
-        s.push(next_step.0);
+        order.push(next_step.0);
         done.insert(next_step.0);
         for (&c, local_dependencies) in dependencies.iter() {
             if local_dependencies.is_subset(&done) && !done.contains(&c) {
@@ -100,7 +101,63 @@ fn run_until_completion(dependencies: &Dependencies, dependants: &Dependants) ->
             }
         }
     }
-    s
+    order
+}
+
+fn seconds_with_workers(workers: u64, dependencies: &Dependencies, dependants: &Dependants) -> u64 {
+    let dependencies_keys = dependencies.keys().cloned().collect::<HashSet<char>>();
+    let dependants_keys = dependants.keys().cloned().collect::<HashSet<char>>();
+    let all_steps = &dependencies_keys | &dependants_keys;
+
+    let mut done = HashSet::new();
+    let mut current_time = 0;
+    let initially_available = (&dependants_keys - &dependencies_keys)
+        .iter()
+        .cloned()
+        .collect::<Vec<char>>();
+    let mut events = initially_available
+        .iter()
+        .map(|&c| (duration(c), c))
+        .collect::<Vec<(u64, char)>>();
+    events.sort();
+    let mut available = Vec::new();
+    let mut available_workers = workers - events.len() as u64;
+
+    while done.len() < all_steps.len() {
+        let (new_time, completed_step) = events.remove(0);
+        current_time = new_time;
+        done.insert(completed_step);
+        available_workers += 1;
+
+        if dependants.contains_key(&completed_step) {
+            let newly_available = dependants[&completed_step]
+                .iter()
+                .cloned()
+                .filter(|&dependant| {
+                    is_available(dependant, dependencies, &done) && !available.contains(&dependant)
+                })
+                .collect::<Vec<char>>();
+            available.extend(newly_available);
+
+            available.sort();
+        }
+
+        while available.len() > 0 && available_workers > 0 {
+            let next_step = available.remove(0);
+            available_workers -= 1;
+            events.push((current_time + duration(next_step), next_step));
+        }
+        events.sort();
+    }
+    current_time
+}
+
+fn duration(c: char) -> u64 {
+    (c as u64) - 4
+}
+
+fn is_available(c: char, dependencies: &Dependencies, done: &HashSet<char>) -> bool {
+    !dependencies.contains_key(&c) || !done.contains(&c) && dependencies[&c].is_subset(done)
 }
 
 #[cfg(test)]
