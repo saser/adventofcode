1315c960ba657aec1be784cf969b03ca21615e0a
Christian Persson <saser@live.se>
2018-12-09 19:14:10 +0100

    2018/07: add example test for part 1

--- year2018/src/day07.rs
+++ year2018/src/day07.rs
@@ -33,8 +33,16 @@ mod tests {
         #[test]
         fn example() {
             let solver = get_solver();
-            let input = "put some input here";
-            let expected = "expected output";
+            let input = "\
+Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.\
+                ";
+            let expected = "CABDFE";
             assert_eq!(expected, solver.solve(Part::One, input).unwrap());
         }
     }
