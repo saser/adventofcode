8e5a23e8cfeaff4edc44f9019fe237b8c76b9dae
Christian Persson <saser@live.se>
2018-12-10 10:00:38 +0100

    2018/10: implement parsing

--- year2018/src/day10.rs
+++ year2018/src/day10.rs
@@ -1,3 +1,10 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+
+use std::num::ParseIntError;
+use std::str::FromStr;
+
+use base::grid::Point;
 use base::{Part, Solver};
 
 pub fn get_solver() -> Box<dyn Solver> {
@@ -13,6 +20,41 @@ impl Solver for Day10 {
             Part::Two => Err("day 10 part 2 not yet implemented".to_string()),
         }
     }
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+struct Star {
+    position: Point,
+    velocity: Point,
+}
+
+impl FromStr for Star {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        lazy_static! {
+            static ref STAR_RE: Regex = Regex::new(
+                r"position=< *(?P<x>-?\d+), *(?P<y>-?\d+)> velocity=< *(?P<dx>-?\d+), *(?P<dy>-?\d+)>"
+            )
+            .unwrap();
+        }
+        let captures = STAR_RE.captures(s).unwrap();
+        let x = i64::from_str(&captures["x"])?;
+        let y = i64::from_str(&captures["y"])?;
+        let dx = i64::from_str(&captures["dx"])?;
+        let dy = i64::from_str(&captures["dy"])?;
+        let position = Point { x, y };
+        let velocity = Point { x: dx, y: dy };
+        Ok(Star { position, velocity })
+    }
+}
+
+fn parse_input(input: &str) -> Vec<Star> {
+    input
+        .lines()
+        .map(Star::from_str)
+        .map(Result::unwrap)
+        .collect()
 }
 
 #[cfg(test)]
