32e4b702ac9cbcb955f89097a6125a1d8da0157c
Christian Persson <saser@live.se>
2018-12-11 14:29:53 +0100

    2018/11: improve on part 2 even further

--- year2018/src/day11.rs
+++ year2018/src/day11.rs
@@ -1,10 +1,10 @@
-use nalgebra::{DMatrix, MatrixMN};
-// use rayon::prelude::*;
-use typenum::U300;
+use nalgebra::DMatrix;
 
 use base::{Part, Solver};
 
-type PowerGrid = MatrixMN<i64, U300, U300>;
+type PowerGrid = DMatrix<i64>;
+type RowSumGrid = DMatrix<i64>;
+type ColSumGrid = DMatrix<i64>;
 type StencilGrid = DMatrix<(usize, i64)>;
 
 pub fn get_solver() -> Box<dyn Solver> {
@@ -16,7 +16,7 @@ struct Day11;
 impl Solver for Day11 {
     fn solve(&self, part: Part, input: &str) -> Result<String, String> {
         let serial = input.parse::<i64>().unwrap();
-        let power_grid = PowerGrid::from_fn(power_level(serial));
+        let power_grid = PowerGrid::from_fn(300, 300, power_level(serial));
         match part {
             Part::One => {
                 let stencil_grid = stencil_grid(&power_grid, 3);
@@ -24,8 +24,8 @@ impl Solver for Day11 {
                 Ok(format!("{},{}", x, y))
             }
             Part::Two => {
-                let stencil_grid = max_stencil_grid(&power_grid);
-                let (x, y, (size, _value)) = max_stencil_xy(&stencil_grid);
+                let max_stencil_grid = max_stencil_grid(&power_grid);
+                let (x, y, (size, _value)) = max_stencil_xy(&max_stencil_grid);
                 Ok(format!("{},{},{}", x, y, size))
             }
         }
@@ -41,7 +41,6 @@ fn ij_to_xy((i, j): (usize, usize)) -> (usize, usize) {
 
 fn power_level(serial: i64) -> impl Fn(usize, usize) -> i64 {
     move |i, j| {
-        // `x` denotes column, and thus depends on `j`. Likewise, `y` denotes row, and thus depends on `i`.
         let (x, y) = ij_to_xy((i, j));
         let x = x as i64;
         let y = y as i64;
@@ -56,25 +55,49 @@ fn power_level(serial: i64) -> impl Fn(usize, usize) -> i64 {
     }
 }
 
+fn sum_grid(power_grid: &PowerGrid) -> (RowSumGrid, ColSumGrid) {
+    let (nrows, ncols) = power_grid.shape();
+    let mut row_sum_grid = RowSumGrid::zeros(nrows, ncols);
+    let mut col_sum_grid = ColSumGrid::zeros(nrows, ncols);
+    for i in (0..nrows).rev() {
+        for j in (0..ncols).rev() {
+            let value = power_grid[(i, j)];
+            let below = (i + 1).min(nrows - 1);
+            let right = (j + 1).min(ncols - 1);
+            // println!("below: {}", below);
+            // println!("right: {}", right);
+            let below_sum = col_sum_grid[(below, j)];
+            let right_sum = row_sum_grid[(i, right)];
+            col_sum_grid[(i, j)] = value + below_sum;
+            row_sum_grid[(i, j)] = value + right_sum;
+        }
+    }
+    (row_sum_grid, col_sum_grid)
+}
+
 fn all_stencils_up_to(
     power_grid: &PowerGrid,
+    row_sum_grid: &RowSumGrid,
+    col_sum_grid: &ColSumGrid,
     (i, j): (usize, usize),
     max_size: usize,
 ) -> Vec<(usize, i64)> {
-    let mut stencils: Vec<(usize, i64)> = Vec::with_capacity(max_size);
+    let mut stencils = Vec::with_capacity(max_size);
     let mut previous_stencil = 0;
     for size in 1..=max_size {
-        let row_start = i + size - 1;
-        let row_length = size - 1;
-        let row = power_grid.slice((row_start, j), (1, row_length));
+        let row = i + size - 1;
+        let col = j + size - 1;
+        let corner_pos = (row, col);
 
-        let col_start = j + size - 1;
-        let col_length = size;
-        let col = power_grid.slice((i, col_start), (col_length, 1));
+        let row_sum_start = (row, j);
+        let row_sum = row_sum_grid[row_sum_start] - row_sum_grid[corner_pos];
 
-        let row_sum = row.iter().sum::<i64>();
-        let col_sum = col.iter().sum::<i64>();
-        let stencil = previous_stencil + row_sum + col_sum;
+        let col_sum_start = (i, col);
+        let col_sum = col_sum_grid[col_sum_start] - col_sum_grid[corner_pos];
+
+        let corner = power_grid[corner_pos];
+        let stencil = previous_stencil + row_sum + col_sum + corner;
+
         stencils.push((size, stencil));
         previous_stencil = stencil;
     }
@@ -83,19 +106,27 @@ fn all_stencils_up_to(
 
 fn max_stencil_up_to(
     power_grid: &PowerGrid,
+    row_sum_grid: &RowSumGrid,
+    col_sum_grid: &ColSumGrid,
     (i, j): (usize, usize),
     max_size: usize,
 ) -> (usize, i64) {
-    all_stencils_up_to(power_grid, (i, j), max_size)
+    // let (row_sum_grid, col_sum_grid) = sum_grid(power_grid);
+    all_stencils_up_to(power_grid, &row_sum_grid, &col_sum_grid, (i, j), max_size)
         .into_iter()
         .max_by_key(|&(_size, value)| value)
         .unwrap()
 }
 
-fn max_stencil(power_grid: &PowerGrid, (i, j): (usize, usize)) -> (usize, i64) {
+fn max_stencil(
+    power_grid: &PowerGrid,
+    row_sum_grid: &RowSumGrid,
+    col_sum_grid: &ColSumGrid,
+    (i, j): (usize, usize),
+) -> (usize, i64) {
     let size = power_grid.nrows();
     let max_size = size - i.max(j);
-    max_stencil_up_to(power_grid, (i, j), max_size)
+    max_stencil_up_to(power_grid, row_sum_grid, col_sum_grid, (i, j), max_size)
 }
 
 fn stencil_grid(power_grid: &PowerGrid, size: usize) -> StencilGrid {
@@ -107,9 +138,12 @@ fn stencil_grid(power_grid: &PowerGrid, size: usize) -> StencilGrid {
     StencilGrid::from_fn(matrix_size, matrix_size, stencil)
 }
 
-fn max_stencil_grid(power_grid: &PowerGrid) -> DMatrix<(usize, i64)> {
+fn max_stencil_grid(power_grid: &PowerGrid) -> StencilGrid {
     let (nrows, ncols) = power_grid.shape();
-    DMatrix::from_fn(nrows, ncols, |i, j| max_stencil(power_grid, (i, j)))
+    let (row_sum_grid, col_sum_grid) = sum_grid(power_grid);
+    StencilGrid::from_fn(nrows, ncols, |i, j| {
+        max_stencil(power_grid, &row_sum_grid, &col_sum_grid, (i, j))
+    })
 }
 
 fn max_stencil_xy(stencil_grid: &StencilGrid) -> (usize, usize, (usize, i64)) {
