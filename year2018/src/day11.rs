2896ba25504f5ecefd34e810b7d9255ab0c2d916
Christian Persson <saser@live.se>
2018-12-11 12:53:00 +0100

    2018/11: improve solution for part 2

--- year2018/src/day11.rs
+++ year2018/src/day11.rs
@@ -1,11 +1,11 @@
 use nalgebra::{DMatrix, MatrixMN};
-use rayon::prelude::*;
+// use rayon::prelude::*;
 use typenum::U300;
 
 use base::{Part, Solver};
 
 type PowerGrid = MatrixMN<i64, U300, U300>;
-type StencilGrid = DMatrix<i64>;
+type StencilGrid = DMatrix<(usize, i64)>;
 
 pub fn get_solver() -> Box<dyn Solver> {
     Box::new(Day11)
@@ -20,19 +20,12 @@ impl Solver for Day11 {
         match part {
             Part::One => {
                 let stencil_grid = stencil_grid(&power_grid, 3);
-                let (x, y, _value) = max_stencil_xy(&stencil_grid);
+                let (x, y, (_size, _value)) = max_stencil_xy(&stencil_grid);
                 Ok(format!("{},{}", x, y))
             }
             Part::Two => {
-                let (x, y, size, _value) = (1..300usize + 1)
-                    .into_par_iter()
-                    .map(|size| {
-                        let stencil_grid = stencil_grid(&power_grid, size);
-                        let (x, y, value) = max_stencil_xy(&stencil_grid);
-                        (x, y, size, value)
-                    })
-                    .max_by_key(|&(_x, _y, _size, value)| value)
-                    .unwrap();
+                let stencil_grid = max_stencil_grid(&power_grid);
+                let (x, y, (size, _value)) = max_stencil_xy(&stencil_grid);
                 Ok(format!("{},{},{}", x, y, size))
             }
         }
@@ -63,28 +56,79 @@ fn power_level(serial: i64) -> impl Fn(usize, usize) -> i64 {
     }
 }
 
+fn all_stencils_up_to(
+    power_grid: &PowerGrid,
+    (i, j): (usize, usize),
+    max_size: usize,
+) -> Vec<(usize, i64)> {
+    let mut stencils: Vec<(usize, i64)> = Vec::with_capacity(max_size);
+    let mut previous_stencil = 0;
+    for size in 1..=max_size {
+        let row_start = i + size - 1;
+        let row_length = size - 1;
+        let row = power_grid.slice((row_start, j), (1, row_length));
+
+        let col_start = j + size - 1;
+        let col_length = size;
+        let col = power_grid.slice((i, col_start), (col_length, 1));
+
+        let row_sum = row.iter().sum::<i64>();
+        let col_sum = col.iter().sum::<i64>();
+        let stencil = previous_stencil + row_sum + col_sum;
+        stencils.push((size, stencil));
+        previous_stencil = stencil;
+    }
+    stencils
+}
+
+fn max_stencil_up_to(
+    power_grid: &PowerGrid,
+    (i, j): (usize, usize),
+    max_size: usize,
+) -> (usize, i64) {
+    all_stencils_up_to(power_grid, (i, j), max_size)
+        .into_iter()
+        .max_by_key(|&(_size, value)| value)
+        .unwrap()
+}
+
+fn max_stencil(power_grid: &PowerGrid, (i, j): (usize, usize)) -> (usize, i64) {
+    let size = power_grid.nrows();
+    let max_size = size - i.max(j);
+    max_stencil_up_to(power_grid, (i, j), max_size)
+}
+
 fn stencil_grid(power_grid: &PowerGrid, size: usize) -> StencilGrid {
     let matrix_size = 300 - size + 1;
-    let stencil = |i, j| power_grid.slice((i, j), (size, size)).iter().sum();
+    let stencil = |i, j| {
+        let sum = power_grid.slice((i, j), (size, size)).iter().sum();
+        (size, sum)
+    };
     StencilGrid::from_fn(matrix_size, matrix_size, stencil)
 }
 
-fn max_stencil_xy(stencil_grid: &StencilGrid) -> (usize, usize, i64) {
+fn max_stencil_grid(power_grid: &PowerGrid) -> DMatrix<(usize, i64)> {
+    let (nrows, ncols) = power_grid.shape();
+    DMatrix::from_fn(nrows, ncols, |i, j| max_stencil(power_grid, (i, j)))
+}
+
+fn max_stencil_xy(stencil_grid: &StencilGrid) -> (usize, usize, (usize, i64)) {
     let (nrows, ncols) = stencil_grid.shape();
     let (mut max_i, mut max_j) = (0, 0);
-    let mut max = 0;
+    let (mut max_size, mut max_value) = (0, 0);
     for i in 0..nrows {
         for j in 0..ncols {
-            let stencil = stencil_grid[(i, j)];
-            if stencil_grid[(i, j)] > max {
-                max = stencil;
+            let (size, value) = stencil_grid[(i, j)];
+            if value > max_value {
+                max_size = size;
+                max_value = value;
                 max_i = i;
                 max_j = j;
             }
         }
     }
     let (x, y) = ij_to_xy((max_i, max_j));
-    (x, y, max)
+    (x, y, (max_size, max_value))
 }
 
 #[cfg(test)]
