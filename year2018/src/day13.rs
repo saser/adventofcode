4c1ef0fe17842b9cdbb30f7e954744a1cab1a12c
Christian Persson <saser@live.se>
2018-12-13 12:50:22 +0100

    2018/13: add function to parse input

--- year2018/src/day13.rs
+++ year2018/src/day13.rs
@@ -1,3 +1,6 @@
+use std::collections::BTreeSet;
+
+use base::grid::Grid;
 use base::{Part, Solver};
 
 pub fn get_solver() -> Box<dyn Solver> {
@@ -5,6 +8,9 @@ pub fn get_solver() -> Box<dyn Solver> {
 }
 
 struct Day13;
+
+type Tiles = Grid<Tile>;
+type Carts = BTreeSet<Cart>;
 
 impl Solver for Day13 {
     fn solve(&self, part: Part, input: &str) -> Result<String, String> {
@@ -65,10 +71,53 @@ impl From<char> for Direction {
 }
 
 #[derive(Copy, Clone, Debug, Hash, Eq, Ord, PartialEq, PartialOrd)]
+enum Turn {
+    Left,
+    Straight,
+    Right,
+}
+
+impl Turn {
+    fn next(&self) -> Self {
+        match *self {
+            Turn::Left => Turn::Straight,
+            Turn::Straight => Turn::Right,
+            Turn::Right => Turn::Left,
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, Hash, Eq, Ord, PartialEq, PartialOrd)]
 struct Cart {
     row: usize,
     col: usize,
     dir: Direction,
+    turn: Turn,
+}
+
+fn parse_input(input: &str) -> (Tiles, Carts) {
+    let char_grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    let nrows = char_grid.len();
+    let ncols = char_grid.iter().map(|row| row.len()).max().unwrap();
+    let mut tiles = Grid::new(nrows, ncols);
+    let mut carts = BTreeSet::new();
+    for row in 0..nrows {
+        for col in 0..ncols {
+            let c = char_grid[row][col];
+            if ['^', '>', 'v', '<'].contains(&c) {
+                let dir = Direction::from(c);
+                carts.insert(Cart {
+                    row,
+                    col,
+                    dir,
+                    turn: Turn::Left,
+                });
+            } else {
+                tiles[(row, col)] = Tile::from(char_grid[row][col]);
+            }
+        }
+    }
+    (tiles, carts)
 }
 
 #[cfg(test)]
