6d99ddb4831d1103f7d00527c5df17b9be66b6eb
Christian Persson <saser@live.se>
2018-12-14 16:29:29 +0100

    2018/14: implement solution for part 1

--- year2018/src/day14.rs
+++ year2018/src/day14.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use base::{Part, Solver};
 
 pub fn get_solver() -> Box<dyn Solver> {
@@ -8,11 +10,56 @@ struct Day14;
 
 impl Solver for Day14 {
     fn solve(&self, part: Part, input: &str) -> Result<String, String> {
+        let made_recipes = parse_input(input);
+        let scores = generate_scores(made_recipes + 10);
         match part {
-            Part::One => Err("day 14 part 1 not yet implemented".to_string()),
+            Part::One => {
+                let following_ten = scores.iter().skip(made_recipes).take(10);
+                let s = following_ten
+                    .map(|score| score.to_string())
+                    .collect::<String>();
+                Ok(s)
+            }
             Part::Two => Err("day 14 part 2 not yet implemented".to_string()),
         }
     }
+}
+
+fn parse_input(input: &str) -> usize {
+    usize::from_str(input).unwrap()
+}
+
+fn print_scores(scores: &[usize], indices: &[usize]) {
+    for (i, score) in scores.iter().enumerate() {
+        let surround = if i == indices[0] {
+            ('(', ')')
+        } else if i == indices[1] {
+            ('[', ']')
+        } else {
+            (' ', ' ')
+        };
+        print!("{}{}{}", surround.0, score, surround.1);
+    }
+    println!();
+}
+
+fn generate_scores(nr_recipes: usize) -> Vec<usize> {
+    let indices = &mut [0, 1];
+    let mut scores = Vec::with_capacity(nr_recipes);
+    scores.push(3);
+    scores.push(7);
+    while scores.len() < nr_recipes {
+        let sum = indices.iter().map(|&idx| scores[idx]).sum::<usize>();
+        if sum >= 10 {
+            scores.push(1);
+        }
+        scores.push(sum % 10);
+        for idx in indices.iter_mut() {
+            *idx += 1 + scores[*idx];
+            *idx %= scores.len();
+        }
+    }
+    scores
 }
 
 #[cfg(test)]
@@ -26,7 +73,7 @@ mod tests {
         fn with_input() {
             let solver = get_solver();
             let input = include_str!("../../inputs/2018/14").trim();
-            let expected = "expected output";
+            let expected = "5371393113";
             assert_eq!(expected, solver.solve(Part::One, input).unwrap());
         }
 
