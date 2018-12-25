cd9e466cdb72416e9bb0eccf4a892d69793e92c5
Christian Persson <saser@live.se>
2018-12-25 12:04:17 +0100

    2018/18: add code to parse the input

--- year2018/src/day18.rs
+++ year2018/src/day18.rs
@@ -1,3 +1,7 @@
+use nalgebra::DMatrix;
+
+use std::fmt;
+
 use base::{Part, Solver};
 
 pub fn get_solver() -> Box<dyn Solver> {
@@ -6,13 +10,55 @@ pub fn get_solver() -> Box<dyn Solver> {
 
 struct Day18;
 
+type Tiles = DMatrix<Tile>;
+
 impl Solver for Day18 {
     fn solve(&self, part: Part, input: &str) -> Result<String, String> {
+        let tiles = parse_input(input);
+        println!("{}", tiles);
         match part {
             Part::One => Err("day 18 part 1 not yet implemented".to_string()),
             Part::Two => Err("day 18 part 2 not yet implemented".to_string()),
         }
     }
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+enum Tile {
+    Open,
+    Tree,
+    Lumberyard,
+}
+
+impl fmt::Display for Tile {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let c = match *self {
+            Tile::Open => '.',
+            Tile::Tree => '|',
+            Tile::Lumberyard => '#',
+        };
+        write!(f, "{}", c)
+    }
+}
+
+impl From<char> for Tile {
+    fn from(c: char) -> Self {
+        match c {
+            '|' => Tile::Tree,
+            '#' => Tile::Lumberyard,
+            _ => Tile::Open,
+        }
+    }
+}
+
+fn parse_input(input: &str) -> Tiles {
+    let chars = input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+    let nrows = chars.len();
+    let ncols = chars[0].len();
+    Tiles::from_fn(nrows, ncols, |row, col| Tile::from(chars[row][col]))
 }
 
 #[cfg(test)]
