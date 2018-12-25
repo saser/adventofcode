47605761da8c15e3a9b714608a74e54e5072ffb7
Christian Persson <saser@live.se>
2018-12-25 01:45:43 +0100

    2018/17: add code to parse and print

--- year2018/src/day17.rs
+++ year2018/src/day17.rs
@@ -1,3 +1,9 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+
+use std::collections::BTreeSet;
+use std::iter;
+
 use base::{Part, Solver};
 
 pub fn get_solver() -> Box<dyn Solver> {
@@ -8,10 +14,73 @@ struct Day17;
 
 impl Solver for Day17 {
     fn solve(&self, part: Part, input: &str) -> Result<String, String> {
+        let (grid, adjusted_spring) = parse_input(input);
+        print(&grid);
         match part {
             Part::One => Err("day 17 part 1 not yet implemented".to_string()),
             Part::Two => Err("day 17 part 2 not yet implemented".to_string()),
         }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct Position {
+    row: usize,
+    col: usize,
+}
+
+fn parse_input(input: &str) -> (Vec<Vec<char>>, Position) {
+    let clay = input
+        .lines()
+        .fold(BTreeSet::new(), |acc, line| &acc | &parse_line(line));
+    let spring = Position { row: 0, col: 500 };
+    let with_spring = clay.iter().chain(iter::once(&spring));
+    let clay_rows = with_spring.clone().map(|position| position.row);
+    let min_row = clay_rows.clone().min().unwrap();
+    let max_row = clay_rows.clone().max().unwrap();
+    let clay_cols = with_spring.clone().map(|position| position.col);
+    let min_col = clay_cols.clone().min().unwrap();
+    let max_col = clay_cols.clone().max().unwrap();
+    let nrows = max_row - min_row + 1;
+    let ncols = max_col - min_col + 1;
+    let mut grid = vec![vec!['.'; ncols]; nrows];
+    for &position in &clay {
+        grid[position.row - min_row][position.col - min_col] = '#';
+    }
+    let adjusted_spring = Position {
+        row: spring.row - min_row,
+        col: spring.col - min_col,
+    };
+    grid[adjusted_spring.row][adjusted_spring.col] = '+';
+    (grid, adjusted_spring)
+}
+
+fn parse_line(line: &str) -> BTreeSet<Position> {
+    lazy_static! {
+        static ref VEIN_RE: Regex = Regex::new(
+            r"(?P<n1>x|y)=(?P<c1>\d+), (?P<n2>x|y)=(?P<c2_start>\d+)\.\.(?P<c2_end>\d+)"
+        )
+        .unwrap();
+    }
+    let captures = VEIN_RE.captures(line).unwrap();
+    let c1 = captures["c1"].parse::<usize>().unwrap();
+    let c2_start = captures["c2_start"].parse::<usize>().unwrap();
+    let c2_end = captures["c2_end"].parse::<usize>().unwrap();
+    let x_first = &captures["n1"] == "x";
+    (c2_start..=c2_end)
+        .map(|c2| {
+            let (x, y) = if x_first { (c1, c2) } else { (c2, c1) };
+            Position { col: x, row: y }
+        })
+        .collect()
+}
+
+fn print(grid: &Vec<Vec<char>>) {
+    for row in grid {
+        for c in row {
+            print!("{}", c);
+        }
+        println!();
     }
 }
 
