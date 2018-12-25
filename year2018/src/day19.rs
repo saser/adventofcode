1bc6100a3f9ca6837ba09ded551a21b9ef31c2b5
Christian Persson <saser@live.se>
2018-12-25 14:47:48 +0100

    2018/19: add example test

--- year2018/src/day19.rs
+++ year2018/src/day19.rs
@@ -94,8 +94,17 @@ mod tests {
         #[test]
         fn example() {
             let solver = get_solver();
-            let input = "put some input here";
-            let expected = "expected output";
+            let input = "\
+#ip 0
+seti 5 0 1
+seti 6 0 2
+addi 0 1 0
+addr 1 2 3
+setr 1 0 0
+seti 8 0 4
+seti 9 0 5\
+            ";
+            let expected = "6";
             assert_eq!(expected, solver.solve(Part::One, input).unwrap());
         }
     }
