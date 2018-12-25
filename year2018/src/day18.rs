38e050fc6dffd3c715925197ddba1ff537b49969
Christian Persson <saser@live.se>
2018-12-25 12:25:04 +0100

    2018/18: add function to get surrounding tiles

--- year2018/src/day18.rs
+++ year2018/src/day18.rs
@@ -1,5 +1,6 @@
 use nalgebra::DMatrix;
 
+use std::cmp::{max, min};
 use std::fmt;
 
 use base::{Part, Solver};
@@ -15,7 +16,7 @@ type Tiles = DMatrix<Tile>;
 impl Solver for Day18 {
     fn solve(&self, part: Part, input: &str) -> Result<String, String> {
         let tiles = parse_input(input);
-        println!("{}", tiles);
+        println!("{:?}", surrounding(0, 0, &tiles));
         match part {
             Part::One => Err("day 18 part 1 not yet implemented".to_string()),
             Part::Two => Err("day 18 part 2 not yet implemented".to_string()),
@@ -59,6 +60,30 @@ fn parse_input(input: &str) -> Tiles {
     let nrows = chars.len();
     let ncols = chars[0].len();
     Tiles::from_fn(nrows, ncols, |row, col| Tile::from(chars[row][col]))
+}
+
+fn surrounding(row: usize, col: usize, tiles: &Tiles) -> Vec<Tile> {
+    let start_row = max(0, row as isize - 1) as usize;
+    let start_col = max(0, col as isize - 1) as usize;
+    let end_row = min((tiles.nrows() - 1) as isize, (row + 1) as isize) as usize;
+    let end_col = min((tiles.ncols() - 1) as isize, (col + 1) as isize) as usize;
+    let nrows = end_row - start_row + 1;
+    let ncols = end_col - start_col + 1;
+    let start = (start_row, start_col);
+    let shape = (nrows, ncols);
+
+    let mut surrounding = tiles
+        .slice(start, shape)
+        .iter()
+        .cloned()
+        .collect::<Vec<Tile>>();
+    let middle_tile = tiles[(row, col)];
+    let middle_tile_index = surrounding
+        .iter()
+        .position(|&tile| tile == middle_tile)
+        .unwrap();
+    surrounding.remove(middle_tile_index);
+    surrounding
 }
 
 #[cfg(test)]
