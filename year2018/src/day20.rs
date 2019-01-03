bbc72bfa097aeb67a81f8b8d7e3ae9ed031dbcd0
Christian Persson <saser@live.se>
2019-01-03 14:15:22 +0100

    2018/20: parse regex into recursive datastructure

--- year2018/src/day20.rs
+++ year2018/src/day20.rs
@@ -8,11 +8,88 @@ struct Day20;
 
 impl Solver for Day20 {
     fn solve(&self, part: Part, input: &str) -> Result<String, String> {
+        let regex = parse(input);
+        println!("{:?}", regex);
         match part {
             Part::One => Err("day 20 part 1 not yet implemented".to_string()),
             Part::Two => Err("day 20 part 2 not yet implemented".to_string()),
         }
     }
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+struct Regex {
+    tokens: Vec<Token>,
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+enum Token {
+    Terminals(Vec<char>),
+    Branch(Vec<Regex>),
+}
+
+fn parse(input: &str) -> Regex {
+    let regex_chars = input.chars().collect::<Vec<char>>();
+    let (regex, _offset) = parse_regex(&regex_chars);
+    regex
+}
+
+fn parse_regex(regex_chars: &[char]) -> (Regex, usize) {
+    let mut tokens = Vec::new();
+    let mut i = 0;
+    while i < regex_chars.len() {
+        match regex_chars[i] {
+            '^' | '$' => i += 1,
+            '|' | ')' => break,
+            '(' => {
+                let (regexes, offset) = parse_branch(&regex_chars[i..]);
+                tokens.push(Token::Branch(regexes));
+                i += offset;
+            }
+            t if is_terminal(t) => {
+                let (terminals, offset) = parse_terminals(&regex_chars[i..]);
+                tokens.push(Token::Terminals(terminals));
+                i += offset;
+            }
+            c => panic!("parse_regex: unexpected char: {}", c),
+        };
+    }
+    (Regex { tokens }, i)
+}
+
+fn parse_terminals(regex_chars: &[char]) -> (Vec<char>, usize) {
+    let terminals = regex_chars
+        .iter()
+        .cloned()
+        .take_while(|&c| is_terminal(c))
+        .collect::<Vec<char>>();
+    let offset = terminals.len();
+    (terminals, offset)
+}
+
+fn parse_branch(regex_chars: &[char]) -> (Vec<Regex>, usize) {
+    let mut regexes = Vec::new();
+    let mut i = 0;
+    while i < regex_chars.len() {
+        match regex_chars[i] {
+            '(' | '|' => {
+                i += 1;
+                let (regex, offset) = parse_regex(&regex_chars[i..]);
+                regexes.push(regex);
+                i += offset;
+            }
+            ')' => {
+                i += 1;
+                break;
+            }
+            c => panic!("parse_branch: unexpected char: {}", c),
+        };
+    }
+    (regexes, i)
+}
+
+fn is_terminal(c: char) -> bool {
+    ['N', 'E', 'S', 'W'].contains(&c)
 }
 
 #[cfg(test)]
