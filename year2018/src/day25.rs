9f68fad332ede766f89d0602fe5530b3f7c8f5c4
Christian Persson <saser@live.se>
2019-01-11 11:38:23 +0100

    2018/25: implement solution for part 1

--- year2018/src/day25.rs
+++ year2018/src/day25.rs
@@ -1,3 +1,4 @@
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::str::FromStr;
 
 use base::{Part, Solver};
@@ -11,12 +12,16 @@ struct Day25;
 impl Solver for Day25 {
     fn solve(&self, part: Part, input: &str) -> Result<String, String> {
         let points = parse_input(input);
+        let graph = build_graph(&points);
+        let constellations = build_constellations(&graph);
         match part {
-            Part::One => Err("day 25 part 1 not yet implemented".to_string()),
+            Part::One => Ok(constellations.len().to_string()),
             Part::Two => Err("day 25 part 2 not yet implemented".to_string()),
         }
     }
 }
+
+type Graph = HashMap<Point4D, HashSet<Point4D>>;
 
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 struct Point4D {
@@ -56,6 +61,46 @@ fn parse_input(input: &str) -> Vec<Point4D> {
         .collect()
 }
 
+fn build_graph(points: &[Point4D]) -> Graph {
+    let n = points.len();
+    let mut graph = Graph::new();
+    for i in 0..n {
+        for j in i..n {
+            let p_i = points[i];
+            let p_j = points[j];
+            let distance = p_i.manhattan_distance_to(p_j);
+            if distance <= 3 {
+                graph.entry(p_i).or_insert_with(HashSet::new).insert(p_j);
+                graph.entry(p_j).or_insert_with(HashSet::new).insert(p_i);
+            }
+        }
+    }
+    graph
+}
+
+fn build_constellations(graph: &Graph) -> Vec<HashSet<Point4D>> {
+    let mut constellations = Vec::new();
+    let mut visited = HashSet::new();
+    for &start_point in graph.keys() {
+        if visited.contains(&start_point) {
+            continue;
+        }
+        let mut constellation = HashSet::new();
+        let mut queue = VecDeque::new();
+        queue.push_back(start_point);
+        while let Some(point) = queue.pop_front() {
+            if constellation.contains(&point) {
+                continue;
+            }
+            constellation.insert(point);
+            visited.insert(point);
+            queue.extend(graph[&point].clone());
+        }
+        constellations.push(constellation);
+    }
+    constellations
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -67,7 +112,7 @@ mod tests {
         fn with_input() {
             let solver = get_solver();
             let input = include_str!("../../inputs/2018/25").trim();
-            let expected = "expected output";
+            let expected = "381";
             assert_eq!(expected, solver.solve(Part::One, input).unwrap());
         }
 
