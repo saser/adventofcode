a4c797be23a7fa5e1bbc040d241b7ac9c131fe46
Christian Persson <Saser@users.noreply.github.com>
2019-10-31 13:30:00 +0100

    cmd/aoc: parse day, verify that solution exists for day in year
    This needs to be further split into solutions for specific parts,
    however.

--- go/cmd/aoc/main.go
+++ go/cmd/aoc/main.go
@@ -8,9 +8,13 @@ import (
 
 var (
 	fYear = flag.Uint("year", 0, "year of event")
+	fDay  = flag.Uint("day", 0, "day in event")
 )
 
-var okYears = map[uint]struct{}{}
+type Years map[uint]Days
+type Days map[uint]struct{}
+
+var okYears Years
 
 func imain() int {
 	flag.Parse()
@@ -20,8 +24,19 @@ func imain() int {
 		fmt.Println("A year must be specified.")
 		return 1
 	}
-	if _, ok := okYears[year]; !ok {
+	okDays, ok := okYears[year]
+	if !ok {
 		fmt.Printf("Invalid year: %d\n", *fYear)
+		return 1
+	}
+
+	day := *fDay
+	if day == 0 {
+		fmt.Println("A day must be specified.")
+		return 1
+	}
+	if _, ok := okDays[day]; !ok {
+		fmt.Printf("Year %d has no solution for day %d.\n", year, day)
 		return 1
 	}
 
