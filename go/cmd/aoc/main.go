423cf0bcfefb085ec2a7a4b15e8a2deceb0fbd98
Christian Persson <Saser@users.noreply.github.com>
2019-10-31 13:28:52 +0100

    cmd/aoc: parse year
    I will begin with the 2015 event, but until there exists at least one
    solution for a day in 2015, there are no "ok years" yet.

--- go/cmd/aoc/main.go
+++ go/cmd/aoc/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var (
+	fYear = flag.Uint("year", 0, "year of event")
+)
+
+var okYears = map[uint]struct{}{}
+
+func imain() int {
+	flag.Parse()
+
+	year := *fYear
+	if year == 0 {
+		fmt.Println("A year must be specified.")
+		return 1
+	}
+	if _, ok := okYears[year]; !ok {
+		fmt.Printf("Invalid year: %d\n", *fYear)
+		return 1
+	}
+
+	return 0
+}
 
 func main() {
-	fmt.Println("Hello, Advent of Code!")
+	os.Exit(imain())
 }
