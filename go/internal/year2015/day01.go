8aacd5672cb8b50fadcda0acf995612c32e05d23
Christian Persson <Saser@users.noreply.github.com>
2019-10-31 14:09:45 +0100

    go/2015/01: add solution for part 1 that passes tests

--- go/internal/year2015/day01.go
+++ go/internal/year2015/day01.go
@@ -1,10 +1,25 @@
 package year2015
 
 import (
-	"errors"
+	"bufio"
+	"fmt"
 	"io"
 )
 
 func One(r io.Reader) (string, error) {
-	return "", errors.New("year 2015, day 01, part 1: not yet implemented")
+	sc := bufio.NewScanner(r)
+	sc.Split(bufio.ScanRunes)
+	floor := 0
+	for sc.Scan() {
+		tok := sc.Text()
+		switch tok {
+		case "(":
+			floor++
+		case ")":
+			floor--
+		default:
+			return "", fmt.Errorf("year 2015, day 01, part 1: invalid token: %s", tok)
+		}
+	}
+	return fmt.Sprint(floor), nil
 }
