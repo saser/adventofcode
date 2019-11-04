4db685fe1def9a6dce75ac31b3f2af7469532f52
Christian Persson <Saser@users.noreply.github.com>
2019-11-04 16:28:31 +0100

    go/2015/02: add solution to part 1
    This highlights an issue that might become more of a problem later on:
    since all files are part of the same package, type names may
    interfere. What if there is another problem where I want to have a
    struct named `box`, that is completely different from the `box` from
    day 02? For now, I am ignoring this issue, and will take rename stuff
    later if it is needed.

--- go/internal/year2015/day02.go
+++ go/internal/year2015/day02.go
@@ -1,10 +1,60 @@
 package year2015
 
 import (
-	"errors"
+	"bufio"
+	"fmt"
 	"io"
+	"strconv"
+	"strings"
 )
 
 func Day02One(r io.Reader) (string, error) {
-	return "", errors.New("not yet implemented")
+	boxes, err := parseDay02(r)
+	if err != nil {
+		return "", fmt.Errorf("year 2015, day 02, part 1: %w", err)
+	}
+	requiredPaper := 0
+	for _, box := range boxes {
+		smallestSide := -1
+		for _, side := range []int{
+			box.l * box.w,
+			box.w * box.h,
+			box.h * box.l,
+		} {
+			if smallestSide == -1 || side < smallestSide {
+				smallestSide = side
+			}
+			requiredPaper += side * 2
+		}
+		requiredPaper += smallestSide
+	}
+	return fmt.Sprint(requiredPaper), nil
+}
+
+type day02Box struct {
+	l, w, h int
+}
+
+func parseDay02(r io.Reader) ([]day02Box, error) {
+	sc := bufio.NewScanner(r)
+	sc.Split(bufio.ScanLines)
+	boxes := make([]day02Box, 0)
+	for sc.Scan() {
+		line := sc.Text()
+		parts := strings.Split(line, "x")
+		l, err := strconv.Atoi(parts[0])
+		if err != nil {
+			return nil, fmt.Errorf("parse: %w", err)
+		}
+		w, err := strconv.Atoi(parts[1])
+		if err != nil {
+			return nil, fmt.Errorf("parse: %w", err)
+		}
+		h, err := strconv.Atoi(parts[2])
+		if err != nil {
+			return nil, fmt.Errorf("parse: %w", err)
+		}
+		boxes = append(boxes, day02Box{l: l, w: w, h: h})
+	}
+	return boxes, nil
 }
