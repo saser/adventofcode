d93aceb119a700710a06506041374790d56273e9
Christian Persson <Saser@users.noreply.github.com>
2019-11-04 16:05:23 +0100

    go/2015/02: add placeholders and tests for part 1
    The test for the actual input is included as well, since I have
    previously solved this puzzle, so the correct answer is already known
    to me.

---
+++ go/internal/year2015/day02_test.go
@@ -0,0 +1,26 @@
+package year2015
+
+import (
+	"testing"
+
+	"github.com/Saser/adventofcode/internal/testcase"
+)
+
+func TestDay02(t *testing.T) {
+	t.Run("part1", func(t *testing.T) {
+		for _, tc := range []testcase.TestCase{
+			testcase.FromString("example1", "2x3x4", "58"),
+			testcase.FromString("example2", "1x1x10", "43"),
+			testcase.FromInputFile(t, 2015, 2, "1586300"),
+		} {
+			testcase.Run(t, tc, Day02One)
+		}
+	})
+}
+
+func BenchmarkDay02(b *testing.B) {
+	tc := testcase.FromInputFile(b, 2015, 2, "")
+	b.Run("part1", func(b *testing.B) {
+		testcase.Bench(b, tc, Day01One)
+	})
+}
