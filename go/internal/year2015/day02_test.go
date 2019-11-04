7bc572ee0b08b9ca4ac35c0cf037a5100fdaf4d7
Christian Persson <Saser@users.noreply.github.com>
2019-11-04 16:36:48 +0100

    go/2015/02: add placeholder function and tests for part 2

--- go/internal/year2015/day02_test.go
+++ go/internal/year2015/day02_test.go
@@ -16,11 +16,23 @@ func TestDay02(t *testing.T) {
 			testcase.Run(t, tc, Day02One)
 		}
 	})
+	t.Run("part2", func(t *testing.T) {
+		for _, tc := range []testcase.TestCase{
+			testcase.FromString("example1", "2x3x4", "34"),
+			testcase.FromString("example2", "1x1x10", "14"),
+			testcase.FromInputFile(t, 2015, 2, "3737498"),
+		} {
+			testcase.Run(t, tc, Day02Two)
+		}
+	})
 }
 
 func BenchmarkDay02(b *testing.B) {
 	tc := testcase.FromInputFile(b, 2015, 2, "")
 	b.Run("part1", func(b *testing.B) {
 		testcase.Bench(b, tc, Day01One)
+	})
+	b.Run("part2", func(b *testing.B) {
+		testcase.Bench(b, tc, Day01Two)
 	})
 }
