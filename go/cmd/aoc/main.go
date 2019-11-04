4ccfcf4eab275defac6db8e8da21441791742db4
Christian Persson <Saser@users.noreply.github.com>
2019-11-04 18:01:30 +0100

    go/cmd/aoc: add year 2015, day 04, part 1 to runner

--- go/cmd/aoc/main.go
+++ go/cmd/aoc/main.go
@@ -27,6 +27,7 @@ var solutions = map[uint]map[uint]Day{
 		1: {One: year2015.Day01One, Two: year2015.Day01Two},
 		2: {One: year2015.Day02One, Two: year2015.Day02Two},
 		3: {One: year2015.Day03One, Two: year2015.Day03Two},
+		4: {One: year2015.Day04One},
 	},
 }
 
