4f8f170472306e095e1fe49496b1c76fc5e49345
Christian Persson <Saser@users.noreply.github.com>
2019-11-05 17:32:52 +0100

    go/2015/07: add solution, tests, and benchmark for part 2

--- go/internal/year2015/day07/day07_test.go
+++ go/internal/year2015/day07/day07_test.go
@@ -31,3 +31,16 @@ func BenchmarkPart1(b *testing.B) {
 	tc := testcase.FromFile(b, inputFile, "")
 	testcase.Bench(b, tc, Part1)
 }
+
+func TestPart2(t *testing.T) {
+	for _, tc := range []testcase.TestCase{
+		testcase.FromFile(t, "../testdata/07", "14710"),
+	} {
+		testcase.Run(t, tc, Part2)
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	tc := testcase.FromFile(b, inputFile, "")
+	testcase.Bench(b, tc, Part2)
+}
