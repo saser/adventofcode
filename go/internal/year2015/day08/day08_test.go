af39cbd4c1a73848e494b70d4085b9234da27be7
Christian Persson <Saser@users.noreply.github.com>
2019-11-05 17:48:31 +0100

    go/2015/08: add placeholder function, tests, and benchmark (part 1)

---
+++ go/internal/year2015/day08/day08_test.go
@@ -0,0 +1,25 @@
+package day08
+
+import (
+	"testing"
+
+	"github.com/Saser/adventofcode/internal/testcase"
+)
+
+const inputFile = "../testdata/08"
+
+func TestPart1(t *testing.T) {
+	for _, tc := range []testcase.TestCase{
+		testcase.FromString("example1", `""`, "2"),
+		testcase.FromString("example2", `"abc"`, "2"),
+		testcase.FromString("example3", `"aaa\"aaa"`, "3"),
+		testcase.FromString("example4", `"\x27"`, "5"),
+	} {
+		testcase.Run(t, tc, Part1)
+	}
+}
+
+func BenchmarkPart1(b *testing.B) {
+	tc := testcase.FromFile(b, inputFile, "")
+	testcase.Bench(b, tc, Part1)
+}
