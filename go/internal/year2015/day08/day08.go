210e4fa032a69e23c17a6917e1140fbe8a329ffc
Christian Persson <Saser@users.noreply.github.com>
2019-11-06 11:40:40 +0100

    go/2015/08: refactor `Part1`/`Part2` to use common `solve` function

--- go/internal/year2015/day08/day08.go
+++ go/internal/year2015/day08/day08.go
@@ -8,20 +8,31 @@ import (
 )
 
 func Part1(r io.Reader) (string, error) {
+	return solve(r, 1)
+}
+
+func Part2(r io.Reader) (string, error) {
+	return solve(r, 2)
+}
+
+func solve(r io.Reader, part int) (string, error) {
 	sc := bufio.NewScanner(r)
 	sc.Split(bufio.ScanLines)
 	s := 0
+	var diff func(string) int
+	switch part {
+	case 1:
+		diff = diffMemory
+	case 2:
+		return "", errors.New("not yet implemented")
+	}
 	for sc.Scan() {
 		s += diff(sc.Text())
 	}
 	return fmt.Sprint(s), nil
 }
 
-func Part2(r io.Reader) (string, error) {
-	return "", errors.New("not yet implemented")
-}
-
-func diff(s string) int {
+func diffMemory(s string) int {
 	memory := 0
 	skip := 0
 	for i := 1; i < len(s)-1; i++ {
