3dc56905ee6ea978276b692955089069d2a475b1
Christian Persson <Saser@users.noreply.github.com>
2019-11-07 11:28:04 +0100

    go/2015/10: add solution for part 1

--- go/internal/year2015/day10/day10.go
+++ go/internal/year2015/day10/day10.go
@@ -1,10 +1,46 @@
 package day10
 
 import (
-	"errors"
+	"bufio"
+	"fmt"
 	"io"
+	"strings"
 )
 
 func Part1(r io.Reader) (string, error) {
-	return "", errors.New("not yet implemented")
+	br := bufio.NewReader(r)
+	line, err := br.ReadString('\n')
+	if err != nil {
+		return "", fmt.Errorf("year 2015, day 10, part 1: %w", err)
+	}
+	answer := strings.TrimSpace(line)
+	for i := 0; i < 40; i++ {
+		answer = lookAndSay(answer)
+	}
+	return fmt.Sprint(len(answer)), nil
+}
+
+func lookAndSay(s string) string {
+	current := rune(s[0])
+	count := 1
+	runes := make([]rune, 0)
+	counts := make([]int, 0)
+	for _, r := range s[1:] {
+		if r == current {
+			count++
+		} else {
+			runes = append(runes, current)
+			counts = append(counts, count)
+			current = r
+			count = 1
+		}
+	}
+	runes = append(runes, current)
+	counts = append(counts, count)
+	var sb strings.Builder
+	for i := 0; i < len(runes); i++ {
+		sb.WriteString(fmt.Sprint(counts[i]))
+		sb.WriteRune(runes[i])
+	}
+	return sb.String()
 }
