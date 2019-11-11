59f4d22ea612bf8e8d7782002ee0af34820ef529
Christian Persson <Saser@users.noreply.github.com>
2019-11-11 21:23:18 +0100

    go/2015/11: add tests for the individual requirements

--- go/internal/year2015/day11/day11.go
+++ go/internal/year2015/day11/day11.go
@@ -8,3 +8,15 @@ import (
 func Part1(r io.Reader) (string, error) {
 	return "", errors.New("not yet implemented")
 }
+
+func hasIncreasing(s string) bool {
+	return false
+}
+
+func hasNoIOL(s string) bool {
+	return false
+}
+
+func hasTwoPairs(s string) bool {
+	return false
+}
--- go/internal/year2015/day11/day11_test.go
+++ go/internal/year2015/day11/day11_test.go
@@ -1,12 +1,65 @@
 package day11
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/Saser/adventofcode/internal/testcase"
+	"github.com/stretchr/testify/require"
 )
 
 const inputFile = "../testdata/11"
+
+func Test_hasIncreasing(t *testing.T) {
+	for _, tt := range []struct {
+		s string
+		b bool
+	}{
+		{s: "hijklmmn", b: true},
+		{s: "abbceffg", b: false},
+		{s: "abbcegjk", b: false},
+		{s: "abcdffaa", b: true},
+		{s: "ghjaabcc", b: true},
+	} {
+		t.Run(fmt.Sprintf("s=%v", tt.s), func(t *testing.T) {
+			require.Equal(t, tt.b, hasIncreasing(tt.s))
+		})
+	}
+}
+
+func Test_hasNoIOL(t *testing.T) {
+	for _, tt := range []struct {
+		s string
+		b bool
+	}{
+		{s: "hijklmmn", b: false},
+		{s: "abbceffg", b: true},
+		{s: "abbcegjk", b: true},
+		{s: "abcdffaa", b: true},
+		{s: "ghjaabcc", b: true},
+	} {
+		t.Run(fmt.Sprintf("s=%v", tt.s), func(t *testing.T) {
+			require.Equal(t, tt.b, hasNoIOL(tt.s))
+		})
+	}
+}
+
+func Test_hasTwoPairs(t *testing.T) {
+	for _, tt := range []struct {
+		s string
+		b bool
+	}{
+		{s: "hijklmmn", b: false},
+		{s: "abbceffg", b: true},
+		{s: "abbcegjk", b: false},
+		{s: "abcdffaa", b: true},
+		{s: "ghjaabcc", b: true},
+	} {
+		t.Run(fmt.Sprintf("s=%v", tt.s), func(t *testing.T) {
+			require.Equal(t, tt.b, hasTwoPairs(tt.s))
+		})
+	}
+}
 
 func TestPart1(t *testing.T) {
 	for _, tc := range []testcase.TestCase{
