2dcea39f13248c9237d519dd5f5777a07e65fd69
Christian Persson <Saser@users.noreply.github.com>
2019-11-11 21:27:01 +0100

    go/2015/11: implement requirement functions to pass tests

--- go/internal/year2015/day11/day11.go
+++ go/internal/year2015/day11/day11.go
@@ -10,13 +10,35 @@ func Part1(r io.Reader) (string, error) {
 }
 
 func hasIncreasing(s string) bool {
+	for i := 0; i < len(s)-2; i++ {
+		r := s[i]
+		if s[i+1] == r+1 && s[i+2] == r+2 {
+			return true
+		}
+	}
 	return false
 }
 
 func hasNoIOL(s string) bool {
-	return false
+	for _, r := range s {
+		switch r {
+		case 'i', 'o', 'l':
+			return false
+		}
+	}
+	return true
 }
 
 func hasTwoPairs(s string) bool {
+	pairs := 0
+	for i := 0; i < len(s)-1; i++ {
+		if s[i] == s[i+1] {
+			pairs++
+			i++
+		}
+		if pairs == 2 {
+			return true
+		}
+	}
 	return false
 }
