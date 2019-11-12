6d3dd73a6a6e5fe56497edabe04e768448fdb86b
Christian Persson <Saser@users.noreply.github.com>
2019-11-12 14:19:15 +0100

    go/2015/12: add solution to part 2
    The solutions to parts 1 and 2 differ quite heavily; they will be
    refactored shortly.

--- go/internal/year2015/day12/day12.go
+++ go/internal/year2015/day12/day12.go
@@ -2,9 +2,9 @@ package day12
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 )
 
 func Part1(r io.Reader) (string, error) {
@@ -23,5 +23,38 @@ func Part1(r io.Reader) (string, error) {
 }
 
 func Part2(r io.Reader) (string, error) {
-	return "", errors.New("not yet implemented")
+	var j interface{}
+	bytes, err := ioutil.ReadAll(r)
+	if err != nil {
+		return "", fmt.Errorf("year 2015, day 12, part 2: %w", err)
+	}
+	if err := json.Unmarshal(bytes, &j); err != nil {
+		return "", fmt.Errorf("year 2015, day 12, part 2: %w", err)
+	}
+	return fmt.Sprint(sumJSON(j, 2)), nil
+}
+
+func sumJSON(j interface{}, part int) int {
+	switch v := j.(type) {
+	case float64:
+		return int(v)
+	case string:
+		return 0
+	case []interface{}:
+		sum := 0
+		for _, k := range v {
+			sum += sumJSON(k, part)
+		}
+		return sum
+	case map[string]interface{}:
+		sum := 0
+		for _, k := range v {
+			if s, ok := k.(string); ok && part == 2 && s == "red" {
+				return 0
+			}
+			sum += sumJSON(k, part)
+		}
+		return sum
+	}
+	panic(fmt.Sprintf("invalid JSON value: %v (%T)", j, j))
 }
