f6390e2d3829a505311370a9380389fe88deb697
Christian Persson <Saser@users.noreply.github.com>
2019-11-13 17:25:13 +0100

    go/2015/13: add function to parse preferences, with simple tests

--- go/internal/year2015/day13/day13.go
+++ go/internal/year2015/day13/day13.go
@@ -2,9 +2,47 @@ package day13
 
 import (
 	"errors"
+	"fmt"
 	"io"
+	"regexp"
+	"strconv"
 )
 
 func Part1(r io.Reader) (string, error) {
 	return "", errors.New("not yet implemented")
+}
+
+type preference struct {
+	from   string
+	to     string
+	change int
+}
+
+func parsePreference(s string) (preference, error) {
+	re, err := regexp.Compile(`^(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+)\.$`)
+	if err != nil {
+		return preference{}, fmt.Errorf("parse preference: %w", err)
+	}
+	matches := re.FindStringSubmatch(s)
+	if matches == nil {
+		return preference{}, fmt.Errorf("parse preference: invalid preference string: %s", s)
+	}
+	var sign int
+	switch matches[2] {
+	case "gain":
+		sign = 1
+	case "lose":
+		sign = -1
+	}
+	change, err := strconv.Atoi(matches[3])
+	if err != nil {
+		return preference{}, fmt.Errorf("parse preference: %w", err)
+	}
+	change *= sign
+	p := preference{
+		from:   matches[1],
+		to:     matches[4],
+		change: change,
+	}
+	return p, nil
 }
--- go/internal/year2015/day13/day13_test.go
+++ go/internal/year2015/day13/day13_test.go
@@ -4,9 +4,43 @@ import (
 	"testing"
 
 	"github.com/Saser/adventofcode/internal/testcase"
+	"github.com/stretchr/testify/require"
 )
 
 const inputFile = "../testdata/13"
+
+func Test_parsePreference(t *testing.T) {
+	for _, tt := range []struct {
+		name string
+		s    string
+		p    preference
+	}{
+		{
+			name: "AliceBobGain54",
+			s:    "Alice would gain 54 happiness units by sitting next to Bob.",
+			p: preference{
+				from:   "Alice",
+				to:     "Bob",
+				change: 54,
+			},
+		},
+		{
+			name: "AliceCarolLose79",
+			s:    "Alice would gain 79 happiness units by sitting next to Carol.",
+			p: preference{
+				from:   "Alice",
+				to:     "Carol",
+				change: 79,
+			},
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			p, err := parsePreference(tt.s)
+			require.NoError(t, err)
+			require.Equal(t, tt.p, p)
+		})
+	}
+}
 
 func TestPart1(t *testing.T) {
 	for _, tc := range []testcase.TestCase{
