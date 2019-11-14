7032ba1a77d6d80419160ccf4da2b9197423052e
Christian Persson <Saser@users.noreply.github.com>
2019-11-14 23:32:31 +0100

    go/2015/15: add function to calculate integer distributions
    This is basically "stars and bars", with non-negative
    integers.

--- go/internal/year2015/day15/day15.go
+++ go/internal/year2015/day15/day15.go
@@ -8,3 +8,16 @@ import (
 func Part1(r io.Reader) (string, error) {
 	return "", errors.New("not yet implemented")
 }
+
+func distributions(sum int, parts int) [][]int {
+	if parts == 1 {
+		return [][]int{{sum}}
+	}
+	ds := make([][]int, 0)
+	for i := 0; i <= sum; i++ {
+		for _, sub := range distributions(sum-i, parts-1) {
+			ds = append(ds, append([]int{i}, sub...))
+		}
+	}
+	return ds
+}
