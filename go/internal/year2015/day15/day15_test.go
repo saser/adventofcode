4b39bc4d8b3cb2e1a57947a67086252fefaea9a5
Christian Persson <Saser@users.noreply.github.com>
2019-11-18 20:20:19 +0100

    go/2015/15: add solution for part 2

--- go/internal/year2015/day15/day15_test.go
+++ go/internal/year2015/day15/day15_test.go
@@ -28,6 +28,7 @@ func BenchmarkPart1(b *testing.B) {
 func TestPart2(t *testing.T) {
 	for _, tc := range []testcase.TestCase{
 		testcase.FromFile(t, exampleFile, "57600000"),
+		testcase.FromFile(t, inputFile, "11171160"),
 	} {
 		testcase.Run(t, tc, Part2)
 	}
