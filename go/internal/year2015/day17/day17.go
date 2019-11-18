fae53387ff57856bf66e3f0d3bd1073026569de3
Christian Persson <Saser@users.noreply.github.com>
2019-11-18 21:16:50 +0100

    go/2015/17: add function to parse input as a multiset of ints
    The multiset is represented as a slice in this case, but the actual
    ordering does not matter, only indexing (i.e. we only care whether the
    element at index `i` is included in the sum or not, but not whether
    the element at index `i` comes before or after the element at index
    `j`).

--- go/internal/year2015/day17/day17.go
+++ go/internal/year2015/day17/day17.go
@@ -1,10 +1,32 @@
 package day17
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
 	"io"
+	"strconv"
 )
 
 func Part1(r io.Reader) (string, error) {
+	parts, err := parse(r)
+	if err != nil {
+		return "", fmt.Errorf("year 2015, day 17, part 1: %w", err)
+	}
+	fmt.Println(parts)
 	return "", errors.New("not implemented yet")
+}
+
+func parse(r io.Reader) ([]int, error) {
+	sc := bufio.NewScanner(r)
+	sc.Split(bufio.ScanLines)
+	parts := make([]int, 0)
+	for sc.Scan() {
+		part, err := strconv.Atoi(sc.Text())
+		if err != nil {
+			return nil, fmt.Errorf("parse: invalid line: %s", sc.Text())
+		}
+		parts = append(parts, part)
+	}
+	return parts, nil
 }
