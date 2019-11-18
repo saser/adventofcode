961f9ed21db4f5437594599e20666ba3e942a2d4
Christian Persson <Saser@users.noreply.github.com>
2019-11-18 20:26:55 +0100

    go/2015/16: add placeholder, test, and benchmark for part 1
    It should be noted that I do not have anything to actually test with
    yet. As such, the only test case will always fail so far.

---
+++ go/internal/year2015/day16/day16_test.go
@@ -0,0 +1,19 @@
+package day16
+
+import (
+	"testing"
+
+	"github.com/Saser/adventofcode/internal/testcase"
+)
+
+const inputFile = "../testdata/16"
+
+func TestPart1(t *testing.T) {
+	tc := testcase.FromFile(t, inputFile, "")
+	testcase.Run(t, tc, Part1)
+}
+
+func BenchmarkPart1(b *testing.B) {
+	tc := testcase.FromFile(b, inputFile, "")
+	testcase.Bench(b, tc, Part1)
+}
