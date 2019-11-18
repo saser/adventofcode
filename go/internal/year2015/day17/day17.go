5cee76628739b42f23d244d24dcf36e6088b5602
Christian Persson <Saser@users.noreply.github.com>
2019-11-18 21:58:48 +0100

    go/2015/17: add solution to part 1
    This one is a little different, since the target differs between the
    example and the actual puzzle.

--- go/internal/year2015/day17/day17.go
+++ go/internal/year2015/day17/day17.go
@@ -2,19 +2,21 @@ package day17
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"io"
 	"strconv"
+
+	"github.com/Saser/adventofcode/internal/solution"
 )
 
-func Part1(r io.Reader) (string, error) {
-	parts, err := parse(r)
-	if err != nil {
-		return "", fmt.Errorf("year 2015, day 17, part 1: %w", err)
+func Part1(target int) solution.Solution {
+	return func(r io.Reader) (string, error) {
+		parts, err := parse(r)
+		if err != nil {
+			return "", fmt.Errorf("year 2015, day 17, part 1: %w", err)
+		}
+		return fmt.Sprint(sumCombinations(target, parts)), nil
 	}
-	fmt.Println(parts)
-	return "", errors.New("not implemented yet")
 }
 
 func parse(r io.Reader) ([]int, error) {
@@ -29,4 +31,24 @@ func parse(r io.Reader) ([]int, error) {
 		parts = append(parts, part)
 	}
 	return parts, nil
+}
+
+func sumCombinations(target int, parts []int) int {
+	if target < 0 {
+		return 0
+	}
+	var rest []int
+	switch len(parts) {
+	case 0:
+		if target == 0 {
+			return 1
+		} else {
+			return 0
+		}
+	case 1:
+		rest = []int{}
+	default:
+		rest = parts[1:]
+	}
+	return sumCombinations(target-parts[0], rest) + sumCombinations(target, rest)
 }
--- go/internal/year2015/day17/day17_test.go
+++ go/internal/year2015/day17/day17_test.go
@@ -3,24 +3,30 @@ package day17
 import (
 	"testing"
 
+	"github.com/Saser/adventofcode/internal/solution"
 	"github.com/Saser/adventofcode/internal/testcase"
 )
 
 const (
-	exampleFile = "testdata/example"
-	inputFile   = "../testdata/17"
+	exampleFile   = "testdata/example"
+	exampleTarget = 25
+	inputFile     = "../testdata/17"
+	inputTarget   = 150
 )
 
 func TestPart1(t *testing.T) {
-	for _, tc := range []testcase.TestCase{
-		testcase.FromFile(t, exampleFile, "4"),
-		testcase.FromFile(t, inputFile, ""),
+	for _, tt := range []struct {
+		tc  testcase.TestCase
+		sol solution.Solution
+	}{
+		{tc: testcase.FromFile(t, exampleFile, "4"), sol: Part1(exampleTarget)},
+		{tc: testcase.FromFile(t, inputFile, "1304"), sol: Part1(inputTarget)},
 	} {
-		testcase.Run(t, tc, Part1)
+		testcase.Run(t, tt.tc, tt.sol)
 	}
 }
 
 func BenchmarkPart1(b *testing.B) {
 	tc := testcase.FromFile(b, inputFile, "")
-	testcase.Bench(b, tc, Part1)
+	testcase.Bench(b, tc, Part1(inputTarget))
 }
