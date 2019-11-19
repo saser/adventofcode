07cb98b76e2915e6ccd0587d73bb461693a7dc18
Christian Persson <Saser@users.noreply.github.com>
2019-11-19 16:04:57 +0100

    go/2015/19: add placeholder, tests, and benchmark for part 2

--- go/internal/year2015/day19/day19_test.go
+++ go/internal/year2015/day19/day19_test.go
@@ -7,15 +7,17 @@ import (
 )
 
 const (
-	exampleFile1 = "testdata/example1"
-	exampleFile2 = "testdata/example2"
-	inputFile    = "../testdata/19"
+	part1Example1File = "testdata/part1example1"
+	part1Example2File = "testdata/part1example2"
+	part2Example1File = "testdata/part2example1"
+	part2Example2File = "testdata/part2example2"
+	inputFile         = "../testdata/19"
 )
 
 func TestPart1(t *testing.T) {
 	for _, tc := range []testcase.TestCase{
-		testcase.FromFile(t, exampleFile1, "4"),
-		testcase.FromFile(t, exampleFile2, "7"),
+		testcase.FromFile(t, part1Example1File, "4"),
+		testcase.FromFile(t, part1Example2File, "7"),
 		testcase.FromFile(t, inputFile, "518"),
 	} {
 		testcase.Run(t, tc, Part1)
@@ -25,4 +27,19 @@ func TestPart1(t *testing.T) {
 func BenchmarkPart1(b *testing.B) {
 	tc := testcase.FromFile(b, inputFile, "")
 	testcase.Bench(b, tc, Part1)
+}
+
+func TestPart2(t *testing.T) {
+	for _, tc := range []testcase.TestCase{
+		testcase.FromFile(t, part2Example1File, "3"),
+		testcase.FromFile(t, part2Example2File, "6"),
+		testcase.FromFile(t, inputFile, ""),
+	} {
+		testcase.Run(t, tc, Part2)
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	tc := testcase.FromFile(b, inputFile, "")
+	testcase.Bench(b, tc, Part2)
 }
