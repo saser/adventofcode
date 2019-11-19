c9276b14c753faba05ef2df2131e862f75e25953
Christian Persson <Saser@users.noreply.github.com>
2019-11-19 10:44:17 +0100

    go/2015/18: add placeholder, tests, and benchmark for part 1

---
+++ go/internal/year2015/day18/day18.go
@@ -0,0 +1,19 @@
+package day18
+
+import (
+	"errors"
+	"io"
+
+	"github.com/Saser/adventofcode/internal/solution"
+)
+
+const (
+	Iterations = 100
+	GridSize   = 100
+)
+
+func Part1(iterations int, gridSize int) solution.Solution {
+	return func(r io.Reader) (string, error) {
+		return "", errors.New("not implemented yet")
+	}
+}
