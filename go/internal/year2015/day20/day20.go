98833ed7905d7177b0979935e8e36a40a31428ec
Christian Persson <Saser@users.noreply.github.com>
2019-11-20 12:30:27 +0100

    go/2015/20: add solution for part 1

--- go/internal/year2015/day20/day20.go
+++ go/internal/year2015/day20/day20.go
@@ -5,19 +5,45 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"math"
 	"strconv"
 )
 
 func Part1(r io.Reader) (string, error) {
-	br := bufio.NewReader(r)
-	line, err := br.ReadString('\n')
-	if err != nil && err != io.EOF {
-		return "", fmt.Errorf("year 2015, day 20, part 1: %w", err)
+	sc := bufio.NewScanner(r)
+	sc.Split(bufio.ScanLines)
+	if ok := sc.Scan(); !ok {
+		return "", fmt.Errorf("year 2015, day 20, part 1: %w", sc.Err())
 	}
+	line := sc.Text()
 	target, err := strconv.Atoi(line)
 	if err != nil {
 		return "", fmt.Errorf("year 2015, day 20, part 1: %w", err)
 	}
-	fmt.Println(target)
+	target /= 10
+	for i := 1; i <= target; i++ {
+		s := sumOfDivisors(i)
+		if s >= target {
+			return fmt.Sprint(i), nil
+		}
+	}
 	return "", errors.New("not implemented yet")
+}
+
+func sumOfDivisors(n int) int {
+	if n == 1 {
+		return 1
+	}
+	sum := 1 + n
+	limit := int(math.Sqrt(float64(n)))
+	for i := 2; i < limit; i++ {
+		if n%i == 0 {
+			sum += i
+			sum += n / i
+		}
+	}
+	if n%limit == 0 {
+		sum += limit
+	}
+	return sum
 }
