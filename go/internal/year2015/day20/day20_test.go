266dd96211a06f15173faf2e7519b2461af8ecf8
Christian Persson <Saser@users.noreply.github.com>
2019-11-20 12:44:04 +0100

    go/2015/20: add placeholder, tests, and benchmark for part 2

--- go/internal/year2015/day20/day20_test.go
+++ go/internal/year2015/day20/day20_test.go
@@ -22,3 +22,18 @@ func BenchmarkPart1(b *testing.B) {
 	tc := testcase.FromFile(b, inputFile, "")
 	testcase.Bench(b, tc, Part1)
 }
+
+func TestPart2(t *testing.T) {
+	for _, tc := range []testcase.TestCase{
+		testcase.FromString("example1", "10", "1"),
+		testcase.FromString("example2", "70", "4"),
+		testcase.FromFile(t, inputFile, ""),
+	} {
+		testcase.Run(t, tc, Part2)
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	tc := testcase.FromFile(b, inputFile, "")
+	testcase.Bench(b, tc, Part2)
+}
