e874a454d2a330255a664ff58d6c346800c1a7b6
Christian Persson <Saser@users.noreply.github.com>
2019-11-20 15:13:55 +0100

    go/2015/20: add solution for part 2

--- go/internal/year2015/day20/day20.go
+++ go/internal/year2015/day20/day20.go
@@ -2,39 +2,66 @@ package day20
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"io"
 	"strconv"
 )
 
 func Part1(r io.Reader) (string, error) {
+	return solve(r, 1)
+}
+
+func Part2(r io.Reader) (string, error) {
+	return solve(r, 2)
+}
+
+func solve(r io.Reader, part int) (string, error) {
 	sc := bufio.NewScanner(r)
 	sc.Split(bufio.ScanLines)
 	if ok := sc.Scan(); !ok {
-		return "", fmt.Errorf("year 2015, day 20, part 1: %w", sc.Err())
+		return "", fmt.Errorf("year 2015, day 20, part %d: %w", part, sc.Err())
 	}
 	line := sc.Text()
 	target, err := strconv.Atoi(line)
 	if err != nil {
-		return "", fmt.Errorf("year 2015, day 20, part 1: %w", err)
+		return "", fmt.Errorf("year 2015, day 20, part %d: %w", part, err)
 	}
-	answer, ok := firstHouse(target)
+	var presents, maxHouses int
+	switch part {
+	case 1:
+		presents = 10
+		maxHouses = 0
+	case 2:
+		presents = 11
+		maxHouses = 50
+	}
+	answer, ok := firstHouse(target, presents, maxHouses)
 	if !ok {
-		return "", errors.New("year 2015, day 20, part 1: no solution found")
+		return "", fmt.Errorf("year 2015, day 20, part %d: no solution found", part)
 	}
 	return fmt.Sprint(answer), nil
 }
 
-func Part2(r io.Reader) (string, error) {
-	return "", errors.New("not implemented yet")
-}
-
-func firstHouse(target int) (int, bool) {
+func firstHouse(target int, presents int, maxHouses int) (int, bool) {
 	houses := make([]int, target+1)
-	for i := 1; i <= target/10; i++ {
-		for j := i; j <= target/10; j += i {
-			houses[j] += i * 10
+	limit := target / presents
+	if limit == 0 {
+		limit = 1
+	}
+	housesLeft := make([]int, limit+1)
+	for i, _ := range housesLeft {
+		housesLeft[i] = maxHouses
+	}
+outer:
+	for i := 1; i <= limit; i++ {
+		for j := i; j <= limit; j += i {
+			houses[j] += i * presents
+			if maxHouses != 0 {
+				housesLeft[i]--
+				if housesLeft[i] == 0 {
+					continue outer
+				}
+			}
 		}
 	}
 	for i, house := range houses {
