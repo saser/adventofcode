33249a808d97f81e90813a166a36ec86db6c22ab
Christian Persson <Saser@users.noreply.github.com>
2019-11-20 11:12:04 +0100

    go/2015/20: add code to parse input

--- go/internal/year2015/day20/day20.go
+++ go/internal/year2015/day20/day20.go
@@ -1,10 +1,23 @@
 package day20
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
 	"io"
+	"strconv"
 )
 
 func Part1(r io.Reader) (string, error) {
+	br := bufio.NewReader(r)
+	line, err := br.ReadString('\n')
+	if err != nil && err != io.EOF {
+		return "", fmt.Errorf("year 2015, day 20, part 1: %w", err)
+	}
+	target, err := strconv.Atoi(line)
+	if err != nil {
+		return "", fmt.Errorf("year 2015, day 20, part 1: %w", err)
+	}
+	fmt.Println(target)
 	return "", errors.New("not implemented yet")
 }
