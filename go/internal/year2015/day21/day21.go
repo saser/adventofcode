f46c77c1ff45805dc1b8a9aa4def646c77af52a8
Christian Persson <Saser@users.noreply.github.com>
2019-11-20 15:38:07 +0100

    go/2015/21: implement `playerWins` function according to test

--- go/internal/year2015/day21/day21.go
+++ go/internal/year2015/day21/day21.go
@@ -15,6 +15,18 @@ type character struct {
 	armor     int
 }
 
+func strikeDamage(damage, armor int) int {
+	strikeDamage := damage - armor
+	if strikeDamage < 1 {
+		strikeDamage = 1
+	}
+	return strikeDamage
+}
+
 func playerWins(player, boss character) bool {
-	return false
+	playerStrikeDamage := strikeDamage(player.damage, boss.armor)
+	playerRequiredStrikes := boss.hitpoints/playerStrikeDamage + 1
+	bossStrikeDamage := strikeDamage(boss.damage, player.armor)
+	bossRequiredStrikes := player.hitpoints/bossStrikeDamage + 1
+	return playerRequiredStrikes <= bossRequiredStrikes
 }
