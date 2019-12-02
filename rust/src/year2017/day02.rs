72a00e067042bec9f628317bccec0d03046c9633
Christian Persson <Saser@users.noreply.github.com>
2019-12-02 20:42:01 +0100

    rust/2017/02: migrate to new structure

--- rust/src/year2017/day02.rs
+++ rust/src/year2017/day02.rs
@@ -1,26 +1,29 @@
+use std::io;
 use std::str::FromStr;
 
-use crate::base::{Part, Solver};
+use crate::base::Part;
 
-pub fn get_solver() -> Box<dyn Solver> {
-    Box::new(Day02)
+pub fn part1(r: &mut dyn io::Read) -> Result<String, String> {
+    solve(r, Part::One)
 }
 
-struct Day02;
+pub fn part2(r: &mut dyn io::Read) -> Result<String, String> {
+    solve(r, Part::Two)
+}
 
-impl Solver for Day02 {
-    fn solve(&self, part: Part, input: &str) -> Result<String, String> {
-        let fun = match part {
-            Part::One => min_max,
-            Part::Two => divisors,
-        };
-        Ok(input
-            .lines()
-            .map(parse_line)
-            .map(|v| fun(&v))
-            .sum::<u32>()
-            .to_string())
-    }
+fn solve(r: &mut dyn io::Read, part: Part) -> Result<String, String> {
+    let mut input = String::new();
+    r.read_to_string(&mut input).map_err(|e| e.to_string())?;
+    let fun = match part {
+        Part::One => min_max,
+        Part::Two => divisors,
+    };
+    Ok(input
+        .lines()
+        .map(parse_line)
+        .map(|v| fun(&v))
+        .sum::<u32>()
+        .to_string())
 }
 
 fn parse_line(line: &str) -> Vec<u32> {
@@ -71,84 +74,40 @@ fn divisors(nums: &[u32]) -> u32 {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::test;
 
     mod part1 {
         use super::*;
 
-        #[test]
-        fn example_1() {
-            let solver = get_solver();
-            let input = "5 1 9 5";
-            let expected = "8";
-            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
-        }
-
-        #[test]
-        fn example_2() {
-            let solver = get_solver();
-            let input = "7 5 3";
-            let expected = "4";
-            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
-        }
-
-        #[test]
-        fn example_3() {
-            let solver = get_solver();
-            let input = "2 4 6 8";
-            let expected = "6";
-            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
-        }
-
-        #[test]
-        fn example_all() {
-            let solver = get_solver();
-            let input = "\
-5 1 9 5
-7 5 3
-2 4 6 8\
-            ";
-            let expected = "18";
-            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
-        }
+        test!(example1, "5 1 9 5", "8", part1);
+        test!(example2, "7 5 3", "4", part1);
+        test!(example3, "2 4 6 8", "6", part1);
+        test!(
+            example_all,
+            include_str!("testdata/day02/p1ex"),
+            "18",
+            part1
+        );
+        test!(
+            actual,
+            include_str!("../../../inputs/2017/02"),
+            "36766",
+            part1
+        );
     }
 
     mod part2 {
         use super::*;
 
-        #[test]
-        fn example_1() {
-            let solver = get_solver();
-            let input = "5 9 2 8";
-            let expected = "4";
-            assert_eq!(expected, solver.solve(Part::Two, input).unwrap());
-        }
-
-        #[test]
-        fn example_2() {
-            let solver = get_solver();
-            let input = "9 4 7 3";
-            let expected = "3";
-            assert_eq!(expected, solver.solve(Part::Two, input).unwrap());
-        }
-
-        #[test]
-        fn example_3() {
-            let solver = get_solver();
-            let input = "3 8 6 5";
-            let expected = "2";
-            assert_eq!(expected, solver.solve(Part::Two, input).unwrap());
-        }
-
-        #[test]
-        fn example_all() {
-            let solver = get_solver();
-            let input = "\
-5 9 2 8
-9 4 7 3
-3 8 6 5\
-            ";
-            let expected = "9";
-            assert_eq!(expected, solver.solve(Part::Two, input).unwrap());
-        }
+        test!(example1, "5 9 2 8", "4", part2);
+        test!(example2, "9 4 7 3", "3", part2);
+        test!(example3, "3 8 6 5", "2", part2);
+        test!(example_all, include_str!("testdata/day02/p2ex"), "9", part2);
+        test!(
+            actual,
+            include_str!("../../../inputs/2017/02"),
+            "261",
+            part2
+        );
     }
 }
