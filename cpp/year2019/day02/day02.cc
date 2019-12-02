89605519503739b5f3a82ba6b5e912fd6ddb07f0
Christian Persson <Saser@users.noreply.github.com>
2019-12-02 16:47:58 +0100

    cpp/2019/02: add code to parse the input
    This introduces an external dependency on Abseil, but since the
    standard library of C++ seems a bit limited (compared to e.g. Go's
    standard library), I think it is reasonable.

--- cpp/year2019/day02/day02.cc
+++ cpp/year2019/day02/day02.cc
@@ -1,11 +1,22 @@
 #include "year2019/day02/day02.h"
 
 #include <istream>
+#include <string>
+#include <vector>
+
+#include "absl/strings/str_split.h"
 
 #include "adventofcode.h"
 
 namespace day02 {
   adventofcode::answer_t part1(std::istream& is) {
+    std::string line;
+    std::getline(is, line);
+    std::vector<std::string> parts = absl::StrSplit(line, ",");
+    std::vector<int> program;
+    for (auto part : parts) {
+      program.push_back(std::stoi(part));
+    }
     return adventofcode::err("not implemented yet");
   }
 }
