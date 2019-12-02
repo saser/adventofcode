81dea994339465cb00c8d45c4f8e1984c68af71c
Christian Persson <Saser@users.noreply.github.com>
2019-12-02 08:31:39 +0100

    cpp/2019/01: add solution for part 2

--- cpp/year2019/day01.cc
+++ cpp/year2019/day01.cc
@@ -4,16 +4,29 @@
 
 #include "adventofcode.h"
 
+adventofcode::answer_t solve(std::istream& is, int part);
+
 namespace day01 {
   adventofcode::answer_t part1(std::istream& is) {
-    int sum = 0;
-    for (int i; is >> i;) {
-      sum += i / 3 - 2;
-    }
-    return adventofcode::ok(std::to_string(sum));
+    return solve(is, 1);
   }
 
   adventofcode::answer_t part2(std::istream& is) {
-    return adventofcode::err("not implemented yet");
+    return solve(is, 2);
   }
+}
+
+adventofcode::answer_t solve(std::istream& is, int part) {
+  int sum = 0;
+  for (int i; is >> i;) {
+    while (i > 0) {
+      int new_i = std::max(0, i / 3 - 2);
+      sum += new_i;
+      if (part == 1) {
+        break;
+      }
+      i = new_i;
+    }
+  }
+  return adventofcode::ok(std::to_string(sum));
 }
