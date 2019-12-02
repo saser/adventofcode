ef7a5ca3ab810873e4772ee8f67734cf5f3f0b00
Christian Persson <Saser@users.noreply.github.com>
2019-12-02 20:10:15 +0100

    rust/lib: add a convenience macro for defining tests
    The purpose is to achieve something like the `TestCase` interface I
    defined in my Go tests. Probably there will later be something like
    this for benchmarks as well.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,3 +6,16 @@ pub mod year2017;
 pub mod year2018;
 
 pub type Solution = fn(r: &mut dyn io::Read) -> Result<String, String>;
+
+#[macro_export]
+macro_rules! test {
+    ($name:ident, $input:expr, $output:expr, $solution:expr) => {
+        #[test]
+        fn $name() {
+            let input: &str = $input;
+            let output: &str = $output;
+            let solution: crate::Solution = $solution;
+            assert_eq!(output, solution(&mut Box::new(input.as_bytes())).unwrap());
+        }
+    };
+}
