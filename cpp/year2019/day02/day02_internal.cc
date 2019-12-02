04b33cc12dbed0c202825bec31e3a732fb11a9bb
Christian Persson <Saser@users.noreply.github.com>
2019-12-02 17:02:14 +0100

    cpp/2019/02: add internal "library" to run the programs, with tests
    The example tests are moved to unit tests for this library instead,
    since for the main puzzle it is required to change the input at
    positions 1 and 2, which is not done in the examples.

---
+++ cpp/year2019/day02/day02_internal.cc
@@ -0,0 +1,28 @@
+#include "year2019/day02/day02_internal.h"
+
+#include <vector>
+
+namespace day02 {
+  int run(const std::vector<int>& program) {
+    std::vector<int> memory = program;
+    int position = 0;
+    int opcode = 0;
+    while ((opcode = memory[position]) != 99) {
+      int operand1 = memory[memory[position + 1]];
+      int operand2 = memory[memory[position + 2]];
+      int destination = memory[position + 3];
+      int value;
+      switch (opcode) {
+      case 1:
+        value = operand1 + operand2;
+        break;
+      case 2:
+        value = operand1 * operand2;
+        break;
+      }
+      memory[destination] = value;
+      position += 4;
+    }
+    return memory[0];
+  }
+}
--- cpp/year2019/day02/day02_test.cc
+++ cpp/year2019/day02/day02_test.cc
@@ -7,46 +7,6 @@
 
 #include "adventofcode.h"
 
-TEST(Year2019Day02, Part1Example1) {
-  std::istringstream input("1,9,10,3,2,3,11,0,99,30,40,50");
-  std::string output = "3500";
-  adventofcode::answer_t a = day02::part1(input);
-  EXPECT_EQ("", a.error);
-  EXPECT_EQ(output, a.answer);
-}
-
-TEST(Year2019Day02, Part1Example2) {
-  std::istringstream input("1,0,0,0,99");
-  std::string output = "2";
-  adventofcode::answer_t a = day02::part1(input);
-  EXPECT_EQ("", a.error);
-  EXPECT_EQ(output, a.answer);
-}
-
-TEST(Year2019Day02, Part1Example3) {
-  std::istringstream input("2,3,0,3,99");
-  std::string output = "2";
-  adventofcode::answer_t a = day02::part1(input);
-  EXPECT_EQ("", a.error);
-  EXPECT_EQ(output, a.answer);
-}
-
-TEST(Year2019Day02, Part1Example4) {
-  std::istringstream input("2,4,4,5,99,0");
-  std::string output = "2";
-  adventofcode::answer_t a = day02::part1(input);
-  EXPECT_EQ("", a.error);
-  EXPECT_EQ(output, a.answer);
-}
-
-TEST(Year2019Day02, Part1Example5) {
-  std::istringstream input("1,1,1,4,99,5,6,0,99");
-  std::string output = "30";
-  adventofcode::answer_t a = day02::part1(input);
-  EXPECT_EQ("", a.error);
-  EXPECT_EQ(output, a.answer);
-}
-
 TEST(Year2019Day02, Part1Actual) {
   std::ifstream input("year2019/testdata/02");
   std::string output = "";
