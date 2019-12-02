865a9c4675643ec3aba554ecdb379fad655357cc
Christian Persson <Saser@users.noreply.github.com>
2019-12-02 16:36:28 +0100

    cpp/2019/02: add placeholder and tests for part 1

---
+++ cpp/year2019/day02/day02.h
@@ -0,0 +1,12 @@
+#ifndef ADVENTOFCODE_YEAR2019_DAY02_H
+#define ADVENTOFCODE_YEAR2019_DAY02_H
+
+#include <istream>
+
+#include "adventofcode.h"
+
+namespace day02 {
+  adventofcode::answer_t part1(std::istream& is);
+}
+
+#endif
