3b27d1fbd89f9abda292347e99e9dad7e2576d91
Christian Persson <Saser@users.noreply.github.com>
2019-12-02 18:19:48 +0100

    cpp: add benchmarks for currently existing solutions
    This introduces Google's `benchmark` library as an external
    dependency.

---
+++ cpp/year2019/day01/day01_benchmark.cc
@@ -0,0 +1,27 @@
+#include "year2019/day01/day01.h"
+
+#include <fstream>
+#include <istream>
+
+#include "benchmark/benchmark.h"
+
+static void Year2019Day01Part1(benchmark::State& state) {
+  std::ifstream input("year2019/testdata/01");
+  for (auto _ : state) {
+    day01::part1(input);
+    input.clear();
+    input.seekg(0);
+  }
+}
+BENCHMARK(Year2019Day01Part1);
+
+
+static void Year2019Day01Part2(benchmark::State& state) {
+  std::ifstream input("year2019/testdata/01");
+  for (auto _ : state) {
+    day01::part2(input);
+    input.clear();
+    input.seekg(0);
+  }
+}
+BENCHMARK(Year2019Day01Part2);
