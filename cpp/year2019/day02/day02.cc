14e863df04295dedaab2cca868ec2db41c23c025
Christian Persson <Saser@users.noreply.github.com>
2019-12-02 16:36:28 +0100

    cpp/2019/02: add placeholder and tests for part 1

---
+++ cpp/year2019/day02/day02.cc
@@ -0,0 +1,11 @@
+#include "year2019/day02/day02.h"
+
+#include <istream>
+
+#include "adventofcode.h"
+
+namespace day02 {
+  adventofcode::answer_t part1(std::istream& is) {
+    return adventofcode::err("not implemented yet");
+  }
+}
