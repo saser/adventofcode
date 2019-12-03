f4ca8314e64e5f8e34997865f4abe99a33c4d3d5
Christian Persson <Saser@users.noreply.github.com>
2019-12-03 17:32:56 +0100

    cpp/2019/03: add placeholder, benchmark, and tests for part 2

--- cpp/year2019/day03/day03_benchmark.cc
+++ cpp/year2019/day03/day03_benchmark.cc
@@ -13,3 +13,13 @@ static void Year2019Day03Part1(benchmark::State& state) {
   }
 }
 BENCHMARK(Year2019Day03Part1);
+
+static void Year2019Day03Part2(benchmark::State& state) {
+  std::ifstream input("year2019/testdata/03");
+  for (auto _ : state) {
+    day03::part2(input);
+    input.clear();
+    input.seekg(0);
+  }
+}
+BENCHMARK(Year2019Day03Part2);
--- cpp/year2019/day03/day03_test.cc
+++ cpp/year2019/day03/day03_test.cc
@@ -41,3 +41,39 @@ TEST(Year2019Day03, Part1Actual) {
   EXPECT_EQ(output, a.answer);
   input.close();
 }
+
+TEST(Year2019Day03, Part2Example1) {
+  std::ifstream input("year2019/day03/testdata/example1");
+  std::string output = "30";
+  adventofcode::answer_t a = day03::part2(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
+
+TEST(Year2019Day03, Part2Example2) {
+  std::ifstream input("year2019/day03/testdata/example2");
+  std::string output = "610";
+  adventofcode::answer_t a = day03::part2(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
+
+TEST(Year2019Day03, Part2Example3) {
+  std::ifstream input("year2019/day03/testdata/example3");
+  std::string output = "410";
+  adventofcode::answer_t a = day03::part2(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
+
+TEST(Year2019Day03, Part2Actual) {
+  std::ifstream input("year2019/testdata/03");
+  std::string output = "";
+  adventofcode::answer_t a = day03::part2(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
