b40689da584edee5894e0d8b8fd7ebf9385d1f0f
Christian Persson <Saser@users.noreply.github.com>
2019-12-04 08:46:13 +0100

    cpp/2019/04: move example tests to internal library

--- cpp/year2019/day04/day04_test.cc
+++ cpp/year2019/day04/day04_test.cc
@@ -1,35 +1,10 @@
 #include "year2019/day04/day04.h"
 
 #include <fstream>
-#include <sstream>
 
 #include "gtest/gtest.h"
 
 #include "adventofcode.h"
-
-TEST(Year2019Day04, Part1Example1) {
-  std::istringstream input("111111");
-  std::string output = "1";
-  adventofcode::answer_t a = day04::part1(input);
-  EXPECT_EQ("", a.error);
-  EXPECT_EQ(output, a.answer);
-}
-
-TEST(Year2019Day04, Part1Example2) {
-  std::istringstream input("223450");
-  std::string output = "0";
-  adventofcode::answer_t a = day04::part1(input);
-  EXPECT_EQ("", a.error);
-  EXPECT_EQ(output, a.answer);
-}
-
-TEST(Year2019Day04, Part1Example3) {
-  std::istringstream input("123789");
-  std::string output = "0";
-  adventofcode::answer_t a = day04::part1(input);
-  EXPECT_EQ("", a.error);
-  EXPECT_EQ(output, a.answer);
-}
 
 TEST(Year2019Day04, Part1Actual) {
   std::ifstream input("year2019/testdata/04");
