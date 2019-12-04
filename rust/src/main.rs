d0cea2fb0cc60529ecd8a6c1531363056b06effe
Christian Persson <Saser@users.noreply.github.com>
2019-12-04 19:32:19 +0100

    rust/2017/15: migrate to new structure

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -137,6 +137,11 @@ fn imain() -> i32 {
                 2 => Ok(aoc::year2017::day14::part2),
                 _ => unreachable!(),
             },
+            15 => match part {
+                1 => Ok(aoc::year2017::day15::part1),
+                2 => Ok(aoc::year2017::day15::part2),
+                _ => unreachable!(),
+            },
             _ => Err(format!("no solution for year 2017 day {}", day)),
         },
         _ => Err(format!("no solutions for year {}", year)),
--- rust/src/year2017/day15.rs
+++ rust/src/year2017/day15.rs
@@ -1,34 +1,37 @@
+use std::io;
 use std::str::FromStr;
 
-use crate::base::{Part, Solver};
-
-pub fn get_solver() -> Box<dyn Solver> {
-    Box::new(Day15)
-}
-
-struct Day15;
+use crate::base::Part;
 
 const MUL_A: u64 = 16807;
 const MUL_B: u64 = 48271;
 const MOD: u64 = 2_147_483_647;
 
-impl Solver for Day15 {
-    fn solve(&self, part: Part, input: &str) -> Result<String, String> {
-        let (start_a, start_b) = parse_input(input);
-        let (repetitions, constraints) = match part {
-            Part::One => (40_000_000, (1, 1)),
-            Part::Two => (5_000_000, (4, 8)),
-        };
-        let matching_pairs = (0..repetitions)
-            .scan((start_a, start_b), |pair, _| {
-                *pair = next_values(*pair, constraints);
-                Some(*pair)
-            })
-            //.inspect(|pair| println!("pair: {:?}", pair))
-            .filter(|&pair| lowest_16_bits_matching(pair))
-            .count();
-        Ok(matching_pairs.to_string())
-    }
+pub fn part1(r: &mut dyn io::Read) -> Result<String, String> {
+    solve(r, Part::One)
+}
+
+pub fn part2(r: &mut dyn io::Read) -> Result<String, String> {
+    solve(r, Part::Two)
+}
+
+fn solve(r: &mut dyn io::Read, part: Part) -> Result<String, String> {
+    let mut input = String::new();
+    r.read_to_string(&mut input).map_err(|e| e.to_string())?;
+    let (start_a, start_b) = parse_input(&input);
+    let (repetitions, constraints) = match part {
+        Part::One => (40_000_000, (1, 1)),
+        Part::Two => (5_000_000, (4, 8)),
+    };
+    let matching_pairs = (0..repetitions)
+        .scan((start_a, start_b), |pair, _| {
+            *pair = next_values(*pair, constraints);
+            Some(*pair)
+        })
+        //.inspect(|pair| println!("pair: {:?}", pair))
+        .filter(|&pair| lowest_16_bits_matching(pair))
+        .count();
+    Ok(matching_pairs.to_string())
 }
 
 fn parse_input(input: &str) -> (u64, u64) {
@@ -63,34 +66,29 @@ fn lowest_16_bits_matching((value_a, value_b): (u64, u64)) -> bool {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::test;
 
     mod part1 {
         use super::*;
 
-        #[test]
-        fn example() {
-            let solver = get_solver();
-            let input = "\
-Generator A starts with 65
-Generator B starts with 8921\
-            ";
-            let expected = "588";
-            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
-        }
+        test!(example, include_str!("testdata/day15/ex"), "588", part1);
+        test!(
+            actual,
+            include_str!("../../../inputs/2017/15"),
+            "609",
+            part1
+        );
     }
 
     mod part2 {
         use super::*;
 
-        #[test]
-        fn example() {
-            let solver = get_solver();
-            let input = "\
-Generator A starts with 65
-Generator B starts with 8921\
-            ";
-            let expected = "309";
-            assert_eq!(expected, solver.solve(Part::Two, input).unwrap());
-        }
+        test!(example, include_str!("testdata/day15/ex"), "309", part2);
+        test!(
+            actual,
+            include_str!("../../../inputs/2017/15"),
+            "253",
+            part2
+        );
     }
 }
