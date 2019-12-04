cd1d81188080b7443d1553d7de703147f9f238e2
Christian Persson <Saser@users.noreply.github.com>
2019-12-04 22:59:31 +0100

    rust/2018/14: migrate to new structure

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -230,6 +230,11 @@ fn imain() -> i32 {
                 2 => Ok(aoc::year2018::day13::part2),
                 _ => unreachable!(),
             },
+            14 => match part {
+                1 => Ok(aoc::year2018::day14::part1),
+                2 => Ok(aoc::year2018::day14::part2),
+                _ => unreachable!(),
+            },
             _ => Err(format!("no solution for year 2018 day {}", day)),
         },
         _ => Err(format!("no solutions for year {}", year)),
--- rust/src/year2018/day14.rs
+++ rust/src/year2018/day14.rs
@@ -1,38 +1,42 @@
+use std::io;
 use std::str::FromStr;
 
-use crate::base::{Part, Solver};
+use crate::base::Part;
 
-pub fn get_solver() -> Box<dyn Solver> {
-    Box::new(Day14)
+pub fn part1(r: &mut dyn io::Read) -> Result<String, String> {
+    solve(r, Part::One)
 }
 
-struct Day14;
+pub fn part2(r: &mut dyn io::Read) -> Result<String, String> {
+    solve(r, Part::Two)
+}
 
-impl Solver for Day14 {
-    fn solve(&self, part: Part, input: &str) -> Result<String, String> {
-        let made_recipes = parse_input(input);
-        let nr_recipes = made_recipes + 10;
-        let mut scores = Vec::with_capacity(nr_recipes);
-        scores.extend(&[3, 7]);
-        let mut indices = [0, 1];
-        match part {
-            Part::One => {
-                generate_scores(&mut scores, &mut indices, nr_recipes);
-                let following_ten = scores.iter().skip(made_recipes).take(10);
-                let s = following_ten
-                    .map(|score| score.to_string())
-                    .collect::<String>();
-                Ok(s)
-            }
-            Part::Two => {
-                let pattern = input
-                    .chars()
-                    .map(|c| c.to_string())
-                    .map(|s| usize::from_str(&s).unwrap())
-                    .collect::<Vec<usize>>();
-                let recipes_before = generate_until_pattern(&mut scores, &mut indices, &pattern);
-                Ok(recipes_before.to_string())
-            }
+fn solve(r: &mut dyn io::Read, part: Part) -> Result<String, String> {
+    let mut input = String::new();
+    r.read_to_string(&mut input).map_err(|e| e.to_string())?;
+    input = input.trim().to_string();
+    let made_recipes = parse_input(&input);
+    let nr_recipes = made_recipes + 10;
+    let mut scores = Vec::with_capacity(nr_recipes);
+    scores.extend(&[3, 7]);
+    let mut indices = [0, 1];
+    match part {
+        Part::One => {
+            generate_scores(&mut scores, &mut indices, nr_recipes);
+            let following_ten = scores.iter().skip(made_recipes).take(10);
+            let s = following_ten
+                .map(|score| score.to_string())
+                .collect::<String>();
+            Ok(s)
+        }
+        Part::Two => {
+            let pattern = input
+                .chars()
+                .map(|c| c.to_string())
+                .map(|s| usize::from_str(&s).unwrap())
+                .collect::<Vec<usize>>();
+            let recipes_before = generate_until_pattern(&mut scores, &mut indices, &pattern);
+            Ok(recipes_before.to_string())
         }
     }
 }
@@ -123,92 +127,35 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::test;
 
     mod part1 {
         use super::*;
 
-        #[test]
-        fn with_input() {
-            let solver = get_solver();
-            let input = include_str!("../../../inputs/2018/14").trim();
-            let expected = "5371393113";
-            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
-        }
-
-        #[test]
-        fn example_1() {
-            let solver = get_solver();
-            let input = "9";
-            let expected = "5158916779";
-            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
-        }
-
-        #[test]
-        fn example_2() {
-            let solver = get_solver();
-            let input = "5";
-            let expected = "0124515891";
-            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
-        }
-
-        #[test]
-        fn example_3() {
-            let solver = get_solver();
-            let input = "18";
-            let expected = "9251071085";
-            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
-        }
-
-        #[test]
-        fn example_4() {
-            let solver = get_solver();
-            let input = "2018";
-            let expected = "5941429882";
-            assert_eq!(expected, solver.solve(Part::One, input).unwrap());
-        }
+        test!(example1, "9", "5158916779", part1);
+        test!(example2, "5", "0124515891", part1);
+        test!(example3, "18", "9251071085", part1);
+        test!(example4, "2018", "5941429882", part1);
+        test!(
+            actual,
+            include_str!("../../../inputs/2018/14"),
+            "5371393113",
+            part1
+        );
     }
 
     mod part2 {
         use super::*;
 
-        #[test]
-        fn with_input() {
-            let solver = get_solver();
-            let input = include_str!("../../../inputs/2018/14").trim();
-            let expected = "20286858";
-            assert_eq!(expected, solver.solve(Part::Two, input).unwrap());
-        }
-
-        #[test]
-        fn example_1() {
-            let solver = get_solver();
-            let input = "51589";
-            let expected = "9";
-            assert_eq!(expected, solver.solve(Part::Two, input).unwrap());
-        }
-
-        #[test]
-        fn example_2() {
-            let solver = get_solver();
-            let input = "01245";
-            let expected = "5";
-            assert_eq!(expected, solver.solve(Part::Two, input).unwrap());
-        }
-
-        #[test]
-        fn example_3() {
-            let solver = get_solver();
-            let input = "92510";
-            let expected = "18";
-            assert_eq!(expected, solver.solve(Part::Two, input).unwrap());
-        }
-
-        #[test]
-        fn example_4() {
-            let solver = get_solver();
-            let input = "59414";
-            let expected = "2018";
-            assert_eq!(expected, solver.solve(Part::Two, input).unwrap());
-        }
+        test!(example1, "51589", "9", part2);
+        test!(example2, "01245", "5", part2);
+        test!(example3, "92510", "18", part2);
+        test!(example4, "59414", "2018", part2);
+        test!(
+            actual,
+            include_str!("../../../inputs/2018/14"),
+            "20286858",
+            part2
+        );
     }
 }
