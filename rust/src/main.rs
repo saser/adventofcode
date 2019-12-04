29e341835327a5069e9c9430c3d225e8fa1f1a01
Christian Persson <Saser@users.noreply.github.com>
2019-12-04 21:56:36 +0100

    rust/2018/09: migrate to new structure

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -205,6 +205,11 @@ fn imain() -> i32 {
                 2 => Ok(aoc::year2018::day08::part2),
                 _ => unreachable!(),
             },
+            9 => match part {
+                1 => Ok(aoc::year2018::day09::part1),
+                2 => Ok(aoc::year2018::day09::part2),
+                _ => unreachable!(),
+            },
             _ => Err(format!("no solution for year 2018 day {}", day)),
         },
         _ => Err(format!("no solutions for year {}", year)),
