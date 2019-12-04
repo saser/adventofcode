cbd89af5f656064948f6f91f9b55e2e1d7de3455
Christian Persson <Saser@users.noreply.github.com>
2019-12-04 09:03:17 +0100

    cpp/2019/04: add solution for part 1

--- cpp/year2019/day04/day04.cc
+++ cpp/year2019/day04/day04.cc
@@ -1,11 +1,27 @@
 #include "year2019/day04/day04.h"
 
 #include <istream>
+#include <string>
+
+#include "absl/strings/str_split.h"
 
 #include "adventofcode.h"
+#include "year2019/day04/day04_internal.h"
 
 namespace day04 {
   adventofcode::answer_t part1(std::istream& is) {
-    return adventofcode::err("not implemented yet");
+    std::string input;
+    std::getline(is, input);
+    std::vector<std::string> parts = absl::StrSplit(input, "-");
+    int start = std::stoi(parts[0]);
+    int end = std::stoi(parts[1]);
+    int count = 0;
+    for (int password = start; password <= end; password++) {
+      auto digits = day04::digits(password);
+      if (day04::has_double(digits) && day04::non_decreasing(digits)) {
+        count++;
+      }
+    }
+    return adventofcode::ok(std::to_string(count));
   }
 }
