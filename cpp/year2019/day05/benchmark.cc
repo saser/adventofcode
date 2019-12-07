efed648e7bdf322a56c867ffe52b6f9ad827ac1b
Christian Persson <Saser@users.noreply.github.com>
2019-12-07 02:38:13 +0100

    cpp/2019/05: add benchmark for part 2

--- cpp/year2019/day05/benchmark.cc
+++ cpp/year2019/day05/benchmark.cc
@@ -13,3 +13,13 @@ static void Year2019Day05Part1(benchmark::State& state) {
   }
 }
 BENCHMARK(Year2019Day05Part1);
+
+static void Year2019Day05Part2(benchmark::State& state) {
+  std::ifstream input("year2019/testdata/05");
+  for (auto _ : state) {
+    day05::part2(input);
+    input.clear();
+    input.seekg(0);
+  }
+}
+BENCHMARK(Year2019Day05Part2);
