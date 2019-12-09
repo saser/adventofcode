c5a4d9da1226144944c6daf2ba060527383b85a1
Christian Persson <Saser@users.noreply.github.com>
2019-12-09 21:08:33 +0100

    cpp/2019/07: move example inputs to own files
    They were a little too long anyway, so they might as well live in a
    separate file, to avoid cluttering down the test source code.

--- cpp/year2019/day07/test.cc
+++ cpp/year2019/day07/test.cc
@@ -17,19 +17,21 @@ TEST(Year2019Day07, Part1Example1) {
 }
 
 TEST(Year2019Day07, Part1Example2) {
-  std::istringstream input("3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0");
+  std::ifstream input("year2019/day07/testdata/p1ex2");
   std::string output = "54321";
   adventofcode::answer_t a = day07::part1(input);
   EXPECT_EQ("", a.error);
   EXPECT_EQ(output, a.answer);
+  input.close();
 }
 
 TEST(Year2019Day07, Part1Example3) {
-  std::istringstream input("3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0");
+  std::ifstream input("year2019/day07/testdata/p1ex3");
   std::string output = "65210";
   adventofcode::answer_t a = day07::part1(input);
   EXPECT_EQ("", a.error);
   EXPECT_EQ(output, a.answer);
+  input.close();
 }
 
 TEST(Year2019Day07, Part1Actual) {
