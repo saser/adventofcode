68f31bf16684b55ad322ea8d7d2a25cbf86f56f8
Christian Persson <Saser@users.noreply.github.com>
2019-12-09 23:25:30 +0100

    cpp/2019/08: add solution for part 1

--- cpp/year2019/day08/day08.cc
+++ cpp/year2019/day08/day08.cc
@@ -1,6 +1,7 @@
 #include "year2019/day08/day08.h"
 
 #include <istream>
+#include <string>
 
 #include "adventofcode.h"
 
@@ -17,5 +18,37 @@ namespace day08 {
 }
 
 adventofcode::answer_t solve(std::istream& is, int part) {
-  return adventofcode::err("not implemented yet");
+  auto width = 25;
+  auto height = 6;
+  auto layer_size = width * height;
+  std::string line;
+  std::getline(is, line);
+  auto zeroes = 0;
+  auto ones = 0;
+  auto twos = 0;
+  auto fewest_zeroes = layer_size;
+  auto product = 0;
+  for (std::string::size_type i = 0; i < line.length(); i++) {
+    if (i != 0 && i % layer_size == 0) {
+      if (zeroes < fewest_zeroes) {
+        fewest_zeroes = zeroes;
+        product = ones * twos;
+      }
+      zeroes = 0;
+      ones = 0;
+      twos = 0;
+    }
+    switch (line[i]) {
+    case '0':
+      zeroes++;
+      break;
+    case '1':
+      ones++;
+      break;
+    case '2':
+      twos++;
+      break;
+    }
+  }
+  return adventofcode::ok(std::to_string(product));
 }
--- cpp/year2019/day08/test.cc
+++ cpp/year2019/day08/test.cc
@@ -9,7 +9,7 @@
 
 TEST(Year2019Day08, Part1Actual) {
   std::ifstream input("year2019/testdata/08");
-  std::string output = "";
+  std::string output = "2032";
   adventofcode::answer_t a = day08::part1(input);
   EXPECT_EQ("", a.error);
   EXPECT_EQ(output, a.answer);
