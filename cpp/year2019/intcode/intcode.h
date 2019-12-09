a01380eda916214131a33fa89e60a596da5d7d9b
Christian Persson <Saser@users.noreply.github.com>
2019-12-09 20:04:18 +0100

    cpp/2019/intcode: improve readability a little bit

--- cpp/year2019/intcode/intcode.h
+++ cpp/year2019/intcode/intcode.h
@@ -17,20 +17,29 @@ namespace intcode {
     8, // equals
     99, // halt
   };
-  // Determine the opcode for a given memory value.
-  int opcode(int instruction);
-  // Determine whether parameter number `n` is in immediate mode.
-  bool immediate_mode(int instruction, int n);
-  bool position_mode(int instruction, int n);
-  // Return the number of parameters for the given opcode.
-  size_t n_params(int opcode);
+
   // Memory is represented as a vector of ints.
   typedef std::vector<int> memory;
+
   // As are input and output of the program.
   typedef std::vector<int> input;
   typedef std::vector<int> output;
+
+  // Determine the opcode for a given memory value.
+  int opcode(int instruction);
+
+  // Determine whether parameter number `n` is in immediate mode.
+  bool immediate_mode(int instruction, int n);
+
+  // Determine whether parameter number `n` is in position mode.
+  bool position_mode(int instruction, int n);
+
+  // Return the number of parameters for the given opcode.
+  size_t n_params(int opcode);
+
   // Parse a string into a memory.
   memory parse(const std::string& s);
+
   // Run a program. The program is started at position 0, and the provided
   // inputs are read in order. The first return value is the state of the memory
   // at the time the program halts; the second return value is the output
