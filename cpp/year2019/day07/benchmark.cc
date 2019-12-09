7dc04764fafd1e4b2d23d9bd6aaa8cf91058bddb
Christian Persson <Saser@users.noreply.github.com>
2019-12-09 18:55:52 +0100

    cpp/2019/07: add placeholders/benchmarks (part 1&2) tests for part 1

---
+++ cpp/year2019/day07/benchmark.cc
@@ -0,0 +1,25 @@
+#include "year2019/day07/day07.h"
+
+#include <fstream>
+
+#include "benchmark/benchmark.h"
+
+static void Year2019Day07Part1(benchmark::State& state) {
+  std::ifstream input("year2019/testdata/07");
+  for (auto _ : state) {
+    day07::part1(input);
+    input.clear();
+    input.seekg(0);
+  }
+}
+BENCHMARK(Year2019Day07Part1);
+
+static void Year2019Day07Part2(benchmark::State& state) {
+  std::ifstream input("year2019/testdata/07");
+  for (auto _ : state) {
+    day07::part2(input);
+    input.clear();
+    input.seekg(0);
+  }
+}
+BENCHMARK(Year2019Day07Part2);
