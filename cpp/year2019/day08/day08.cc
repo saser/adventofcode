637d07f915f3675429ea7904040e9e2f685a7938
Christian Persson <Saser@users.noreply.github.com>
2019-12-10 00:06:53 +0100

    cpp/2019/08: add solution for part 2

--- cpp/year2019/day08/day08.cc
+++ cpp/year2019/day08/day08.cc
@@ -1,11 +1,14 @@
 #include "year2019/day08/day08.h"
 
-#include <istream>
+#include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
 
 #include "adventofcode.h"
 
 adventofcode::answer_t solve(std::istream& is, int part);
+std::string render(const std::vector<std::vector<char>>& grid);
 
 namespace day08 {
   adventofcode::answer_t part1(std::istream& is) {
@@ -18,37 +21,69 @@ namespace day08 {
 }
 
 adventofcode::answer_t solve(std::istream& is, int part) {
-  auto width = 25;
-  auto height = 6;
-  auto layer_size = width * height;
   std::string line;
   std::getline(is, line);
+  auto n_rows = 6;
+  auto n_cols = 25;
+  std::vector<std::vector<char>> grid(n_rows, std::vector<char>(n_cols, '\0'));
+  auto layer_size = n_rows * n_cols;
   auto zeroes = 0;
   auto ones = 0;
   auto twos = 0;
   auto fewest_zeroes = layer_size;
   auto product = 0;
-  for (std::string::size_type i = 0; i < line.length(); i++) {
-    if (i != 0 && i % layer_size == 0) {
-      if (zeroes < fewest_zeroes) {
-        fewest_zeroes = zeroes;
-        product = ones * twos;
+  for (std::string::size_type i = 0; i < line.length(); i += layer_size) {
+    auto layer = line.substr(i, layer_size);
+    for (auto row_i = 0; row_i < n_rows; row_i++) {
+      auto row = layer.substr(row_i * n_cols, n_cols);
+      for (auto col_i = 0; col_i < n_cols; col_i++) {
+        auto c = row[col_i];
+        char& color = grid[row_i][col_i];
+        switch (c) {
+        case '0':
+        case '1':
+          if (color == '\0') {
+            color = c;
+          }
+          if (c == '0') {
+            zeroes++;
+          } else {
+            ones++;
+          }
+          break;
+        case '2':
+          twos++;
+          break;
+        }
       }
-      zeroes = 0;
-      ones = 0;
-      twos = 0;
     }
-    switch (line[i]) {
-    case '0':
-      zeroes++;
-      break;
-    case '1':
-      ones++;
-      break;
-    case '2':
-      twos++;
-      break;
+    if (zeroes < fewest_zeroes) {
+      fewest_zeroes = zeroes;
+      product = ones * twos;
     }
+    zeroes = 0;
+    ones = 0;
+    twos = 0;
   }
-  return adventofcode::ok(std::to_string(product));
+  if (part == 1) {
+    return adventofcode::ok(std::to_string(product));
+  }
+  return adventofcode::ok(render(grid));
+}
+
+std::string render(const std::vector<std::vector<char>>& grid) {
+  std::stringbuf sb;
+  std::ostream os(&sb);
+  for (auto row : grid) {
+    for (auto c : row) {
+      if (c == '1') {
+        c = '#';
+      } else {
+        c = '.';
+      }
+      os << c;
+    }
+    os << std::endl;
+  }
+  return sb.str();
 }
--- cpp/year2019/day08/test.cc
+++ cpp/year2019/day08/test.cc
@@ -1,5 +1,6 @@
 #include "year2019/day08/day08.h"
 
+#include <istream>
 #include <fstream>
 #include <string>
 
@@ -16,19 +17,12 @@ TEST(Year2019Day08, Part1Actual) {
   input.close();
 }
 
-// TEST(Year2019Day08, Part2Example1) {
-//   std::istringstream input("some input here");
-//   std::string output = "some output here";
-//   adventofcode::answer_t a = day08::part2(input);
-//   EXPECT_EQ("", a.error);
-//   EXPECT_EQ(output, a.answer);
-// }
-
-// TEST(Year2019Day08, Part2Actual) {
-//   std::ifstream input("year2019/testdata/08");
-//   std::string output = "some output here";
-//   adventofcode::answer_t a = day08::part2(input);
-//   EXPECT_EQ("", a.error);
-//   EXPECT_EQ(output, a.answer);
-//   input.close();
-// }
+TEST(Year2019Day08, Part2Actual) {
+  std::ifstream input("year2019/testdata/08");
+  std::ifstream output_file("year2019/day08/testdata/p2out");
+  std::string output(std::istreambuf_iterator<char>(output_file), {});
+  adventofcode::answer_t a = day08::part2(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
