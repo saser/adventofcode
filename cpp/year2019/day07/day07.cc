14f2d2fb603627bf243c60246deb70a3f8979fae
Christian Persson <Saser@users.noreply.github.com>
2019-12-09 19:26:02 +0100

    cpp/2019/07: add function to calculate permutations of vector
    Wohoo, my first generic code in C++! I am not at all sure if I am
    doing it right, but it seems to work while manually testing at least.

--- cpp/year2019/day07/day07.cc
+++ cpp/year2019/day07/day07.cc
@@ -5,6 +5,8 @@
 #include "adventofcode.h"
 
 adventofcode::answer_t solve(std::istream& is, int part);
+template<class T>
+std::vector<std::vector<T>> permutations(const std::vector<T>& is);
 
 namespace day07 {
   adventofcode::answer_t part1(std::istream& is) {
@@ -18,4 +20,28 @@ namespace day07 {
 
 adventofcode::answer_t solve(std::istream& is, int part) {
   return adventofcode::err("not implemented yet");
+}
+
+template<class T>
+std::vector<std::vector<T>> permutations(const std::vector<T>& is) {
+  auto n = is.size();
+  if (n == 1) {
+    return {is};
+  }
+  std::vector<std::vector<int>> perms;
+  for (size_t i = 0; i < n; i++) {
+    std::vector<int> rest;
+    rest.reserve(n - 1);
+    auto mid = is.begin() + i;
+    rest.insert(rest.begin(), is.begin(), mid);
+    rest.insert(rest.end(), mid + 1, is.end());
+    for (auto subperm : permutations(rest)) {
+      std::vector<int> perm;
+      perm.reserve(n);
+      perm.push_back(*mid);
+      perm.insert(perm.end(), subperm.begin(), subperm.end());
+      perms.push_back(perm);
+    }
+  }
+  return perms;
 }
