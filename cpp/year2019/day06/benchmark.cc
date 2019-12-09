ae36f77d476d9ee267fead617133860aec803b62
Christian Persson <Saser@users.noreply.github.com>
2019-12-09 14:02:17 +0100

    cpp/2019/06: add placeholder, tests, and benchmark for part 2

--- cpp/year2019/day06/benchmark.cc
+++ cpp/year2019/day06/benchmark.cc
@@ -13,3 +13,13 @@ static void Year2019Day06Part1(benchmark::State& state) {
   }
 }
 BENCHMARK(Year2019Day06Part1);
+
+static void Year2019Day06Part2(benchmark::State& state) {
+  std::ifstream input("year2019/testdata/06");
+  for (auto _ : state) {
+    day06::part2(input);
+    input.clear();
+    input.seekg(0);
+  }
+}
+BENCHMARK(Year2019Day06Part2);
