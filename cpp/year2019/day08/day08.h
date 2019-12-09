e6e5a2132dd76e8fa93b2056b225c82b723d3901
Christian Persson <Saser@users.noreply.github.com>
2019-12-09 23:11:57 +0100

    cpp/2019/08: add placeholders, tests, and benchmarks

---
+++ cpp/year2019/day08/day08.h
@@ -0,0 +1,13 @@
+#ifndef ADVENTOFCODE_YEAR2019_DAY08_H
+#define ADVENTOFCODE_YEAR2019_DAY08_H
+
+#include <istream>
+
+#include "adventofcode.h"
+
+namespace day08 {
+  adventofcode::answer_t part1(std::istream& is);
+  adventofcode::answer_t part2(std::istream& is);
+}
+
+#endif
