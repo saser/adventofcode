a5839ecbb41ec8f67b171bcd469799b81a4395a3
Christian Persson <Saser@users.noreply.github.com>
2019-12-10 11:23:55 +0100

    cpp/2019/10: add placeholders, tests, and benchmarks (part 1 and 2)

---
+++ cpp/year2019/day10/test.cc
@@ -0,0 +1,79 @@
+#include "year2019/day10/day10.h"
+
+#include <fstream>
+#include <string>
+
+#include "gtest/gtest.h"
+
+#include "adventofcode.h"
+
+TEST(Year2019Day10, Part1Example1) {
+  std::ifstream input("year2019/day10/testdata/ex1");
+  std::string output = "8";
+  adventofcode::answer_t a = day10::part1(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
+
+TEST(Year2019Day10, Part1Example2) {
+  std::ifstream input("year2019/day10/testdata/ex2");
+  std::string output = "33";
+  adventofcode::answer_t a = day10::part1(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
+
+TEST(Year2019Day10, Part1Example3) {
+  std::ifstream input("year2019/day10/testdata/ex3");
+  std::string output = "35";
+  adventofcode::answer_t a = day10::part1(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
+
+TEST(Year2019Day10, Part1Example4) {
+  std::ifstream input("year2019/day10/testdata/ex4");
+  std::string output = "41";
+  adventofcode::answer_t a = day10::part1(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
+
+TEST(Year2019Day10, Part1Example5) {
+  std::ifstream input("year2019/day10/testdata/ex5");
+  std::string output = "210";
+  adventofcode::answer_t a = day10::part1(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
+
+TEST(Year2019Day10, Part1Actual) {
+  std::ifstream input("year2019/testdata/10");
+  std::string output = "";
+  adventofcode::answer_t a = day10::part1(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
+
+// TEST(Year2019Day10, Part2Example1) {
+//   std::istringstream input("some input here");
+//   std::string output = "some output here";
+//   adventofcode::answer_t a = day10::part2(input);
+//   EXPECT_EQ("", a.error);
+//   EXPECT_EQ(output, a.answer);
+// }
+
+// TEST(Year2019Day10, Part2Actual) {
+//   std::ifstream input("year2019/testdata/10");
+//   std::string output = "some output here";
+//   adventofcode::answer_t a = day10::part2(input);
+//   EXPECT_EQ("", a.error);
+//   EXPECT_EQ(output, a.answer);
+//   input.close();
+// }
