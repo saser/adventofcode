a50beb8f04950099ecabd8f1602e4765a3babd1c
Christian Persson <Saser@users.noreply.github.com>
2019-12-11 10:36:36 +0100

    cpp/2019/11: add placholders, tests, and benchmarks (part 1 and 2)

---
+++ cpp/year2019/day11/day11.h
@@ -0,0 +1,13 @@
+#ifndef ADVENTOFCODE_YEAR2019_DAY11_H
+#define ADVENTOFCODE_YEAR2019_DAY11_H
+
+#include <istream>
+
+#include "adventofcode.h"
+
+namespace day11 {
+  adventofcode::answer_t part1(std::istream& is);
+  adventofcode::answer_t part2(std::istream& is);
+}
+
+#endif
