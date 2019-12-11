069c2bf3fabde1466409b8634b75f021e5bc4754
Christian Persson <Saser@users.noreply.github.com>
2019-12-11 13:13:20 +0100

    cpp/2019/11: add solution for part 1

--- cpp/year2019/day11/test.cc
+++ cpp/year2019/day11/test.cc
@@ -9,7 +9,7 @@
 
 TEST(Year2019Day11, Part1Actual) {
   std::ifstream input("year2019/testdata/11");
-  std::string output = "";
+  std::string output = "2093";
   adventofcode::answer_t a = day11::part1(input);
   EXPECT_EQ("", a.error);
   EXPECT_EQ(output, a.answer);
