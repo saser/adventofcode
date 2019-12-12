00663320773ecac05069f8d2e6794c86ff89fccf
Christian Persson <Saser@users.noreply.github.com>
2019-12-12 21:11:29 +0100

    cpp/2019/12: add tests for part 2

--- cpp/year2019/day12/test.cc
+++ cpp/year2019/day12/test.cc
@@ -34,19 +34,29 @@ TEST(Year2019Day12, Part1Actual) {
   input.close();
 }
 
-// TEST(Year2019Day12, Part2Example1) {
-//   std::istringstream input("some input here");
-//   std::string output = "some output here";
-//   adventofcode::answer_t a = day12::part2(input);
-//   EXPECT_EQ("", a.error);
-//   EXPECT_EQ(output, a.answer);
-// }
+TEST(Year2019Day12, Part2Example1) {
+  std::ifstream input("year2019/day12/testdata/ex1");
+  std::string output = "2772";
+  adventofcode::answer_t a = day12::part2(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
 
-// TEST(Year2019Day12, Part2Actual) {
-//   std::ifstream input("year2019/testdata/12");
-//   std::string output = "some output here";
-//   adventofcode::answer_t a = day12::part2(input);
-//   EXPECT_EQ("", a.error);
-//   EXPECT_EQ(output, a.answer);
-//   input.close();
-// }
+TEST(Year2019Day12, Part2Example2) {
+  std::ifstream input("year2019/day12/testdata/ex2");
+  std::string output = "4686774924";
+  adventofcode::answer_t a = day12::part2(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
+
+TEST(Year2019Day12, Part2Actual) {
+  std::ifstream input("year2019/testdata/12");
+  std::string output = "";
+  adventofcode::answer_t a = day12::part2(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
