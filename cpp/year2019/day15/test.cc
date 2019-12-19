16b656be33c204018cb199dee73f41b3524b0cbe
Christian Persson <Saser@users.noreply.github.com>
2019-12-19 02:44:48 +0100

    cpp/2019/15: add placeholders, test, and benchmarks (parts 1 and 2)

---
+++ cpp/year2019/day15/test.cc
@@ -0,0 +1,34 @@
+#include "year2019/day15/day15.h"
+
+#include <fstream>
+#include <string>
+
+#include "gtest/gtest.h"
+
+#include "adventofcode.h"
+
+TEST(Year2019Day15, Part1Actual) {
+  std::ifstream input("year2019/testdata/15");
+  std::string output = "";
+  adventofcode::answer_t a = day15::part1(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
+
+// TEST(Year2019Day15, Part2Example1) {
+//   std::istringstream input("some input here");
+//   std::string output = "some output here";
+//   adventofcode::answer_t a = day15::part2(input);
+//   EXPECT_EQ("", a.error);
+//   EXPECT_EQ(output, a.answer);
+// }
+
+// TEST(Year2019Day15, Part2Actual) {
+//   std::ifstream input("year2019/testdata/15");
+//   std::string output = "some output here";
+//   adventofcode::answer_t a = day15::part2(input);
+//   EXPECT_EQ("", a.error);
+//   EXPECT_EQ(output, a.answer);
+//   input.close();
+// }
