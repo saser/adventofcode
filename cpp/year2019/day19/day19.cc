a1be4b97defecbf294e96a61bfb6a67f752a6e28
Christian Persson <Saser@users.noreply.github.com>
2019-12-22 00:05:10 +0100

    cpp/2019/19: refactor part 1 a bit

--- cpp/year2019/day19/day19.cc
+++ cpp/year2019/day19/day19.cc
@@ -6,6 +6,17 @@
 #include "adventofcode.h"
 #include "year2019/intcode/intcode.h"
 
+struct point_t {
+  int64_t x;
+  int64_t y;
+};
+
+struct tractor_t {
+  intcode::memory program;
+
+  int64_t test(point_t p);
+};
+
 adventofcode::answer_t solve(std::istream& is, int part);
 
 namespace day19 {
@@ -22,13 +33,20 @@ adventofcode::answer_t solve(std::istream& is, int part) {
   std::string line;
   std::getline(is, line);
   intcode::memory program = intcode::parse(line);
-  intcode::input inputs;
-  auto sum = 0;
-  for (int64_t x = 0; x < 50; x++) {
-    for (int64_t y = 0; y < 50; y++) {
-      auto [_, output] = intcode::run(program, {x, y});
-      sum += output[0];
+  tractor_t tractor {program};
+  if (part == 1) {
+    auto sum = 0;
+    for (int64_t x = 0; x < 50; x++) {
+      for (int64_t y = 0; y < 50; y++) {
+        sum += tractor.test(point_t {x, y});
+      }
     }
+    return adventofcode::ok(std::to_string(sum));
   }
-  return adventofcode::ok(std::to_string(sum));
+  return adventofcode::err("not implemented yet");
+}
+
+int64_t tractor_t::test(point_t p) {
+  auto [_, output] = intcode::run(program, {p.x, p.y});
+  return output[0];
 }
