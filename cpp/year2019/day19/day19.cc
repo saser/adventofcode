0352b8f8eda5cf7618871858c1ea6dfb6564de19
Christian Persson <Saser@users.noreply.github.com>
2019-12-21 20:34:06 +0100

    cpp/2019/19: add solution for part 1

--- cpp/year2019/day19/day19.cc
+++ cpp/year2019/day19/day19.cc
@@ -1,8 +1,10 @@
 #include "year2019/day19/day19.h"
 
 #include <istream>
+#include <string>
 
 #include "adventofcode.h"
+#include "year2019/intcode/intcode.h"
 
 adventofcode::answer_t solve(std::istream& is, int part);
 
@@ -17,5 +19,16 @@ namespace day19 {
 }
 
 adventofcode::answer_t solve(std::istream& is, int part) {
-  return adventofcode::err("not implemented yet");
+  std::string line;
+  std::getline(is, line);
+  intcode::memory program = intcode::parse(line);
+  intcode::input inputs;
+  auto sum = 0;
+  for (int64_t x = 0; x < 50; x++) {
+    for (int64_t y = 0; y < 50; y++) {
+      auto [_, output] = intcode::run(program, {x, y});
+      sum += output[0];
+    }
+  }
+  return adventofcode::ok(std::to_string(sum));
 }
