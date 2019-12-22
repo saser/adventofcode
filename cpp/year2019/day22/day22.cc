e952037749974b6ff1b9078079880cbaad08a694
Christian Persson <Saser@users.noreply.github.com>
2019-12-22 17:29:19 +0100

    cpp/2019/22: add placeholders, test, and benchmarks (parts 1 and 2)

---
+++ cpp/year2019/day22/day22.cc
@@ -0,0 +1,21 @@
+#include "year2019/day22/day22.h"
+
+#include <istream>
+
+#include "adventofcode.h"
+
+adventofcode::answer_t solve(std::istream& is, int part);
+
+namespace day22 {
+  adventofcode::answer_t part1(std::istream& is) {
+    return solve(is, 1);
+  }
+
+  adventofcode::answer_t part2(std::istream& is) {
+    return solve(is, 2);
+  }
+}
+
+adventofcode::answer_t solve(std::istream& is, int part) {
+  return adventofcode::err("not implemented yet");
+}
---
+++ cpp/year2019/day22/day22.h
@@ -0,0 +1,13 @@
+#ifndef ADVENTOFCODE_YEAR2019_DAY22_H
+#define ADVENTOFCODE_YEAR2019_DAY22_H
+
+#include <istream>
+
+#include "adventofcode.h"
+
+namespace day22 {
+  adventofcode::answer_t part1(std::istream& is);
+  adventofcode::answer_t part2(std::istream& is);
+}
+
+#endif
