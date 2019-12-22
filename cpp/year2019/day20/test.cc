bd527723b1607f332840dc362eaf857ac4177c9f
Christian Persson <Saser@users.noreply.github.com>
2019-12-22 14:58:30 +0100

    cpp/2019/20: add tests for part 2

--- cpp/year2019/day20/test.cc
+++ cpp/year2019/day20/test.cc
@@ -34,19 +34,20 @@ TEST(Year2019Day20, Part1Actual) {
   input.close();
 }
 
-// TEST(Year2019Day20, Part2Example1) {
-//   std::istringstream input("some input here");
-//   std::string output = "some output here";
-//   adventofcode::answer_t a = day20::part2(input);
-//   EXPECT_EQ("", a.error);
-//   EXPECT_EQ(output, a.answer);
-// }
+TEST(Year2019Day20, Part2Example1) {
+  std::ifstream input("year2019/day20/testdata/p2ex1");
+  std::string output = "396";
+  adventofcode::answer_t a = day20::part2(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
 
-// TEST(Year2019Day20, Part2Actual) {
-//   std::ifstream input("year2019/testdata/20");
-//   std::string output = "some output here";
-//   adventofcode::answer_t a = day20::part2(input);
-//   EXPECT_EQ("", a.error);
-//   EXPECT_EQ(output, a.answer);
-//   input.close();
-// }
+TEST(Year2019Day20, Part2Actual) {
+  std::ifstream input("year2019/testdata/20");
+  std::string output = "";
+  adventofcode::answer_t a = day20::part2(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
