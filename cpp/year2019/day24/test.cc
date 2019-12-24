ddc78934acda5726381403004a219a79cd8dd8cc
Christian Persson <Saser@users.noreply.github.com>
2019-12-24 22:13:58 +0100

    cpp/2019/24: add solution for part 2

--- cpp/year2019/day24/test.cc
+++ cpp/year2019/day24/test.cc
@@ -25,19 +25,11 @@ TEST(Year2019Day24, Part1Actual) {
   input.close();
 }
 
-// TEST(Year2019Day24, Part2Example1) {
-//   std::istringstream input("some input here");
-//   std::string output = "some output here";
-//   adventofcode::answer_t a = day24::part2(input);
-//   EXPECT_EQ("", a.error);
-//   EXPECT_EQ(output, a.answer);
-// }
-
-// TEST(Year2019Day24, Part2Actual) {
-//   std::ifstream input("year2019/testdata/24");
-//   std::string output = "some output here";
-//   adventofcode::answer_t a = day24::part2(input);
-//   EXPECT_EQ("", a.error);
-//   EXPECT_EQ(output, a.answer);
-//   input.close();
-// }
+TEST(Year2019Day24, Part2Actual) {
+  std::ifstream input("year2019/testdata/24");
+  std::string output = "2014";
+  adventofcode::answer_t a = day24::part2(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
