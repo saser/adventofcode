f2ec880ff96d7b9c50d3aa089e9c9a9725931666
Christian Persson <Saser@users.noreply.github.com>
2019-12-25 03:25:26 +0100

    cpp/2019/18: add tests for part 2

--- cpp/year2019/day18/test.cc
+++ cpp/year2019/day18/test.cc
@@ -61,19 +61,47 @@ TEST(Year2019Day18, Part1Actual) {
   input.close();
 }
 
-// TEST(Year2019Day18, Part2Example1) {
-//   std::istringstream input("some input here");
-//   std::string output = "some output here";
-//   adventofcode::answer_t a = day18::part2(input);
-//   EXPECT_EQ("", a.error);
-//   EXPECT_EQ(output, a.answer);
-// }
+TEST(Year2019Day18, Part2Example1) {
+  std::ifstream input("year2019/day18/testdata/p2ex1");
+  std::string output = "8";
+  adventofcode::answer_t a = day18::part2(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
 
-// TEST(Year2019Day18, Part2Actual) {
-//   std::ifstream input("year2019/testdata/18");
-//   std::string output = "some output here";
-//   adventofcode::answer_t a = day18::part2(input);
-//   EXPECT_EQ("", a.error);
-//   EXPECT_EQ(output, a.answer);
-//   input.close();
-// }
+TEST(Year2019Day18, Part2Example2) {
+  std::ifstream input("year2019/day18/testdata/p2ex2");
+  std::string output = "24";
+  adventofcode::answer_t a = day18::part2(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
+
+TEST(Year2019Day18, Part2Example3) {
+  std::ifstream input("year2019/day18/testdata/p2ex3");
+  std::string output = "32";
+  adventofcode::answer_t a = day18::part2(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
+
+TEST(Year2019Day18, Part2Example4) {
+  std::ifstream input("year2019/day18/testdata/p2ex4");
+  std::string output = "72";
+  adventofcode::answer_t a = day18::part2(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
+
+TEST(Year2019Day18, Part2Actual) {
+  std::ifstream input("year2019/testdata/18");
+  std::string output = "some output here";
+  adventofcode::answer_t a = day18::part2(input);
+  EXPECT_EQ("", a.error);
+  EXPECT_EQ(output, a.answer);
+  input.close();
+}
