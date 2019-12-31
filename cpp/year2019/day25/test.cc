f2b9b1aba23495288123e444e4b171eef36af47e
Christian Persson <Saser@users.noreply.github.com>
2019-12-31 15:26:04 +0100

    cpp/2019/25: remove code relating to part 2, as there is no part 2

--- cpp/year2019/day25/test.cc
+++ cpp/year2019/day25/test.cc
@@ -15,20 +15,3 @@ TEST(Year2019Day25, Part1Actual) {
   EXPECT_EQ(output, a.answer);
   input.close();
 }
-
-// TEST(Year2019Day25, Part2Example1) {
-//   std::istringstream input("some input here");
-//   std::string output = "some output here";
-//   adventofcode::answer_t a = day25::part2(input);
-//   EXPECT_EQ("", a.error);
-//   EXPECT_EQ(output, a.answer);
-// }
-
-// TEST(Year2019Day25, Part2Actual) {
-//   std::ifstream input("year2019/testdata/25");
-//   std::string output = "some output here";
-//   adventofcode::answer_t a = day25::part2(input);
-//   EXPECT_EQ("", a.error);
-//   EXPECT_EQ(output, a.answer);
-//   input.close();
-// }
