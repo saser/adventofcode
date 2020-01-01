a40b7b9a17e447938a2c83ab26a77848303388b2
Christian Persson <Saser@users.noreply.github.com>
2020-01-01 18:12:04 +0100

    go/2015/24: add placeholders, tests, and benchmarks (parts 1 and 2)

---
+++ go/internal/year2015/day24/day24.go
@@ -0,0 +1,18 @@
+package day24
+
+import (
+	"errors"
+	"io"
+)
+
+func Part1(r io.Reader) (string, error) {
+	return solve(r, 1)
+}
+
+func Part2(r io.Reader) (string, error) {
+	return solve(r, 2)
+}
+
+func solve(r io.Reader, part int) (string, error) {
+	return "", errors.New("not implemented yet")
+}
