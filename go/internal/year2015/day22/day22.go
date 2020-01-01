49234fde4105dee48691c554015fd31625a0ebb5
Christian Persson <Saser@users.noreply.github.com>
2020-01-01 14:02:54 +0100

    go/2015/22: add function to parse input as boss stats

--- go/internal/year2015/day22/day22.go
+++ go/internal/year2015/day22/day22.go
@@ -1,8 +1,12 @@
 package day22
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
 	"io"
+	"strconv"
+	"strings"
 )
 
 func Part1(r io.Reader) (string, error) {
@@ -14,5 +18,45 @@ func Part2(r io.Reader) (string, error) {
 }
 
 func solve(r io.Reader, part int) (string, error) {
+	boss, err := parse(r)
+	if err != nil {
+		return "", fmt.Errorf("year 2015, day 22, part %d: %w", part, err)
+	}
+	fmt.Printf("%#v\n", boss)
+	player := playerStats{
+		hitpoints: 50,
+		armor:     0,
+		mana:      500,
+	}
+	fmt.Printf("%#v\n", player)
 	return "", errors.New("not implemented yet")
+}
+
+type bossStats struct {
+	hitpoints int
+	damage    int
+}
+
+type playerStats struct {
+	hitpoints int
+	armor     int
+	mana      int
+}
+
+func parse(r io.Reader) (bossStats, error) {
+	sc := bufio.NewScanner(r)
+	sc.Split(bufio.ScanLines)
+	var values []int
+	for sc.Scan() {
+		parts := strings.Split(sc.Text(), ": ")
+		v, err := strconv.Atoi(parts[1])
+		if err != nil {
+			return bossStats{}, fmt.Errorf("parse: %w", err)
+		}
+		values = append(values, v)
+	}
+	return bossStats{
+		hitpoints: values[0],
+		damage:    values[1],
+	}, nil
 }
