5825d20cea667b55607cde83f5a68ae16bc412c6
Christian Persson <Saser@users.noreply.github.com>
2020-01-04 19:53:44 +0100

    java/2016/04: add placeholders, tests, benchmarks (parts 1 and 2)

---
+++ java/src/test/java/com/github/saser/adventofcode/year2016/day04/Day04Test.java
@@ -0,0 +1,48 @@
+package com.github.saser.adventofcode.year2016.day04;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class Day04Test {
+    @Test
+    public void part1Example() {
+        var input = new InputStreamReader(this.getClass().getResourceAsStream("example"));
+        var output = "1514";
+        var result = Day04.part1(input);
+        Assert.assertEquals("no error", "", result.error);
+        Assert.assertEquals("correct output", output, result.answer);
+    }
+
+    @Test
+    public void part1Actual() throws IOException {
+        try (var input = new FileReader("inputs/2016/04")) {
+            var output = "361724";
+            var result = Day04.part1(input);
+            Assert.assertEquals("no error", "", result.error);
+            Assert.assertEquals("correct output", output, result.answer);
+        }
+    }
+
+    // @Test
+    // public void part2Example() {
+    //     var input = new StringReader("");
+    //     var output = "";
+    //     var result = Day04.part2(input);
+    //     Assert.assertEquals("no error", "", result.error);
+    //     Assert.assertEquals("correct output", output, result.answer);
+    // }
+
+    // @Test
+    // public void part2Actual() throws IOException {
+    //     try (var input = new FileReader("inputs/2016/04")) {
+    //         var output = "";
+    //         var result = Day04.part2(input);
+    //         Assert.assertEquals("no error", "", result.error);
+    //         Assert.assertEquals("correct output", output, result.answer);
+    //     }
+    // }
+}
