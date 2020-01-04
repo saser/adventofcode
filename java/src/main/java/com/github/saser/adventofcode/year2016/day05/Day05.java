132b0a39f3c57f3773b0caa68e54ea354fd84797
Christian Persson <Saser@users.noreply.github.com>
2020-01-05 00:19:14 +0100

    java/2016/05: add solution for part 1

--- java/src/main/java/com/github/saser/adventofcode/year2016/day05/Day05.java
+++ java/src/main/java/com/github/saser/adventofcode/year2016/day05/Day05.java
@@ -1,6 +1,10 @@
 package com.github.saser.adventofcode.year2016.day05;
 
+import java.io.BufferedReader;
 import java.io.Reader;
+import java.security.MessageDigest;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
 
 import com.github.saser.adventofcode.Result;
 
@@ -14,6 +18,35 @@ public final class Day05 {
     }
 
     private static Result solve(Reader r, int part) {
-        return Result.err("not implemented yet");
+        try {
+            var br = new BufferedReader(r);
+            var doorID = br.readLine();
+            var digests = interestingDigests(doorID);
+            return Result.ok(password(digests));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.err(e.getMessage());
+        }
+    }
+
+    private static Stream<byte[]> interestingDigests(String doorID) throws Exception {
+        var md = MessageDigest.getInstance("MD5");
+        return Stream.iterate(0, (i) -> i + 1)
+            .map((i) -> md.digest((doorID + i).getBytes()))
+            .filter(Day05::isInteresting);
+    }
+
+    private static boolean isInteresting(byte[] digest) {
+        return digest[0] == 0x00 && digest[1] == 0x00 && digest[2] >= 0x00 && digest[2] <= 0x0f;
+    }
+
+    private static String password(Stream<byte[]> digests) {
+        return digests
+            .limit(8)
+            .map((digest) -> Character.forDigit(digest[2], 16))
+            .collect(Collector.of(StringBuilder::new,
+                                  StringBuilder::append,
+                                  StringBuilder::append,
+                                  StringBuilder::toString));
     }
 }
