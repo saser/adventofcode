beac1d235b3226b2eb70f54b9f81e24365a1e2e6
Christian Persson <Saser@users.noreply.github.com>
2020-01-04 01:17:33 +0100

    aocgen/templates/java: add templates for test files

---
+++ aocgen/templates/java/src/test/java/com/github/saser/adventofcode/yearYYYY/dayDD/DayDDTest.java
@@ -0,0 +1,48 @@
+package com.github.saser.adventofcode.{{.FullYear}}.{{.FullDay}};
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class Day{{.PaddedDay}}Test {
+    @Test
+    public void part1Example() {
+        var input = new StringReader("");
+        var output = "";
+        var result = Day{{.PaddedDay}}.part1(input);
+        Assert.assertEquals("no error", "", result.error);
+        Assert.assertEquals("correct output", output, result.answer);
+    }
+
+    @Test
+    public void part1Actual() throws IOException {
+        try (var input = new FileReader("inputs/{{.Year}}/{{.PaddedDay}}")) {
+            var output = "";
+            var result = Day{{.PaddedDay}}.part1(input);
+            Assert.assertEquals("no error", "", result.error);
+            Assert.assertEquals("correct output", output, result.answer);
+        }
+    }
+
+    // @Test
+    // public void part2Example() {
+    //     var input = new StringReader("");
+    //     var output = "";
+    //     var result = Day{{.PaddedDay}}.part2(input);
+    //     Assert.assertEquals("no error", "", result.error);
+    //     Assert.assertEquals("correct output", output, result.answer);
+    // }
+
+    // @Test
+    // public void part2Actual() {
+    //     try (var input = new FileReader("inputs/{{.Year}}/{{.PaddedDay}}")) {
+    //         var output = "";
+    //         var result = Day{{.PaddedDay}}.part2(input);
+    //         Assert.assertEquals("no error", "", result.error);
+    //         Assert.assertEquals("correct output", output, result.answer);
+    //     }
+    // }
+}
