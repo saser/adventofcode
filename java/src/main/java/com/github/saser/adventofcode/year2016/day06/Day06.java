8182d88e629026e052459565fb8c34a5a1978a13
Christian Persson <Saser@users.noreply.github.com>
2020-01-05 17:35:30 +0100

    java/2016/06: add solution for part 1

--- java/src/main/java/com/github/saser/adventofcode/year2016/day06/Day06.java
+++ java/src/main/java/com/github/saser/adventofcode/year2016/day06/Day06.java
@@ -1,6 +1,12 @@
 package com.github.saser.adventofcode.year2016.day06;
 
+import java.io.BufferedReader;
 import java.io.Reader;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.github.saser.adventofcode.Result;
 
@@ -14,6 +20,38 @@ public final class Day06 {
     }
 
     private static Result solve(Reader r, int part) {
-        return Result.err("not implemented yet");
+        var br = new BufferedReader(r);
+        var message = transpose(br.lines())
+                .map(Day06::count)
+                .map(Day06::maxByValue)
+                .collect(Collector.of( StringBuilder::new, StringBuilder::append, StringBuilder::append, StringBuilder::toString));
+        return Result.ok(message);
+    }
+
+    private static Stream<String> transpose(Stream<String> lines) {
+        var m = lines.map(String::toCharArray).collect(Collectors.toList()).toArray(new char[0][0]);
+        var nRows = m.length;
+        var nCols = m[0].length;
+        var mt = new char[nCols][nRows];
+        for (var row = 0; row < nRows; row++) {
+            for (var col = 0; col < nCols; col++) {
+                mt[col][row] = m[row][col];
+            }
+        }
+        return Arrays.stream(mt).map(String::new);
+    }
+
+    private static Map<Character, Long> count(String s) {
+        return s.chars()
+                .mapToObj((c) -> (char) c)
+                .collect(Collectors.groupingBy((c) -> c, Collectors.counting()));
+    }
+
+    private static <K, V extends Comparable<? super V>> K maxByValue(Map<K, V> map) {
+        return map.entrySet()
+                .stream()
+                .max(Map.Entry.<K, V>comparingByValue())
+                .get()
+                .getKey();
     }
 }
