c75be687eecd42b20ad8c7c3f113292afc0801c0
Christian Persson <Saser@users.noreply.github.com>
2020-01-05 18:23:07 +0100

    java/2016/07: add solution for part 1

--- java/src/main/java/com/github/saser/adventofcode/year2016/day07/Day07.java
+++ java/src/main/java/com/github/saser/adventofcode/year2016/day07/Day07.java
@@ -1,6 +1,10 @@
 package com.github.saser.adventofcode.year2016.day07;
 
+import java.io.BufferedReader;
 import java.io.Reader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import com.github.saser.adventofcode.Result;
 
@@ -14,6 +18,49 @@ public final class Day07 {
     }
 
     private static Result solve(Reader r, int part) {
-        return Result.err("not implemented yet");
+        var br = new BufferedReader(r);
+        var count = br.lines()
+                .map(Address::parse)
+                .filter(Address::supportsTLS)
+                .count();
+        return Result.ok(Long.toString(count));
+    }
+
+    private static class Address {
+        public final List<String> supernets;
+        public final List<String> hypernets;
+
+        public Address(List<String> supernets, List<String> hypernets) {
+            this.supernets = supernets;
+            this.hypernets = hypernets;
+        }
+
+        public static Address parse(String line) {
+            var parts = line
+                    .replace("[", "/_")
+                    .replace(']', '/')
+                    .split("/");
+            var split = Arrays.stream(parts)
+                    .collect(Collectors.partitioningBy((s) -> s.startsWith("_")));
+            var supernets = split.get(false);
+            var hypernets = split.get(true);
+            return new Address(supernets, hypernets);
+        }
+
+        public boolean supportsTLS() {
+            var hypernetsContainsABBA = this.hypernets.stream().anyMatch(Address::containsABBA);
+            var supernetsContainsABBA = this.supernets.stream().anyMatch(Address::containsABBA);
+            return !hypernetsContainsABBA && supernetsContainsABBA;
+        }
+
+        private static boolean containsABBA(String s) {
+            var chars = s.toCharArray();
+            for (var i = 0; i < chars.length - 3; i++) {
+                if (chars[i] == chars[i + 3] && chars[i + 1] == chars[i + 2] && chars[i] != chars[i + 1]) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
