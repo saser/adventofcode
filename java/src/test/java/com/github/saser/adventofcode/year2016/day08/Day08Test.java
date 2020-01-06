c5371d8e6ce97c495968e44be77cc7e50364f12c
Christian Persson <Saser@users.noreply.github.com>
2020-01-06 12:09:46 +0100

    java/2016/08: add solution for part 2

--- java/src/test/java/com/github/saser/adventofcode/year2016/day08/Day08Test.java
+++ java/src/test/java/com/github/saser/adventofcode/year2016/day08/Day08Test.java
@@ -1,8 +1,8 @@
 package com.github.saser.adventofcode.year2016.day08;
 
+import java.io.BufferedInputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.StringReader;
 
 import org.junit.Test;
 import org.junit.Assert;
@@ -18,22 +18,15 @@ public class Day08Test {
         }
     }
 
-    // @Test
-    // public void part2Example() {
-    //     var input = new StringReader("");
-    //     var output = "";
-    //     var result = Day08.part2(input);
-    //     Assert.assertEquals("no error", "", result.error);
-    //     Assert.assertEquals("correct output", output, result.answer);
-    // }
-
-    // @Test
-    // public void part2Actual() throws IOException {
-    //     try (var input = new FileReader("inputs/2016/08")) {
-    //         var output = "";
-    //         var result = Day08.part2(input);
-    //         Assert.assertEquals("no error", "", result.error);
-    //         Assert.assertEquals("correct output", output, result.answer);
-    //     }
-    // }
+     @Test
+     public void part2Actual() throws IOException {
+         try (var input = new FileReader("inputs/2016/08")) {
+             var outputResource = this.getClass().getResourceAsStream("output");
+             var outputBytes = new BufferedInputStream(outputResource).readAllBytes();
+             var output = new String(outputBytes);
+             var result = Day08.part2(input);
+             Assert.assertEquals("no error", "", result.error);
+             Assert.assertEquals("correct output", output, result.answer);
+         }
+     }
 }
