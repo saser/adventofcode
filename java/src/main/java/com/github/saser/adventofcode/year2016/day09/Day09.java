5a528499a7db557e2cbf5cb5473034b84da676b3
Christian Persson <Saser@users.noreply.github.com>
2020-01-06 12:49:18 +0100

    java/2016/09: add solution for part 1

--- java/src/main/java/com/github/saser/adventofcode/year2016/day09/Day09.java
+++ java/src/main/java/com/github/saser/adventofcode/year2016/day09/Day09.java
@@ -1,5 +1,6 @@
 package com.github.saser.adventofcode.year2016.day09;
 
+import java.io.BufferedReader;
 import java.io.Reader;
 
 import com.github.saser.adventofcode.Result;
@@ -14,6 +15,43 @@ public final class Day09 {
     }
 
     private static Result solve(Reader r, int part) {
-        return Result.err("not implemented yet");
+        try {
+            var br = new BufferedReader(r);
+            var input = br.readLine();
+            var decompressed = decompress(input);
+            return Result.ok(Integer.toString(decompressed.length()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.err(e.getMessage());
+        }
+    }
+
+    private static String decompress(String s) {
+        var sb = new StringBuilder(s.length());
+        for (var i = 0; i < s.length(); i++) {
+            var markerStart = s.indexOf('(', i);
+            if (markerStart == -1) {
+                sb.append(s.substring(i));
+                break;
+            }
+            sb.append(s, i, markerStart);
+            var markerEnd = s.indexOf(')', markerStart);
+            var marker = parseMarker(s.substring(markerStart + 1, markerEnd));
+            var dataStart = markerEnd + 1;
+            var dataEnd = markerEnd + 1 + marker[0];
+            var data = s.substring(dataStart, dataEnd);
+            for (var r = 0; r < marker[1]; r++) {
+                sb.append(data);
+            }
+            i = dataEnd - 1;
+        }
+        return sb.toString();
+    }
+
+    private static int[] parseMarker(String marker) {
+        var parts = marker.split("x");
+        var length = Integer.parseInt(parts[0]);
+        var repeat = Integer.parseInt(parts[1]);
+        return new int[] {length, repeat};
     }
 }
