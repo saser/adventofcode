12fb5c7c8571407583d07692194a95aefa2a779d
Christian Persson <Saser@users.noreply.github.com>
2020-01-08 01:10:51 +0100

    java/geo: add `equals`, `hashCode`, and `toString` to `Point2D`

--- java/src/main/java/com/github/saser/adventofcode/geo/Point2D.java
+++ java/src/main/java/com/github/saser/adventofcode/geo/Point2D.java
@@ -1,5 +1,7 @@
 package com.github.saser.adventofcode.geo;
 
+import java.util.Objects;
+
 public final class Point2D {
     public int x;
     public int y;
@@ -21,6 +23,25 @@ public final class Point2D {
     public void add(Point2D other) {
         this.x += other.x;
         this.y += other.y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point2D point2D = (Point2D) o;
+        return x == point2D.x &&
+                y == point2D.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", this.x, this.y);
     }
 
     @Override
