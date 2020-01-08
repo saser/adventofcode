9ff4ec92158300467fcef0d3887fb49695709d7e
Christian Persson <Saser@users.noreply.github.com>
2020-01-08 01:19:19 +0100

    java/2016/13: add solution for part 2

--- java/src/test/java/com/github/saser/adventofcode/year2016/day13/Day13Test.java
+++ java/src/test/java/com/github/saser/adventofcode/year2016/day13/Day13Test.java
@@ -17,22 +17,13 @@ public class Day13Test {
         }
     }
 
-    // @Test
-    // public void part2Example() {
-    //     var input = new StringReader("");
-    //     var output = "";
-    //     var result = Day13.part2(input);
-    //     Assert.assertEquals("no error", "", result.error);
-    //     Assert.assertEquals("correct output", output, result.answer);
-    // }
-
-    // @Test
-    // public void part2Actual() throws IOException {
-    //     try (var input = new FileReader("inputs/2016/13")) {
-    //         var output = "";
-    //         var result = Day13.part2(input);
-    //         Assert.assertEquals("no error", "", result.error);
-    //         Assert.assertEquals("correct output", output, result.answer);
-    //     }
-    // }
+     @Test
+     public void part2Actual() throws IOException {
+         try (var input = new FileReader("inputs/2016/13")) {
+             var output = "124";
+             var result = Day13.part2(input);
+             Assert.assertEquals("no error", "", result.error);
+             Assert.assertEquals("correct output", output, result.answer);
+         }
+     }
 }
