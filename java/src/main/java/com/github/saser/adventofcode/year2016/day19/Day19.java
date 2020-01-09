4e693e282dd22f00bd93f42956d5a4003a219bf6
Christian Persson <Saser@users.noreply.github.com>
2020-01-09 22:19:44 +0100

    java/2016/19: add solution for part 1

--- java/src/main/java/com/github/saser/adventofcode/year2016/day19/Day19.java
+++ java/src/main/java/com/github/saser/adventofcode/year2016/day19/Day19.java
@@ -1,6 +1,9 @@
 package com.github.saser.adventofcode.year2016.day19;
 
+import java.io.BufferedReader;
 import java.io.Reader;
+import java.util.Arrays;
+import java.util.Optional;
 
 import com.github.saser.adventofcode.Result;
 
@@ -14,6 +17,39 @@ public final class Day19 {
     }
 
     private static Result solve(Reader r, int part) {
-        return Result.err("not implemented yet");
+        var input = new BufferedReader(r)
+                .lines()
+                .findFirst()
+                .get();
+        var n = Integer.parseInt(input);
+        var presents = Day19.play(n);
+        var winner = Day19.next(presents, 0) + 1;
+        return Result.ok(Integer.toString(winner));
+    }
+
+    private static int[] play(int n) {
+        var presents = new int[n];
+        Arrays.fill(presents, 1);
+        var i = 0;
+        while (true) {
+            var next = Day19.next(presents, i);
+            if (next == i) {
+                break;
+            }
+            presents[i] += presents[next];
+            presents[next] = 0;
+            i = Day19.next(presents, i);
+        }
+        return presents;
+    }
+
+    private static int next(int[] presents, int start) {
+        var n = presents.length;
+        for (var i = (start + 1) % n; i != start; i = (i + 1) % n) {
+            if (presents[i] > 0) {
+                return i;
+            }
+        }
+        return start;
     }
 }
