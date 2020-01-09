46a1371c7bd54e8432da7791d6065741620be91e
Christian Persson <Saser@users.noreply.github.com>
2020-01-09 12:09:44 +0100

    java/2016/17: add solution for part 1

--- java/src/main/java/com/github/saser/adventofcode/year2016/day17/Day17.java
+++ java/src/main/java/com/github/saser/adventofcode/year2016/day17/Day17.java
@@ -1,8 +1,15 @@
 package com.github.saser.adventofcode.year2016.day17;
 
+import java.io.BufferedReader;
 import java.io.Reader;
+import java.security.MessageDigest;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
 
 import com.github.saser.adventofcode.Result;
+import com.github.saser.adventofcode.geo.Point2D;
+import com.github.saser.adventofcode.tuple.Tuple2;
 
 public final class Day17 {
     public static Result part1(Reader r) {
@@ -14,6 +21,85 @@ public final class Day17 {
     }
 
     private static Result solve(Reader r, int part) {
-        return Result.err("not implemented yet");
+        try {
+            var passcode = new BufferedReader(r).readLine();
+            var maze = new Maze(passcode);
+            var path = maze.findVault(new Point2D(3, 3));
+            return Result.ok(path);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.err(e.getMessage());
+        }
+    }
+
+    private static class Maze {
+        public final String passcode;
+        private final MessageDigest md;
+
+        public Maze(String passcode) throws Exception {
+            this.passcode = passcode;
+            this.md = MessageDigest.getInstance("MD5");
+        }
+
+        public String findVault(Point2D target) {
+            var queue = new LinkedList<Tuple2<Point2D, String>>();
+            queue.add(new Tuple2<>(new Point2D(0, 0), ""));
+            while (!queue.isEmpty()) {
+                var tuple = queue.remove();
+                var point = tuple.v1;
+                var path = tuple.v2;
+                if (point.equals(target)) {
+                    return path;
+                }
+                for (var nextStep : this.nextSteps(path)) {
+                    var delta = new Point2D(0, 0);
+                    switch (nextStep) {
+                        case 'U':
+                            delta.y = -1;
+                            break;
+                        case 'D':
+                            delta.y = 1;
+                            break;
+                        case 'L':
+                            delta.x = -1;
+                            break;
+                        case 'R':
+                            delta.x = 1;
+                            break;
+                    }
+                    var nextPoint = point.plus(delta);
+                    if (nextPoint.x < 0 || nextPoint.x > 3 || nextPoint.y < 0 || nextPoint.y > 3) {
+                        continue;
+                    }
+                    queue.add(new Tuple2<>(nextPoint, path + nextStep));
+                }
+            }
+            throw new Error("unreachable");
+        }
+
+        private Set<Character> nextSteps(String path) {
+            var toHash = this.passcode + path;
+            var hash = Day17.hexString(md.digest((this.passcode + path).getBytes()));
+            var doors = "UDLR".toCharArray();
+            var openChars = "bcdef";
+            var openDoors = new HashSet<Character>();
+            for (var i = 0; i < 4; i++) {
+                if (openChars.contains(hash.substring(i, i + 1))) {
+                    openDoors.add(doors[i]);
+                }
+            }
+            return openDoors;
+        }
+    }
+
+    private static String hexString(byte[] bytes) {
+        final var HEX_CHARS = "0123456789abcdef".toCharArray();
+        var chars = new char[2 * bytes.length];
+        for (var i = 0; i < bytes.length; i++) {
+            var b = bytes[i] & 0xFF;
+            chars[2 * i] = HEX_CHARS[b >>> 4]; // upper nibble
+            chars[2 * i + 1] = HEX_CHARS[b & 0x0F]; // lower nibble;
+        }
+        return new String(chars);
     }
 }
