2d122828d8c29b3b362829451c82186c76ec0392
Christian Persson <Saser@users.noreply.github.com>
2020-01-09 12:47:26 +0100

    java/2016/18: add solution for part 1

--- java/src/test/java/com/github/saser/adventofcode/year2016/day18/Day18Test.java
+++ java/src/test/java/com/github/saser/adventofcode/year2016/day18/Day18Test.java
@@ -10,7 +10,7 @@ public class Day18Test {
     @Test
     public void part1Actual() throws IOException {
         try (var input = new FileReader("inputs/2016/18")) {
-            var output = "";
+            var output = "2016";
             var result = Day18.part1(input);
             Assert.assertEquals("no error", "", result.error);
             Assert.assertEquals("correct output", output, result.answer);
