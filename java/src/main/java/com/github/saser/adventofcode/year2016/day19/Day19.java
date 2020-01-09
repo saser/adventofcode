db8600673733f86db0fd4ab8c64db04f906aca64
Christian Persson <Saser@users.noreply.github.com>
2020-01-09 23:49:17 +0100

    java/2016/19: add much more improved solution for part 1

--- java/src/main/java/com/github/saser/adventofcode/year2016/day19/Day19.java
+++ java/src/main/java/com/github/saser/adventofcode/year2016/day19/Day19.java
@@ -2,8 +2,6 @@ package com.github.saser.adventofcode.year2016.day19;
 
 import java.io.BufferedReader;
 import java.io.Reader;
-import java.util.Arrays;
-import java.util.Optional;
 
 import com.github.saser.adventofcode.Result;
 
@@ -22,33 +20,17 @@ public final class Day19 {
                 .findFirst()
                 .get();
         var n = Integer.parseInt(input);
-        var presents = Day19.play(n);
-        var winner = Day19.next(presents, 0) + 1;
+        var winner = play(n);
         return Result.ok(Integer.toString(winner));
     }
 
-    private static int[] play(int n) {
-        var presents = new int[n];
-        Arrays.fill(presents, 1);
-        var i = 0;
-        while (true) {
-            var next = Day19.next(presents, i);
-            if (next == i) {
-                break;
-            }
-            presents[i] += presents[next];
-            presents[next] = 0;
-            i = Day19.next(presents, i);
-        }
-        return presents;
-    }
-
-    private static int next(int[] presents, int start) {
-        var n = presents.length;
-        for (var i = (start + 1) % n; i != start; i = (i + 1) % n) {
-            if (presents[i] > 0) {
-                return i;
-            }
+    private static int play(int n) {
+        var start = 1;
+        var delta = 2;
+        while (n > 1) {
+            start += (n % 2) * delta;
+            delta *= 2;
+            n /= 2;
         }
         return start;
     }
