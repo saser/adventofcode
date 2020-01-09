ff45b1b0207c09667e5c9d1299bd325719d6512f
Christian Persson <Saser@users.noreply.github.com>
2020-01-09 12:47:26 +0100

    java/2016/18: add solution for part 1

--- java/src/main/java/com/github/saser/adventofcode/year2016/day18/Day18.java
+++ java/src/main/java/com/github/saser/adventofcode/year2016/day18/Day18.java
@@ -1,6 +1,8 @@
 package com.github.saser.adventofcode.year2016.day18;
 
+import java.io.BufferedReader;
 import java.io.Reader;
+import java.util.stream.Stream;
 
 import com.github.saser.adventofcode.Result;
 
@@ -14,6 +16,45 @@ public final class Day18 {
     }
 
     private static Result solve(Reader r, int part) {
-        return Result.err("not implemented yet");
+        var state = Day18.parse(r);
+        var states = Stream.iterate(state, Day18::next);
+        var sum = states
+                .limit(40)
+                .mapToInt(Day18::count)
+                .sum();
+        return Result.ok(Integer.toString(sum));
+    }
+
+    private static boolean[] parse(Reader r) {
+        var line = new BufferedReader(r)
+            .lines()
+            .findFirst()
+            .get()
+            .toCharArray();
+        var state = new boolean[line.length];
+        for (var i = 0; i < state.length; i++) {
+            state[i] = line[i] == '^';
+        }
+        return state;
+    }
+
+    private static int count(boolean[] state) {
+        var count = 0;
+        for (var trap : state) {
+            if (!trap) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static boolean[] next(boolean[] state) {
+        var next = new boolean[state.length];
+        for (var i = 0; i < state.length; i++) {
+            var left = i != 0 && state[i - 1];
+            var right = i != state.length - 1 && state[i + 1];
+            next[i] = !right == left;
+        }
+        return next;
     }
 }
