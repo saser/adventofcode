4513d1b6f96468612ad9f6e514901702c27db140
Christian Persson <Saser@users.noreply.github.com>
2020-01-12 15:37:30 +0100

    java/2016/21: add placeholders, tests, and benchmarks

---
+++ java/src/test/java/com/github/saser/adventofcode/year2016/day21/Day21Test.java
@@ -0,0 +1,39 @@
+package com.github.saser.adventofcode.year2016.day21;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class Day21Test {
+    @Test
+    public void part1Actual() throws IOException {
+        try (var input = new FileReader("inputs/2016/21")) {
+            var output = "";
+            var result = Day21.part1(input);
+            Assert.assertEquals("no error", "", result.error);
+            Assert.assertEquals("correct output", output, result.answer);
+        }
+    }
+
+    // @Test
+    // public void part2Example() {
+    //     var input = new StringReader("");
+    //     var output = "";
+    //     var result = Day21.part2(input);
+    //     Assert.assertEquals("no error", "", result.error);
+    //     Assert.assertEquals("correct output", output, result.answer);
+    // }
+
+    // @Test
+    // public void part2Actual() throws IOException {
+    //     try (var input = new FileReader("inputs/2016/21")) {
+    //         var output = "";
+    //         var result = Day21.part2(input);
+    //         Assert.assertEquals("no error", "", result.error);
+    //         Assert.assertEquals("correct output", output, result.answer);
+    //     }
+    // }
+}
