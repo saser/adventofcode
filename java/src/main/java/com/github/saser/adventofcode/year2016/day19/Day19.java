520a9260643f14a13833478c6f50d0ee8b8afbd9
Christian Persson <Saser@users.noreply.github.com>
2020-01-12 12:34:45 +0100

    java/2016/19: add solution for part 2

--- java/src/main/java/com/github/saser/adventofcode/year2016/day19/Day19.java
+++ java/src/main/java/com/github/saser/adventofcode/year2016/day19/Day19.java
@@ -2,6 +2,10 @@ package com.github.saser.adventofcode.year2016.day19;
 
 import java.io.BufferedReader;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import com.github.saser.adventofcode.Result;
 
@@ -20,12 +24,35 @@ public final class Day19 {
                 .findFirst()
                 .get();
         var n = Integer.parseInt(input);
-        var winner = play(n);
+        int winner;
+        if (part == 1) {
+            winner = play1(n);
+        } else {
+            winner = play2(n);
+        }
         return Result.ok(Integer.toString(winner));
     }
 
-    private static int play(int n) {
+    private static int play1(int n) {
         int k = Integer.highestOneBit(n);
         return ((n - k) << 1) + 1;
+    }
+
+    private static int play2(int n) {
+        List<Integer> elves = new ArrayList<>(n);
+        for (var i = 1; i <= n; i++) {
+            elves.add(i);
+        }
+        while (elves.size() > 1) {
+            while (elves.size() % 3 != 0) {
+                elves.remove(elves.size() / 2);
+                elves.add(elves.remove(0));
+            }
+            elves = IntStream.rangeClosed(0, elves.size())
+                    .filter(idx -> idx % 3 == 2)
+                    .mapToObj(elves::get)
+                    .collect(Collectors.toList());
+        }
+        return elves.get(0);
     }
 }
