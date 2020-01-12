a9127b969f102fdd2f965450134f29525344c50a
Christian Persson <Saser@users.noreply.github.com>
2020-01-12 19:25:12 +0100

    java/2016/22: add placeholders, tests, and benchmarks

---
+++ java/src/main/java/com/github/saser/adventofcode/year2016/day22/Day22.java
@@ -0,0 +1,20 @@
+package com.github.saser.adventofcode.year2016.day22;
+
+import java.io.Reader;
+import java.util.stream.Stream;
+
+import com.github.saser.adventofcode.Result;
+
+public final class Day22 {
+    public static Result part1(Reader r) {
+        return solve(r, 1);
+    }
+
+    public static Result part2(Reader r) {
+        return solve(r, 2);
+    }
+
+    private static Result solve(Reader r, int part) {
+        return Result.err("not implemented yet");
+    }
+}
