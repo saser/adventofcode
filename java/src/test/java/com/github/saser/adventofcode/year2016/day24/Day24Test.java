80eb8d0331f4084d04d27f507a0679ee06c1bd07
Christian Persson <Saser@users.noreply.github.com>
2020-01-13 17:37:56 +0100

    java/2016/24: add solution for part 1

--- java/src/test/java/com/github/saser/adventofcode/year2016/day24/Day24Test.java
+++ java/src/test/java/com/github/saser/adventofcode/year2016/day24/Day24Test.java
@@ -20,7 +20,7 @@ public class Day24Test {
     @Test
     public void part1Actual() throws IOException {
         try (var input = new FileReader("inputs/2016/24")) {
-            var output = "";
+            var output = "470";
             var result = Day24.part1(input);
             Assert.assertEquals("no error", "", result.error);
             Assert.assertEquals("correct output", output, result.answer);
