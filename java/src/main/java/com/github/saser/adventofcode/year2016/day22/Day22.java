7e437f1f8aa7db1c8bf2bbd6de51a64cba384680
Christian Persson <Saser@users.noreply.github.com>
2020-01-13 01:04:54 +0100

    java/2016/22: add solution for part 2

--- java/src/main/java/com/github/saser/adventofcode/year2016/day22/Day22.java
+++ java/src/main/java/com/github/saser/adventofcode/year2016/day22/Day22.java
@@ -3,6 +3,8 @@ package com.github.saser.adventofcode.year2016.day22;
 import java.io.BufferedReader;
 import java.io.Reader;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
 import java.util.regex.Pattern;
@@ -10,6 +12,8 @@ import java.util.stream.Collectors;
 
 import com.github.saser.adventofcode.Result;
 import com.github.saser.adventofcode.geo.Point2D;
+import com.github.saser.adventofcode.tuple.Tuple2;
+import com.github.saser.adventofcode.tuple.Tuple3;
 
 public final class Day22 {
     public static Result part1(Reader r) {
@@ -22,8 +26,20 @@ public final class Day22 {
 
     private static Result solve(Reader r, int part) {
         var grid = Grid.parse(r);
-        var count = grid.findViable().size();
-        return Result.ok(Integer.toString(count));
+        if (part == 1) {
+            var count = grid.findViable().size();
+            return Result.ok(Integer.toString(count));
+        }
+        var maxX = grid.nodes
+                .keySet()
+                .stream()
+                .mapToInt(point -> point.x)
+                .max()
+                .getAsInt();
+        var from = new Point2D(maxX, 0);
+        var to = new Point2D(0, 0);
+        var steps = grid.moveData(from, to);
+        return Result.ok(Integer.toString(steps));
     }
 
     private static class Node {
@@ -96,6 +112,54 @@ public final class Day22 {
                     .filter(entry -> Node.viable(entry.getValue(), emptyNode))
                     .map(Map.Entry::getKey)
                     .collect(Collectors.toSet());
+        }
+
+        public int moveData(Point2D from, Point2D to) {
+            var queue = new LinkedList<Tuple3<Point2D, Point2D, Integer>>();
+            var empty = this.findEmpty();
+            queue.add(new Tuple3<>(empty, from.clone(), 0));
+            var visited = new HashSet<Tuple2<Point2D, Point2D>>();
+            var maxX = 0;
+            var maxY = 0;
+            for (var point : this.nodes.keySet()) {
+                maxX = Math.max(maxX, point.x);
+                maxY = Math.max(maxY, point.y);
+            }
+            var viable = this.findViable();
+            viable.add(empty);
+            while (!queue.isEmpty()) {
+                var tuple = queue.remove();
+                var emptyPosition = tuple.v1;
+                var dataPosition = tuple.v2;
+                var state = new Tuple2<>(emptyPosition, dataPosition);
+                if (visited.contains(state)) {
+                    continue;
+                }
+                visited.add(state);
+                var steps = tuple.v3;
+                if (dataPosition.equals(to)) {
+                    return steps;
+                }
+                var neighbors = new Point2D[] {
+                        emptyPosition.plus(new Point2D(1, 0)),
+                        emptyPosition.plus(new Point2D(-1, 0)),
+                        emptyPosition.plus(new Point2D(0, 1)),
+                        emptyPosition.plus(new Point2D(0, -1)),
+                };
+                for (var neighbor : neighbors) {
+                    var nx = neighbor.x;
+                    var ny = neighbor.y;
+                    if (nx < 0 || nx > maxX || ny < 0 || ny > maxY) {
+                        continue;
+                    }
+                    if (!viable.contains(neighbor)) {
+                        continue;
+                    }
+                    var newDataPosition = (neighbor.equals(dataPosition) ? emptyPosition : dataPosition).clone();
+                    queue.add(new Tuple3<>(neighbor, newDataPosition, steps + 1));
+                }
+            }
+            return -1;
         }
     }
 }
--- java/src/test/java/com/github/saser/adventofcode/year2016/day22/Day22Test.java
+++ java/src/test/java/com/github/saser/adventofcode/year2016/day22/Day22Test.java
@@ -2,6 +2,7 @@ package com.github.saser.adventofcode.year2016.day22;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.junit.Test;
 import org.junit.Assert;
@@ -17,22 +18,22 @@ public class Day22Test {
         }
     }
 
-    // @Test
-    // public void part2Example() {
-    //     var input = new StringReader("");
-    //     var output = "";
-    //     var result = Day22.part2(input);
-    //     Assert.assertEquals("no error", "", result.error);
-    //     Assert.assertEquals("correct output", output, result.answer);
-    // }
+     @Test
+     public void part2Example() {
+         var input = new InputStreamReader(this.getClass().getResourceAsStream("example"));
+         var output = "7";
+         var result = Day22.part2(input);
+         Assert.assertEquals("no error", "", result.error);
+         Assert.assertEquals("correct output", output, result.answer);
+     }
 
-    // @Test
-    // public void part2Actual() throws IOException {
-    //     try (var input = new FileReader("inputs/2016/22")) {
-    //         var output = "";
-    //         var result = Day22.part2(input);
-    //         Assert.assertEquals("no error", "", result.error);
-    //         Assert.assertEquals("correct output", output, result.answer);
-    //     }
-    // }
+     @Test
+     public void part2Actual() throws IOException {
+         try (var input = new FileReader("inputs/2016/22")) {
+             var output = "236";
+             var result = Day22.part2(input);
+             Assert.assertEquals("no error", "", result.error);
+             Assert.assertEquals("correct output", output, result.answer);
+         }
+     }
 }
