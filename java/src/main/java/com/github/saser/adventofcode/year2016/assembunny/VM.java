9c303f199e5ae07a4220a2581269ef0b15a873bd
Christian Persson <Saser@users.noreply.github.com>
2020-01-13 12:15:09 +0100

    java/2016/assembunny: update VM to handle `tgl` operation

--- java/src/main/java/com/github/saser/adventofcode/year2016/assembunny/VM.java
+++ java/src/main/java/com/github/saser/adventofcode/year2016/assembunny/VM.java
@@ -3,19 +3,18 @@ package com.github.saser.adventofcode.year2016.assembunny;
 import java.io.BufferedReader;
 import java.io.Reader;
 import java.io.StringReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.Optional;
 
 public class VM {
-    public final String[] program;
+    public final String[][] program;
     public int pc;
-    public Map<String, Integer> registers;
+    public int[] registers;
 
     public VM(String[] program, int pc, int a, int b, int c, int d) {
-        this.program = program;
+        this.program = splitProgram(program);
         this.pc = pc;
-        this.registers = new HashMap<>(Map.of("a", a, "b", b, "c", c, "d", d));
+        this.registers = new int[] {a, b, c, d};
     }
 
     public VM(String[] program) {
@@ -34,36 +33,42 @@ public class VM {
         return VM.from(new StringReader(program));
     }
 
+    private static String[][] splitProgram(String[] program) {
+        return Arrays.stream(program)
+                .map(instruction -> instruction.split(" "))
+                .toArray(String[][]::new);
+    }
+
     public int a() {
-        return this.registerValueOf("a").get();
+        return this.register("a");
     }
 
     public int b() {
-        return this.registerValueOf("b").get();
+        return this.register("b");
     }
 
     public int c() {
-        return this.registerValueOf("c").get();
+        return this.register("c");
     }
 
     public int d() {
-        return this.registerValueOf("d").get();
+        return this.register("d");
     }
 
     public void a(int i) {
-        this.registers.put("a", i);
+        this.register("a", i);
     }
 
     public void b(int i) {
-        this.registers.put("b", i);
+        this.register("b", i);
     }
 
     public void c(int i) {
-        this.registers.put("c", i);
+        this.register("c", i);
     }
 
     public void d(int i) {
-        this.registers.put("d", i);
+        this.register("d", i);
     }
 
     public void runAll() {
@@ -74,51 +79,82 @@ public class VM {
 
     public void run() {
         var instruction = this.program[this.pc];
-        var parts = instruction.split(" ", 2);
-        var op = parts[0];
-        var params = parts[1].split(" ");
+        var op = instruction[0];
+        var param1 = instruction[1];
+        var value1 = this.valueOf(param1);
+        var param2 = Optional.<String>empty();
+        var value2 = Optional.<Integer>empty();
+        if (instruction.length - 1 == 2) {
+            param2 = Optional.of(instruction[2]);
+            value2 = Optional.of(this.valueOf(param2.get()));
+        }
         var delta = 1;
         switch (op) {
             case "cpy":
-                this.registers.put(params[1], valueOf(params[0]));
+                try {
+                    this.register(param2.get(), value1);
+                } catch (IllegalArgumentException e) {
+                    // do nothing, this was most likely caused by a `tgl` instruction
+                }
                 break;
             case "inc":
             case "dec":
-                this.registers.merge(params[0], op.equals("inc") ? 1 : -1, Integer::sum);
-                break;
-            case "jnz":
-                if (valueOf(params[0]) != 0) {
-                    delta = valueOf(params[1]);
+                try {
+                    this.register(param1, value1 + (op.equals("inc") ? 1 : -1));
+                } catch (IllegalArgumentException e) {
+                    // do nothing, this was most likely caused by a `tgl` instruction
                 }
                 break;
+            case "jnz":
+                if (value1 != 0) {
+                    delta = value2.get();
+                }
+                break;
+            case "tgl": {
+                var tglIdx = this.pc + value1;
+                if (tglIdx >= this.program.length) {
+                    break;
+                }
+                var tglOp = this.program[tglIdx][0];
+                switch (this.program[tglIdx].length - 1) {
+                    case 1: {
+                        this.program[tglIdx][0] = tglOp.equals("inc") ? "dec" : "inc";
+                        break;
+                    }
+                    case 2: {
+                        this.program[tglIdx][0] = tglOp.equals("jnz") ? "cpy" : "jnz";
+                        break;
+                    }
+                }
+                break;
+            }
             default:
                 throw new IllegalArgumentException(String.format("invalid op: %s", op));
         }
         this.pc += delta;
     }
 
+    private int registerOffset(String x) {
+        var offset = x.charAt(0) - 'a';
+        if (offset < 0 || offset > 3) {
+            throw new IllegalArgumentException(String.format("invalid register: %s", x));
+        }
+        return offset;
+    }
+
+    private int register(String x) {
+        return this.registers[this.registerOffset(x)];
+    }
+
+    private void register(String x, int i) {
+        this.registers[this.registerOffset(x)] = i;
+    }
+
     private int valueOf(String x) {
-        var immediate = this.immediateValueOf(x);
-        if (immediate.isPresent()) {
-            return immediate.get();
-        }
-        var register = this.registerValueOf(x);
-        if (register.isPresent()) {
-            return register.get();
-        }
-        throw new IllegalArgumentException(String.format("invalid parameter: %s", x));
-    }
-
-    private Optional<Integer> immediateValueOf(String x) {
         try {
-            var i = Integer.parseInt(x);
-            return Optional.of(i);
+            return Integer.parseInt(x);
         } catch (NumberFormatException e) {
-            return Optional.empty();
+            return this.register(x);
         }
-    }
-
-    private Optional<Integer> registerValueOf(String x) {
-        return Optional.ofNullable(this.registers.get(x));
     }
 }
