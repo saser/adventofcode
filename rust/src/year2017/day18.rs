3638db9ab72f5efbd3f3a907756260548c604903
Christian Persson <Saser@users.noreply.github.com>
2020-02-07 09:24:18 +0100

    rust/2017/18: add solution for part 1

--- rust/src/year2017/day18.rs
+++ rust/src/year2017/day18.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, VecDeque};
 use std::io::{BufRead, BufReader, Read};
 use std::str::FromStr;
 
@@ -19,9 +19,92 @@ fn solve(r: &mut dyn Read, part: Part) -> Result<String, String> {
         .map(|ref line| Instruction::from_str(line))
         .map(|i| i.expect("instruction parsing failed"))
         .collect::<Vec<Instruction>>();
+    let mut vm0 = VM::new(&instructions);
+    let mut outs0 = vm0.run();
+    if part == Part::One {
+        return Ok(outs0.back().unwrap().to_string());
+    }
     Err("not implemented yet".to_string())
 }
 
+struct VM {
+    instructions: Vec<Instruction>,
+    registers: HashMap<char, i64>,
+    inputs: VecDeque<i64>,
+    pc: isize,
+}
+
+impl VM {
+    fn new(instructions: &[Instruction]) -> Self {
+        VM {
+            instructions: instructions.to_vec(),
+            registers: HashMap::new(),
+            inputs: VecDeque::new(),
+            pc: 0,
+        }
+    }
+
+    fn run(&mut self) -> VecDeque<i64> {
+        let mut outs = VecDeque::new();
+        while let Some(opt_out) = self.step() {
+            if let Some(i) = opt_out {
+                outs.push_back(i);
+            }
+        }
+        outs
+    }
+
+    fn step(&mut self) -> Option<Option<i64>> {
+        if self.pc as usize >= self.instructions.len() {
+            return None;
+        }
+        let mut new_pc = self.pc + 1;
+        let mut out = None;
+        match self.instructions[self.pc as usize] {
+            Instruction::Snd(op) => out = Some(self.eval_op(op)),
+            Instruction::Set(op1, op2) => *self.must_register(op1) = self.eval_op(op2),
+            Instruction::Add(op1, op2) => *self.must_register(op1) += self.eval_op(op2),
+            Instruction::Mul(op1, op2) => *self.must_register(op1) *= self.eval_op(op2),
+            Instruction::Mod(op1, op2) => *self.must_register(op1) %= self.eval_op(op2),
+            Instruction::Rcv(op) => {
+                if self.eval_op(op) != 0 {
+                    match self.inputs.pop_front() {
+                        Some(i) => *self.must_register(op) = i,
+                        None => return None,
+                    };
+                }
+            }
+            Instruction::Jgz(op1, op2) => {
+                if self.eval_op(op1) > 0 {
+                    new_pc = self.pc + self.eval_op(op2) as isize;
+                }
+            }
+        };
+        self.pc = new_pc;
+        Some(out)
+    }
+
+    fn eval_op(&mut self, op: Operand) -> i64 {
+        match op {
+            Operand::Integer(i) => i,
+            Operand::Register(c) => *self.register(c),
+        }
+    }
+
+    fn register(&mut self, c: char) -> &mut i64 {
+        self.registers.entry(c).or_insert(0)
+    }
+
+    fn must_register(&mut self, op: Operand) -> &mut i64 {
+        if let Operand::Register(c) = op {
+            self.register(c)
+        } else {
+            panic!("non-register operand {:?}", op)
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
 enum Instruction {
     Snd(Operand),
     Set(Operand, Operand),
@@ -63,6 +146,7 @@ impl FromStr for Instruction {
     }
 }
 
+#[derive(Clone, Copy, Debug)]
 enum Operand {
     Integer(i64),
     Register(char),
