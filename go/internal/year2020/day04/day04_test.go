2353a24a3714be5f29193a19bc6162df1a632085
Christian Persson <Saser@users.noreply.github.com>
2020-12-04 07:12:13 +0100

    go/2020/04: add skeleton and input

---
+++ go/internal/year2020/day04/day04_test.go
@@ -0,0 +1,36 @@
+package day04
+
+import (
+	"testing"
+
+	"github.com/Saser/adventofcode/internal/testcase"
+)
+
+const inputFile = "../testdata/04"
+
+func TestPart1(t *testing.T) {
+	for _, tc := range []testcase.TestCase{
+		testcase.FromFile(t, inputFile, ""),
+		testcase.FromFile(t, "testdata/example", "2"),
+	} {
+		testcase.Run(t, tc, Part1)
+	}
+}
+
+func BenchmarkPart1(b *testing.B) {
+	tc := testcase.FromFile(b, inputFile, "")
+	testcase.Bench(b, tc, Part1)
+}
+
+func TestPart2(t *testing.T) {
+	for _, tc := range []testcase.TestCase{
+		testcase.FromFile(t, inputFile, ""),
+	} {
+		testcase.Run(t, tc, Part2)
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	tc := testcase.FromFile(b, inputFile, "")
+	testcase.Bench(b, tc, Part2)
+}
