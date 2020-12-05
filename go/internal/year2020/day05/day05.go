44b9788aa6ab58a644d8d112124c3497485e1461
Christian Persson <Saser@users.noreply.github.com>
2020-12-05 18:57:01 +0100

    go/2020/05: add solution to part 1

--- go/internal/year2020/day05/day05.go
+++ go/internal/year2020/day05/day05.go
@@ -1,6 +1,7 @@
 package day05
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 )
@@ -13,6 +14,31 @@ func Part2(r io.Reader) (string, error) {
 	return solve(r, 2)
 }
 
+func parse(s string) int16 {
+	if len(s) != 10 {
+		panic(fmt.Sprintf("invalid string: %q", s))
+	}
+	n := int16(0)
+	for _, r := range s {
+		n = n << 1
+		if r == 'B' || r == 'R' {
+			n += 1
+		}
+	}
+	return n
+}
+
 func solve(r io.Reader, part int) (string, error) {
-	return "", fmt.Errorf("solution not implemented for part %v", part)
+	if part == 2 {
+		return "", fmt.Errorf("solution not implemented for part %v", part)
+	}
+	sc := bufio.NewScanner(r)
+	sc.Split(bufio.ScanLines)
+	var max int16
+	for sc.Scan() {
+		if n := parse(sc.Text()); n > max {
+			max = n
+		}
+	}
+	return fmt.Sprint(max), nil
 }
--- go/internal/year2020/day05/day05_test.go
+++ go/internal/year2020/day05/day05_test.go
@@ -8,13 +8,29 @@ import (
 
 const inputFile = "../testdata/05"
 
+func Test_parse(t *testing.T) {
+	for _, tt := range []struct {
+		s    string
+		want int16
+	}{
+		{s: "FBFBBFFRLR", want: (44 << 3) + 5},
+		{s: "BFFFBBFRRR", want: (70 << 3) + 7},
+		{s: "FFFBBBFRRR", want: (14 << 3) + 7},
+		{s: "BBFFBBFRLL", want: (102 << 3) + 4},
+	} {
+		if got := parse(tt.s); got != tt.want {
+			t.Errorf("parse(%q) = %v; want %v", tt.s, got, tt.want)
+		}
+	}
+}
+
 func TestPart1(t *testing.T) {
 	for _, tc := range []testcase.TestCase{
 		testcase.FromString("example1", "FBFBBFFRLR", "357"),
 		testcase.FromString("example2", "BFFFBBFRRR", "567"),
 		testcase.FromString("example3", "FFFBBBFRRR", "119"),
 		testcase.FromString("example4", "BBFFBBFRLL", "820"),
-		testcase.FromFile(t, inputFile, ""),
+		testcase.FromFile(t, inputFile, "832"),
 	} {
 		testcase.Run(t, tc, Part1)
 	}
