dee4b6e05c399e62e78f385a8a7ed05fea54ac5c
Christian Persson <Saser@users.noreply.github.com>
2020-12-05 08:57:17 +0100

    go/2020/05: add examples for part 1

--- go/internal/year2020/day05/day05_test.go
+++ go/internal/year2020/day05/day05_test.go
@@ -10,6 +10,10 @@ const inputFile = "../testdata/05"
 
 func TestPart1(t *testing.T) {
 	for _, tc := range []testcase.TestCase{
+		testcase.FromString("example1", "FBFBBFFRLR", "357"),
+		testcase.FromString("example2", "BFFFBBFRRR", "567"),
+		testcase.FromString("example3", "FFFBBBFRRR", "119"),
+		testcase.FromString("example4", "BBFFBBFRLL", "820"),
 		testcase.FromFile(t, inputFile, ""),
 	} {
 		testcase.Run(t, tc, Part1)
