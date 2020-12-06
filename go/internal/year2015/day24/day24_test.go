fb0a80ca172b5a3f4a565fa1f42a6e830ba56d17
Christian Persson <Saser@users.noreply.github.com>
2020-12-06 12:39:09 +0100

    2015/24

--- go/internal/year2015/day24/day24_test.go
+++ go/internal/year2015/day24/day24_test.go
@@ -11,10 +11,15 @@ const (
 	inputFile   = "../testdata/24"
 )
 
+var (
+	tcPart1 = testcase.NewFile("input", inputFile, "11266889531")
+	tcPart2 = testcase.NewFile("input", inputFile, "77387711")
+)
+
 func TestPart1(t *testing.T) {
 	for _, tc := range []testcase.TestCase2{
 		testcase.NewFile(exampleFile, exampleFile, "99"),
-		testcase.NewFile(inputFile, inputFile, "11266889531"),
+		tcPart1,
 	} {
 		tc.Test(t, Part1)
 	}
@@ -27,7 +32,7 @@ func BenchmarkPart1(b *testing.B) {
 func TestPart2(t *testing.T) {
 	for _, tc := range []testcase.TestCase2{
 		testcase.NewFile(exampleFile, exampleFile, "44"),
-		testcase.NewFile(inputFile, inputFile, "77387711"),
+		tcPart2,
 	} {
 		tc.Test(t, Part2)
 	}
