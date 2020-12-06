56ef538c942aac1bcab0a768ddfcc15020a384a5
Christian Persson <Saser@users.noreply.github.com>
2020-12-06 12:23:43 +0100

    2015/18

--- go/internal/year2015/day18/day18_test.go
+++ go/internal/year2015/day18/day18_test.go
@@ -14,6 +14,11 @@ const (
 	inputFile              = "../testdata/18"
 )
 
+var (
+	tcPart1 = testcase.NewFile("input", inputFile, "1061")
+	tcPart2 = testcase.NewFile("input", inputFile, "1006")
+)
+
 func TestPart1(t *testing.T) {
 	for _, tt := range []struct {
 		tc         testcase.TestCase2
@@ -21,14 +26,22 @@ func TestPart1(t *testing.T) {
 		gridSize   int
 	}{
 		{tc: testcase.NewFile(exampleFile, exampleFile, "4"), iterations: exampleIterationsPart1, gridSize: exampleGridSize},
-		{tc: testcase.NewFile(inputFile, inputFile, "1061"), iterations: Iterations, gridSize: GridSize},
+		{tc: tcPart1, iterations: Iterations, gridSize: GridSize},
 	} {
-		tt.tc.Test(t, Part1(tt.iterations, tt.gridSize))
+		resetIterations := Iterations
+		Iterations = tt.iterations
+		resetGridSize := GridSize
+		GridSize = tt.gridSize
+
+		tt.tc.Test(t, Part1)
+
+		GridSize = resetGridSize
+		Iterations = resetIterations
 	}
 }
 
 func BenchmarkPart1(b *testing.B) {
-	testcase.Bench(b, tc, Part1(Iterations, GridSize))
+	tcPart1.Benchmark(b, Part1)
 }
 
 func TestPart2(t *testing.T) {
@@ -38,12 +51,20 @@ func TestPart2(t *testing.T) {
 		gridSize   int
 	}{
 		{tc: testcase.NewFile(exampleFile, exampleFile, "17"), iterations: exampleIterationsPart2, gridSize: exampleGridSize},
-		{tc: testcase.NewFile(inputFile, inputFile, "1006"), iterations: Iterations, gridSize: GridSize},
+		{tc: tcPart2, iterations: Iterations, gridSize: GridSize},
 	} {
-		tt.tc.Test(t, Part2(tt.iterations, tt.gridSize))
+		resetIterations := Iterations
+		Iterations = tt.iterations
+		resetGridSize := GridSize
+		GridSize = tt.gridSize
+
+		tt.tc.Test(t, Part2)
+
+		GridSize = resetGridSize
+		Iterations = resetIterations
 	}
 }
 
 func BenchmarkPart2(b *testing.B) {
-	testcase.Bench(b, tc, Part2(Iterations, GridSize))
+	tcPart2.Benchmark(b, Part2)
 }
