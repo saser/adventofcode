01862fd491d252c8cfa096daf9b8d5626b8a7a4c
Christian Persson <Saser@users.noreply.github.com>
2020-12-06 17:38:56 +0100

    internal/striter: improve Split runtime?????

--- go/internal/striter/split.go
+++ go/internal/striter/split.go
@@ -3,9 +3,19 @@ package striter
 import "strings"
 
 func Split(s, sep string, f func(int, string) bool) {
-	for i, ss := range strings.Split(s, sep) {
-		if !f(i, ss) {
+	i := 0
+	start := 0
+	for {
+		offset := strings.Index(s[start:], sep)
+		if offset == -1 {
+			f(i, s[start:])
 			break
 		}
+		end := start + offset
+		if !f(i, s[start:end]) {
+			break
+		}
+		start = end + len(sep)
+		i++
 	}
 }
