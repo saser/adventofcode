850a16d406aaafb8ad61d9134b609d7ab7ede459
Christian Persson <Saser@users.noreply.github.com>
2020-12-07 21:55:22 +0100

    go/2020/07: add parsing functions

--- go/internal/year2020/day07/day07.go
+++ go/internal/year2020/day07/day07.go
@@ -1,6 +1,10 @@
 package day07
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func Part1(input string) (string, error) {
 	return solve(input, 1)
@@ -8,6 +12,72 @@ func Part1(input string) (string, error) {
 
 func Part2(input string) (string, error) {
 	return solve(input, 2)
+}
+
+// parseLine parses a single line into the bag being described, and what it can
+// contain.
+func parseLine(line string) (string, map[string]int) {
+	// The lines look like this:
+	//
+	//     shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+	//
+	// The general parsing idea is as follows:
+	// 1. Split around the string " contain ", resulting in these two parts:
+	//        "shiny gold bags"                        // key
+	//        "1 dark olive bag, 2 vibrant plum bags." // contents
+	// 2. For key, trim off the suffix " bags". The result is the return
+	//    value of key.
+	// 3. For contents, trim off the trailing period. After that, contents
+	//    can be split around ", ", resulting in these two parts:
+	//        "1 dark olive bag"    // item1
+	//        "2 vibrant plum bags" // item2
+	// 4. For all itemN:
+	//     a. Trim off any "bag(s)" suffix.
+	//     b. Then, split the string around the first space. The first part
+	//        of that split is then the number, and the second part is the
+	//        bag description. In the contents return value, store a mapping
+	//        from the bag description to the number.
+
+	// Step 1.
+	parts := strings.Split(line, " contain ")
+	// Step 2.
+	key := strings.TrimSuffix(parts[0], " bags")
+	// Step 3.
+	contentsStr := parts[1]
+	if contentsStr == "no other bags." {
+		return key, nil
+	}
+	items := strings.Split(strings.TrimSuffix(parts[1], "."), ", ")
+	// Step 4.
+	contents := make(map[string]int)
+	for _, item := range items {
+		// Step 4a.
+		if item[len(item)-1] == 's' {
+			item = item[:len(item)-1]
+		}
+		item = strings.TrimSuffix(item, " bag")
+		// Step 4b.
+		parts := strings.SplitN(item, " ", 2)
+		number, err := strconv.Atoi(parts[0])
+		if err != nil {
+			panic(err)
+		}
+		bagDesc := parts[1]
+		contents[bagDesc] = number
+	}
+	return key, contents
+}
+
+func parse(input string) map[string]map[string]int {
+	m := make(map[string]map[string]int)
+	for _, line := range strings.Split(input, "\n") {
+		if line == "" {
+			continue
+		}
+		key, contents := parseLine(line)
+		m[key] = contents
+	}
+	return m
 }
 
 func solve(input string, part int) (string, error) {
--- go/internal/year2020/day07/day07_test.go
+++ go/internal/year2020/day07/day07_test.go
@@ -1,21 +1,151 @@
 package day07
 
 import (
+	"io/ioutil"
+	"reflect"
 	"testing"
 
 	"github.com/Saser/adventofcode/internal/testcase"
 )
 
-const inputFile = "../testdata/07"
+const (
+	inputFile   = "../testdata/07"
+	exampleFile = "testdata/example"
+)
 
 var (
 	tcPart1 = testcase.NewFile("input", inputFile, "")
 	tcPart2 = testcase.NewFile("input", inputFile, "")
 )
 
+func Test_parseLine(t *testing.T) {
+	for _, tt := range []struct {
+		line         string
+		wantKey      string
+		wantContents map[string]int
+	}{
+		{
+			line:    "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+			wantKey: "light red",
+			wantContents: map[string]int{
+				"bright white": 1,
+				"muted yellow": 2,
+			},
+		},
+		{
+			line:    "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+			wantKey: "dark orange",
+			wantContents: map[string]int{
+				"bright white": 3,
+				"muted yellow": 4,
+			},
+		},
+		{
+			line:    "bright white bags contain 1 shiny gold bag.",
+			wantKey: "bright white",
+			wantContents: map[string]int{
+				"shiny gold": 1,
+			},
+		},
+		{
+			line:    "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+			wantKey: "muted yellow",
+			wantContents: map[string]int{
+				"shiny gold": 2,
+				"faded blue": 9,
+			},
+		},
+		{
+			line:    "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+			wantKey: "shiny gold",
+			wantContents: map[string]int{
+				"dark olive":   1,
+				"vibrant plum": 2,
+			},
+		},
+		{
+			line:    "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+			wantKey: "dark olive",
+			wantContents: map[string]int{
+				"faded blue":   3,
+				"dotted black": 4,
+			},
+		},
+		{
+			line:    "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+			wantKey: "vibrant plum",
+			wantContents: map[string]int{
+				"faded blue":   5,
+				"dotted black": 6,
+			},
+		},
+		{
+			line:         "faded blue bags contain no other bags.",
+			wantKey:      "faded blue",
+			wantContents: nil,
+		},
+		{
+			line:         "dotted black bags contain no other bags.",
+			wantKey:      "dotted black",
+			wantContents: nil,
+		},
+	} {
+		gotKey, gotContents := parseLine(tt.line)
+		if gotKey != tt.wantKey {
+			t.Errorf("parseLine(%q) key = %v; want %v", tt.line, gotKey, tt.wantKey)
+		}
+		if !reflect.DeepEqual(gotContents, tt.wantContents) {
+			t.Errorf("parseLine(%q) contents = %v; want %v", tt.line, gotContents, tt.wantContents)
+		}
+	}
+}
+
+func Test_parse(t *testing.T) {
+	data, err := ioutil.ReadFile(exampleFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+	input := string(data)
+	got := parse(input)
+	want := map[string]map[string]int{
+		"light red": {
+			"bright white": 1,
+			"muted yellow": 2,
+		},
+		"dark orange": {
+			"bright white": 3,
+			"muted yellow": 4,
+		},
+		"bright white": {
+			"shiny gold": 1,
+		},
+		"muted yellow": {
+			"shiny gold": 2,
+			"faded blue": 9,
+		},
+		"shiny gold": {
+			"dark olive":   1,
+			"vibrant plum": 2,
+		},
+		"dark olive": {
+			"faded blue":   3,
+			"dotted black": 4,
+		},
+		"vibrant plum": {
+			"faded blue":   5,
+			"dotted black": 6,
+		},
+		"faded blue":   nil,
+		"dotted black": nil,
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("parse() = %v\nwant: %v", got, want)
+	}
+}
+
 func TestPart1(t *testing.T) {
 	for _, tc := range []testcase.TestCase{
-		testcase.NewFile("example", "testdata/example", "4"),
+		testcase.NewFile("example", exampleFile, "4"),
 		tcPart1,
 	} {
 		tc.Test(t, Part1)
