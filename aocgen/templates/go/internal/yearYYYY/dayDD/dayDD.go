bb7d883d95686b29807fde7e1c93cc2c8df334c5
Christian Persson <Saser@users.noreply.github.com>
2020-12-07 20:58:06 +0100

    aocgen: update Go templates after input string refactoring

--- aocgen/templates/go/internal/yearYYYY/dayDD/dayDD.go
+++ aocgen/templates/go/internal/yearYYYY/dayDD/dayDD.go
@@ -1,18 +1,15 @@
 package day{{.PaddedDay}}
 
-import (
-	"fmt"
-	"io"
-)
+import "fmt"
 
-func Part1(r io.Reader) (string, error) {
-	return solve(r, 1)
+func Part1(input string) (string, error) {
+	return solve(input, 1)
 }
 
-func Part2(r io.Reader) (string, error) {
-	return solve(r, 2)
+func Part2(input string) (string, error) {
+	return solve(input, 2)
 }
 
-func solve(r io.Reader, part int) (string, error) {
+func solve(input string, part int) (string, error) {
 	return "", fmt.Errorf("solution not implemented for part %v", part)
 }
