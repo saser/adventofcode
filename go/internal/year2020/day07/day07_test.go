6a365b47b07e61c7f8abcdd42a263239093027fd
Christian Persson <Saser@users.noreply.github.com>
2020-12-07 20:58:24 +0100

    go/2020/07: add skeleton code

---
+++ go/internal/year2020/day07/day07_test.go
@@ -0,0 +1,38 @@
+package day07
+
+import (
+	"testing"
+
+	"github.com/Saser/adventofcode/internal/testcase"
+)
+
+const inputFile = "../testdata/07"
+
+var (
+	tcPart1 = testcase.NewFile("input", inputFile, "")
+	tcPart2 = testcase.NewFile("input", inputFile, "")
+)
+
+func TestPart1(t *testing.T) {
+	for _, tc := range []testcase.TestCase{
+		tcPart1,
+	} {
+		tc.Test(t, Part1)
+	}
+}
+
+func BenchmarkPart1(b *testing.B) {
+	tcPart1.Benchmark(b, Part1)
+}
+
+func TestPart2(t *testing.T) {
+	for _, tc := range []testcase.TestCase{
+		tcPart2,
+	} {
+		tc.Test(t, Part2)
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	tcPart2.Benchmark(b, Part2)
+}
