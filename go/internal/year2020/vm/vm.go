00d25bc343e38225f395e3d1078dd9c578b39a12
Christian Persson <Saser@users.noreply.github.com>
2020-12-08 07:46:34 +0100

    go/2020/vm: add package vm with parsing functions

---
+++ go/internal/year2020/vm/vm.go
@@ -0,0 +1,83 @@
+package vm
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Op int
+
+const (
+	Nop Op = iota
+	Acc
+	Jmp
+)
+
+func (op Op) String() string {
+	switch op {
+	case Nop:
+		return "nop"
+	case Acc:
+		return "acc"
+	case Jmp:
+		return "jmp"
+	}
+	panic(fmt.Sprintf("invalid op: %d", op))
+}
+
+type Arg int
+
+func (arg Arg) String() string {
+	return fmt.Sprintf("%+d", arg)
+}
+
+type Instruction struct {
+	Op  Op
+	Arg Arg
+}
+
+func ParseInstruction(s string) (Instruction, error) {
+	sep := " "
+	split := strings.Index(s, sep)
+	opStr := s[:split]
+	var op Op
+	switch opStr {
+	case "nop":
+		op = Nop
+	case "acc":
+		op = Acc
+	case "jmp":
+		op = Jmp
+	default:
+		return Instruction{}, fmt.Errorf("invalid instruction: %q", s)
+	}
+	argStr := s[split+len(sep):]
+	arg, err := strconv.Atoi(argStr)
+	if err != nil {
+		return Instruction{}, fmt.Errorf("invalid instruction: %q: %w", s, err)
+	}
+	return Instruction{
+		Op:  op,
+		Arg: Arg(arg),
+	}, nil
+}
+
+func (i Instruction) String() string {
+	return fmt.Sprintf("%v %v", i.Op, i.Arg)
+}
+
+type Program []Instruction
+
+func ParseProgram(s string) (Program, error) {
+	lines := strings.Split(strings.TrimSpace(s), "\n")
+	instrs := make([]Instruction, len(lines))
+	for i, line := range lines {
+		instr, err := ParseInstruction(line)
+		if err != nil {
+			return nil, fmt.Errorf("parse program: %w", err)
+		}
+		instrs[i] = instr
+	}
+	return instrs, nil
+}
---
+++ go/internal/year2020/vm/vm_test.go
@@ -0,0 +1,114 @@
+package vm
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestOp_String(t *testing.T) {
+	for _, tt := range []struct {
+		op   Op
+		want string
+	}{
+		{op: Nop, want: "nop"},
+		{op: Acc, want: "acc"},
+		{op: Jmp, want: "jmp"},
+	} {
+		if got := tt.op.String(); got != tt.want {
+			t.Errorf("op.String() = %q; want %q", got, tt.want)
+		}
+	}
+}
+
+func TestArg_String(t *testing.T) {
+	for _, tt := range []struct {
+		arg  Arg
+		want string
+	}{
+		{arg: 0, want: "+0"},
+		{arg: 1, want: "+1"},
+		{arg: 99, want: "+99"},
+		{arg: -1, want: "-1"},
+		{arg: -99, want: "-99"},
+	} {
+		if got := tt.arg.String(); got != tt.want {
+			t.Errorf("arg.String() = %q; want %q", got, tt.want)
+		}
+	}
+}
+
+func TestParseInstruction(t *testing.T) {
+	for _, tt := range []struct {
+		s    string
+		want Instruction
+	}{
+		{s: "nop +0", want: Instruction{Op: Nop, Arg: 0}},
+		{s: "acc +1", want: Instruction{Op: Acc, Arg: 1}},
+		{s: "jmp +4", want: Instruction{Op: Jmp, Arg: 4}},
+		{s: "acc +3", want: Instruction{Op: Acc, Arg: 3}},
+		{s: "jmp -3", want: Instruction{Op: Jmp, Arg: -3}},
+		{s: "acc -99", want: Instruction{Op: Acc, Arg: -99}},
+		{s: "acc +1", want: Instruction{Op: Acc, Arg: 1}},
+		{s: "jmp -4", want: Instruction{Op: Jmp, Arg: -4}},
+		{s: "acc +6", want: Instruction{Op: Acc, Arg: 6}},
+	} {
+		got, err := ParseInstruction(tt.s)
+		if err != nil {
+			t.Errorf("ParseInstruction(%q) err = %v", tt.s, err)
+		}
+		if got != tt.want {
+			t.Errorf("ParseInstruction(%q) instr = %#v; want %#v", tt.s, got, tt.want)
+		}
+	}
+}
+
+func TestInstruction_String(t *testing.T) {
+	for _, tt := range []struct {
+		i    Instruction
+		want string
+	}{
+		{i: Instruction{Op: Nop, Arg: 0}, want: "nop +0"},
+		{i: Instruction{Op: Acc, Arg: 1}, want: "acc +1"},
+		{i: Instruction{Op: Jmp, Arg: 4}, want: "jmp +4"},
+		{i: Instruction{Op: Acc, Arg: 3}, want: "acc +3"},
+		{i: Instruction{Op: Jmp, Arg: -3}, want: "jmp -3"},
+		{i: Instruction{Op: Acc, Arg: -99}, want: "acc -99"},
+		{i: Instruction{Op: Acc, Arg: 1}, want: "acc +1"},
+		{i: Instruction{Op: Jmp, Arg: -4}, want: "jmp -4"},
+		{i: Instruction{Op: Acc, Arg: 6}, want: "acc +6"},
+	} {
+		if got := tt.i.String(); got != tt.want {
+			t.Errorf("i.String() = %q; want %q", got, tt.want)
+		}
+	}
+}
+
+func TestParseProgram(t *testing.T) {
+	s := `nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6`
+	want := Program{
+		{Op: Nop, Arg: 0},
+		{Op: Acc, Arg: 1},
+		{Op: Jmp, Arg: 4},
+		{Op: Acc, Arg: 3},
+		{Op: Jmp, Arg: -3},
+		{Op: Acc, Arg: -99},
+		{Op: Acc, Arg: 1},
+		{Op: Jmp, Arg: -4},
+		{Op: Acc, Arg: 6},
+	}
+	got, err := ParseProgram(s)
+	if err != nil {
+		t.Errorf("ParseProgram() err = %v", err)
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("ParseProgram() prog = %v; want %v", got, want)
+	}
+}
