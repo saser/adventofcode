f0ed7c6219c658fce77713ef8de805a69ce99fb3
Christian Persson <Saser@users.noreply.github.com>
2020-12-09 07:47:12 +0100

    go/2020/09: add skeleton code and input

---
+++ go/internal/year2020/day09/day09.go
@@ -0,0 +1,17 @@
+package day09
+
+import "fmt"
+
+var Lookback = 25
+
+func Part1(input string) (string, error) {
+	return solve(input, 1)
+}
+
+func Part2(input string) (string, error) {
+	return solve(input, 2)
+}
+
+func solve(input string, part int) (string, error) {
+	return "", fmt.Errorf("solution not implemented for part %v", part)
+}
---
+++ go/internal/year2020/day09/day09_test.go
@@ -0,0 +1,51 @@
+package day09
+
+import (
+	"testing"
+
+	"github.com/Saser/adventofcode/internal/testcase"
+)
+
+const (
+	inputFile       = "../testdata/09"
+	defaultLookback = 25
+
+	exampleFile     = "testdata/example"
+	exampleLookback = 5
+)
+
+var (
+	tcPart1 = testcase.NewFile("input", inputFile, "")
+	tcPart2 = testcase.NewFile("input", inputFile, "")
+)
+
+func TestPart1(t *testing.T) {
+	for _, tt := range []struct {
+		tc       testcase.TestCase
+		lookback int
+	}{
+		{tc: testcase.NewFile("example", exampleFile, "127"), lookback: exampleLookback},
+		{tc: tcPart1, lookback: defaultLookback},
+	} {
+		reset := Lookback
+		Lookback = tt.lookback
+		tt.tc.Test(t, Part1)
+		Lookback = reset
+	}
+}
+
+func BenchmarkPart1(b *testing.B) {
+	tcPart1.Benchmark(b, Part1)
+}
+
+func TestPart2(t *testing.T) {
+	for _, tc := range []testcase.TestCase{
+		tcPart2,
+	} {
+		tc.Test(t, Part2)
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	tcPart2.Benchmark(b, Part2)
+}
