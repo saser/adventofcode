1895da582d641cce7e71ded7359400f0e3a6fe98
Christian Persson <Saser@users.noreply.github.com>
2020-12-13 10:54:07 +0100

    go/2020/13: add solution for part 2

--- go/internal/year2020/day13/day13.go
+++ go/internal/year2020/day13/day13.go
@@ -2,6 +2,7 @@ package day13
 
 import (
 	"fmt"
+	"math/big"
 	"strconv"
 	"strings"
 )
@@ -37,11 +38,7 @@ func parse(input string) (int, []int) {
 	return ts, buses
 }
 
-func solve(input string, part int) (string, error) {
-	if part == 2 {
-		return "", fmt.Errorf("solution not implemented for part %v", part)
-	}
-	ts, buses := parse(input)
+func findBestBus(ts int, buses []int) (int, int) {
 	minWait := 60
 	var bestBus int
 	for _, bus := range buses {
@@ -54,5 +51,68 @@ func solve(input string, part int) (string, error) {
 			bestBus = bus
 		}
 	}
-	return fmt.Sprint(bestBus * minWait), nil
+	return bestBus, minWait
+}
+
+type eq struct {
+	rem, mod *big.Int
+}
+
+// crt uses the Chinese remainder theorem to find a solution to the
+// set of equations given b eqs.
+func crt(eqs []eq) *big.Int {
+	var (
+		gcd            big.Int
+		m1, m2         big.Int
+		a2m1n1, a1m2n2 big.Int
+		n1n2           big.Int
+		x              big.Int
+	)
+	acc := eqs[0]
+	for _, eq := range eqs[1:] {
+		a1, n1 := acc.rem, acc.mod
+		a2, n2 := eq.rem, eq.mod
+		// GCD runs the extended Euclidean algorithm. We know
+		// the result is 1, and we are only interested in
+		// obtaining the values of m1 and m2.
+		gcd.GCD(&m1, &m2, n1, n2)
+		a2m1n1.Mul(a2, &m1).Mul(&a2m1n1, n1)
+		a1m2n2.Mul(a1, &m2).Mul(&a1m2n2, n2)
+		x.Add(&a2m1n1, &a1m2n2)
+		n1n2.Mul(n1, n2)
+		acc.rem.Mod(&x, &n1n2)
+		acc.mod = &n1n2
+	}
+	return acc.rem
+}
+
+func earliestTimestamp(buses []int) *big.Int {
+	var eqs []eq
+	for i, bus := range buses {
+		if bus == -1 {
+			continue
+		}
+		rem := (bus - i) % bus
+		if rem < 0 {
+			rem += bus
+		}
+		eqs = append(eqs, eq{
+			rem: big.NewInt(int64(rem)),
+			mod: big.NewInt(int64(bus)),
+		})
+	}
+	return crt(eqs)
+}
+
+func solve(input string, part int) (string, error) {
+	ts, buses := parse(input)
+	switch part {
+	case 1:
+		bestBus, minWait := findBestBus(ts, buses)
+		return fmt.Sprint(bestBus * minWait), nil
+	case 2:
+		ts := earliestTimestamp(buses)
+		return ts.String(), nil
+	}
+	return "", fmt.Errorf("invalid part: %v", part)
 }
--- go/internal/year2020/day13/day13_test.go
+++ go/internal/year2020/day13/day13_test.go
@@ -1,6 +1,7 @@
 package day13
 
 import (
+	"math/big"
 	"testing"
 
 	"github.com/Saser/adventofcode/internal/testcase"
@@ -13,7 +14,7 @@ const (
 
 var (
 	tcPart1 = testcase.NewFile("input", inputFile, "203")
-	tcPart2 = testcase.NewFile("input", inputFile, "")
+	tcPart2 = testcase.NewFile("input", inputFile, "905694340256752")
 )
 
 func TestPart1(t *testing.T) {
@@ -29,8 +30,34 @@ func BenchmarkPart1(b *testing.B) {
 	tcPart1.Benchmark(b, Part1)
 }
 
+func Test_crt(t *testing.T) {
+	for _, tt := range []struct {
+		eqs  []eq
+		want *big.Int
+	}{
+		{
+			eqs: []eq{
+				{rem: big.NewInt(0), mod: big.NewInt(3)},
+				{rem: big.NewInt(3), mod: big.NewInt(4)},
+				{rem: big.NewInt(4), mod: big.NewInt(5)},
+			},
+			want: big.NewInt(39),
+		},
+	} {
+		if got := crt(tt.eqs); got.Cmp(tt.want) != 0 {
+			t.Errorf("crt(%+v) = %v; want %v", tt.eqs, got, tt.want)
+		}
+	}
+}
+
 func TestPart2(t *testing.T) {
 	for _, tc := range []testcase.TestCase{
+		testcase.New("example1", "0\n7,13,x,x,59,x,31,19", "1068781"),
+		testcase.New("example2", "0\n17,x,13,19", "3417"),
+		testcase.New("example3", "0\n67,7,59,61", "754018"),
+		testcase.New("example4", "0\n67,x,7,59,61", "779210"),
+		testcase.New("example5", "0\n67,7,x,59,61", "1261476"),
+		testcase.New("example6", "0\n1789,37,47,1889", "1202161486"),
 		tcPart2,
 	} {
 		tc.Test(t, Part2)
