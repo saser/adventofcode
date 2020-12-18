b9dbe47a7da832634e214470459b4a6ba42ce2cc
Christian Persson <Saser@users.noreply.github.com>
2020-12-18 07:50:40 +0100

    go/2020/18: add skeleton code and input

---
+++ go/internal/year2020/day18/day18_test.go
@@ -0,0 +1,44 @@
+package day18
+
+import (
+	"testing"
+
+	"github.com/Saser/adventofcode/internal/testcase"
+)
+
+const inputFile = "../testdata/18"
+
+var (
+	tcPart1 = testcase.NewFile("input", inputFile, "")
+	tcPart2 = testcase.NewFile("input", inputFile, "")
+)
+
+func TestPart1(t *testing.T) {
+	for _, tc := range []testcase.TestCase{
+		testcase.New("example1", "1 + 2 * 3 + 4 * 5 + 6", "71"),
+		testcase.New("example2", "1 + (2 * 3) + (4 * (5 + 6))", "51"),
+		testcase.New("example3", "2 * 3 + (4 * 5)", "26"),
+		testcase.New("example4", "5 + (8 * 3 + 9 + 3 * 4 * 3)", "437"),
+		testcase.New("example5", "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", "12240"),
+		testcase.New("example6", "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", "13632"),
+		tcPart1,
+	} {
+		tc.Test(t, Part1)
+	}
+}
+
+func BenchmarkPart1(b *testing.B) {
+	tcPart1.Benchmark(b, Part1)
+}
+
+func TestPart2(t *testing.T) {
+	for _, tc := range []testcase.TestCase{
+		tcPart2,
+	} {
+		tc.Test(t, Part2)
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	tcPart2.Benchmark(b, Part2)
+}
