e066042dcb671efc79793531b80f4324779e753b
Christian Persson <Saser@users.noreply.github.com>
2020-12-18 12:22:59 +0100

    go/2020/18: add example tests for part 2

--- go/internal/year2020/day18/day18_test.go
+++ go/internal/year2020/day18/day18_test.go
@@ -33,6 +33,12 @@ func BenchmarkPart1(b *testing.B) {
 
 func TestPart2(t *testing.T) {
 	for _, tc := range []testcase.TestCase{
+		testcase.New("example1", "1 + 2 * 3 + 4 * 5 + 6", "231"),
+		testcase.New("example2", "1 + (2 * 3) + (4 * (5 + 6))", "51"),
+		testcase.New("example3", "2 * 3 + (4 * 5)", "46"),
+		testcase.New("example4", "5 + (8 * 3 + 9 + 3 * 4 * 3)", "1445"),
+		testcase.New("example5", "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", "669060"),
+		testcase.New("example6", "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", "23340"),
 		tcPart2,
 	} {
 		tc.Test(t, Part2)
