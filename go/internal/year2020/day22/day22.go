53013d8975f334455e1bc870c418ad1e00175f03
Christian Persson <Saser@users.noreply.github.com>
2020-12-22 23:21:13 +0100

    go/2020/22: improve runtime by using a custom hashmap for seen games

--- go/internal/year2020/day22/day22.go
+++ go/internal/year2020/day22/day22.go
@@ -1,7 +1,9 @@
 package day22
 
 import (
+	"bytes"
 	"fmt"
+	"hash/maphash"
 	"strconv"
 	"strings"
 )
@@ -14,25 +16,31 @@ func Part2(input string) (string, error) {
 	return solve(input, 2)
 }
 
-type deck []int
+type deck []uint8
 
-func newDeck(cards []int) *deck {
+func newDeck(cards []uint8) *deck {
 	d := make(deck, len(cards))
 	copy(d, cards)
 	return &d
+}
+
+func (d *deck) Clone() *deck {
+	d2 := make(deck, len(*d))
+	copy(d2, *d)
+	return &d2
 }
 
 func (d *deck) Empty() bool {
 	return len(*d) == 0
 }
 
-func (d *deck) PopFront() int {
+func (d *deck) PopFront() uint8 {
 	v := (*d)[0]
 	*d = (*d)[1:]
 	return v
 }
 
-func (d *deck) PushBack(v int) {
+func (d *deck) PushBack(v uint8) {
 	*d = append(*d, v)
 }
 
@@ -44,15 +52,15 @@ func (d *deck) String() string {
 	return strings.Join(ss, ",")
 }
 
-func parsePlayer(paragraph string) []int {
+func parsePlayer(paragraph string) []uint8 {
 	lines := strings.Split(paragraph, "\n")[1:] // skip the first line
-	cards := make([]int, len(lines))
+	cards := make([]uint8, len(lines))
 	for i, line := range lines {
 		card, err := strconv.Atoi(line)
 		if err != nil {
 			panic(err)
 		}
-		cards[i] = card
+		cards[i] = uint8(card)
 	}
 	return cards
 }
@@ -75,61 +83,105 @@ func combat(deck1, deck2 *deck) *deck {
 	return deck2
 }
 
-func recursiveCombat(deck1, deck2 *deck) *deck {
-	// memo holds the set of previously seen configurations in this
-	// game. Its elements are computed using the memoKey function.
-	memo := make(map[string]bool)
-	memoKey := func(deck1, deck2 *deck) string {
-		return deck1.String() + "/" + deck2.String()
+type cacheEntry struct {
+	deck1, deck2 *deck
+}
+
+type cache struct {
+	m map[uint64][]cacheEntry
+	h maphash.Hash
+}
+
+func newCache() *cache {
+	return &cache{
+		m: make(map[uint64][]cacheEntry),
 	}
+}
+
+func (c *cache) hash(deck1, deck2 *deck) uint64 {
+	c.h.Reset()
+	c.h.Write(*deck1)
+	c.h.WriteByte(0) // assumes that 0 does not appear in the input
+	c.h.Write(*deck2)
+	return c.h.Sum64()
+}
+
+func (c *cache) Contains(deck1, deck2 *deck) bool {
+	hash := c.hash(deck1, deck2)
+	entries, ok := c.m[hash]
+	if !ok {
+		return false
+	}
+	for _, entry := range entries {
+		if bytes.Equal(*entry.deck1, *deck1) && bytes.Equal(*entry.deck2, *deck2) {
+			return true
+		}
+	}
+	return false
+}
+
+func (c *cache) Store(deck1, deck2 *deck) {
+	hash := c.hash(deck1, deck2)
+	entry := cacheEntry{
+		deck1: deck1.Clone(),
+		deck2: deck2.Clone(),
+	}
+	c.m[hash] = append(c.m[hash], entry)
+}
+
+func subGame(deck1, deck2 *deck) (winner *deck) {
+	// seen holds the set of previously seen configurations in this
+	// game. Its elements are computed using the memoKey function.
+	seen := newCache()
 	for !deck1.Empty() && !deck2.Empty() {
-		if memo[memoKey(deck1, deck2)] {
+		if ok := seen.Contains(deck1, deck2); ok {
 			return deck1
 		}
-		memo[memoKey(deck1, deck2)] = true
+		seen.Store(deck1, deck2)
 		card1 := deck1.PopFront()
 		card2 := deck2.PopFront()
 		var (
-			winner        *deck
-			first, second int
+			roundWinner   *deck
+			first, second uint8
 		)
-		if len(*deck1) >= card1 && len(*deck2) >= card2 {
+		if uint8(len(*deck1)) >= card1 && uint8(len(*deck2)) >= card2 {
 			subDeck1 := make(deck, card1)
 			copy(subDeck1, (*deck1)[:card1])
 			subDeck2 := make(deck, card2)
 			copy(subDeck2, (*deck2)[:card2])
-			subWinner := recursiveCombat(&subDeck1, &subDeck2)
+			subWinner := subGame(&subDeck1, &subDeck2)
 			switch subWinner {
 			case &subDeck1:
-				winner = deck1
+				roundWinner = deck1
 				first = card1
 				second = card2
 			case &subDeck2:
-				winner = deck2
+				roundWinner = deck2
 				first = card2
 				second = card1
 			}
 		} else {
 			if card1 > card2 {
-				winner = deck1
+				roundWinner = deck1
 				first = card1
 				second = card2
 			} else {
-				winner = deck2
+				roundWinner = deck2
 				first = card2
 				second = card1
 			}
 		}
-		winner.PushBack(first)
-		winner.PushBack(second)
+		roundWinner.PushBack(first)
+		roundWinner.PushBack(second)
 	}
-	var winner *deck
 	if !deck1.Empty() {
-		winner = deck1
-	} else {
-		winner = deck2
+		return deck1
 	}
-	return winner
+	return deck2
+}
+
+func recursiveCombat(deck1, deck2 *deck) *deck {
+	return subGame(deck1, deck2)
 }
 
 func solve(input string, part int) (string, error) {
@@ -146,7 +198,7 @@ func solve(input string, part int) (string, error) {
 	score := 0
 	f := len(*winner)
 	for _, card := range *winner {
-		score += card * f
+		score += int(card) * f
 		f--
 	}
 	return fmt.Sprint(score), nil
