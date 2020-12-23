5bc87b002631be38a63a3c24fd938c3d5e68a7d6
Christian Persson <Saser@users.noreply.github.com>
2020-12-23 10:52:28 +0100

    go/2020/23: add skeleton code and input

---
+++ go/internal/year2020/day23/day23.go
@@ -0,0 +1,17 @@
+package day23
+
+import (
+	"fmt"
+)
+
+func Part1(input string) (string, error) {
+	return solve(input, 1)
+}
+
+func Part2(input string) (string, error) {
+	return solve(input, 2)
+}
+
+func solve(input string, part int) (string, error) {
+	return "", fmt.Errorf("solution not implemented for part %v", part)
+}
