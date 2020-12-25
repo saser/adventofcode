38906af9f7f42d53f8d7a556ff0b3711b8a978af
Christian Persson <Saser@users.noreply.github.com>
2020-12-26 00:30:29 +0100

    go/2020/23: improve runtime by replacing ring with integer slice

--- go/internal/year2020/day23/day23.go
+++ go/internal/year2020/day23/day23.go
@@ -1,7 +1,6 @@
 package day23
 
 import (
-	"container/ring"
 	"fmt"
 	"strings"
 )
@@ -15,115 +14,112 @@ func Part2(input string) (string, error) {
 }
 
 type cups struct {
-	start *ring.Ring
-	nodes []*ring.Ring // number -> node in ring
-	max   int
+	curr int
+	next []int // number -> next number
 }
 
-func parse(input string) *ring.Ring {
-	trimmed := strings.TrimSpace(input)
-	r := ring.New(len(trimmed))
-	for _, c := range trimmed {
-		v := int(c - '0')
-		r.Value = v
-		r = r.Next()
+func newCups(numbers []int) *cups {
+	n := len(numbers)
+	c := &cups{
+		curr: numbers[0],
+		next: make([]int, n),
 	}
-	return r
-}
-
-func newCups(r *ring.Ring) *cups {
-	n := r.Len()
-	m2 := make([]*ring.Ring, n)
-	node := r
-	for i := 0; i < n; i++ {
-		v := node.Value.(int)
-		m2[v-1] = node
-		node = node.Next()
+	for i := 0; i < n-1; i++ {
+		c.setNext(numbers[i], numbers[i+1])
 	}
-	return &cups{
-		start: r,
-		nodes: m2,
-		max:   n,
-	}
+	c.setNext(numbers[n-1], numbers[0])
+	return c
 }
 
-func (c *cups) String() string {
-	var sb strings.Builder
-	c.nodes[0].Do(func(v interface{}) {
-		n := v.(int)
-		if n == 1 {
-			return
-		}
-		sb.WriteString(fmt.Sprint(n))
-	})
-	return sb.String()
+func (c *cups) Next(n int) int {
+	return c.next[n-1]
 }
 
-func (c *cups) DebugString() string {
-	var ss []string
-	c.start.Do(func(v interface{}) {
-		n := v.(int)
-		if len(ss) == 0 {
-			ss = append(ss, fmt.Sprintf("(%s)", fmt.Sprint(n)))
-			return
-		}
-		ss = append(ss, fmt.Sprint(n))
-	})
-	return strings.Join(ss, " ")
-}
-
-func (c *cups) Part2Product() int64 {
-	var prod int64 = 1
-	node := c.nodes[0].Next()
-	prod *= int64(node.Value.(int))
-	prod *= int64(node.Next().Value.(int))
-	return prod
+func (c *cups) setNext(n, next int) {
+	c.next[n-1] = next
 }
 
 func (c *cups) Move() {
-	curr := c.start.Value.(int)
-	held := c.start.Unlink(3)
-	h1 := held
-	h2 := h1.Next()
-	h3 := h2.Next()
-	forbidden := [...]int{
-		h1.Value.(int),
-		h2.Value.(int),
-		h3.Value.(int),
-	}
-	dest := curr - 1
+	// curr -> n1 -> n2 -> n3 -> next
+	n1 := c.Next(c.curr)
+	n2 := c.Next(n1)
+	n3 := c.Next(n2)
+	next := c.Next(n3)
+
+	// Find the destination cup.
+	dest := c.curr - 1
 	for {
 		if dest < 1 {
-			dest = c.max
+			dest = len(c.next)
 		}
-		if !(dest == forbidden[0] || dest == forbidden[1] || dest == forbidden[2]) {
+		if !(dest == n1 || dest == n2 || dest == n3) {
 			break
 		}
 		dest--
 	}
-	c.nodes[dest-1].Link(held)
-	c.start = c.start.Next()
+	dest2 := c.Next(dest)
+	// curr -> n1 -> n2 -> n3 -> next -> ... -> dest -> dest2 -> ...
+
+	// "Unlink" by pointing curr directly to next.
+	//
+	//     curr -> next -> ... -> dest -> dest2 -> ...
+	//              ^
+	//              |
+	// n1 -> n2 -> n3
+	c.setNext(c.curr, next)
+
+	// "Link" by pointing dest to n1 ...
+	//
+	//                              curr -> next
+	//                                       ^
+	//                                       |
+	// (next -> ... ->) dest -> n1 -> n2 -> n3
+	c.setNext(dest, n1)
+	// ... and then pointing n3 to dest2.
+	//
+	// curr -> next -> ... -> dest -> n1 -> n2 -> n3 -> dest2 -> ...
+	c.setNext(n3, dest2)
+
+	// Finally, update c.curr.
+	c.curr = next
+}
+
+func parse(input string, numbers []int) {
+	for i, r := range input {
+		numbers[i] = int(r - '0')
+	}
 }
 
 func solve(input string, part int) (string, error) {
-	r := parse(input)
-	moveCount := 100
-	if part == 2 {
-		n := r.Len()
-		rest := ring.New(1000000 - n)
-		for i := n + 1; i <= 1000000; i++ {
-			rest.Value = i
-			rest = rest.Next()
+	trimmed := strings.TrimSpace(input)
+	var (
+		numbers   []int
+		moveCount int
+	)
+	switch part {
+	case 1:
+		numbers = make([]int, len(trimmed))
+		moveCount = 100
+	case 2:
+		numbers = make([]int, 1000000)
+		for i := 1; i <= 1000000; i++ {
+			numbers[i-1] = i
 		}
-		r.Prev().Link(rest)
 		moveCount = 10000000
 	}
-	c := newCups(r)
+	parse(trimmed, numbers)
+	c := newCups(numbers)
 	for i := 0; i < moveCount; i++ {
 		c.Move()
 	}
 	if part == 1 {
-		return c.String(), nil
+		var sb strings.Builder
+		for n := c.Next(1); n != 1; n = c.Next(n) {
+			sb.WriteRune(rune(n) + '0')
+		}
+		return sb.String(), nil
 	}
-	return fmt.Sprint(c.Part2Product()), nil
+	p1 := c.Next(1)
+	p2 := c.Next(p1)
+	return fmt.Sprint(p1 * p2), nil
 }
